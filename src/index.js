f1e0eb1928ac565e33eb42869f9d69d45c08832d
Syver Enstad <syver.enstad@gmail.com>
2020-04-17 14:25:39 +0200

    Viser forerhund skjema
    * foreløpig ikke noe React her

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+window.onload = function() {
+    window.Formio.builder(document.getElementById('builder'), 'http://localhost:3001/forerhund/', {});
+};
+
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+ */
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
