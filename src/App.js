b28a7a5058c67565583c3982452a14a773f3ef3e
Syver Enstad <syver.enstad@gmail.com>
2020-04-20 17:27:41 +0200

    Custom fields

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import { FormEdit } from "react-formio";
+import { FormEdit, Components } from "react-formio";
 import Formiojs from "formiojs/Formio";
+
+import components from "./Custom";
+
+Components.setComponents(components);
 
 function App() {
   const path = `http://localhost:3001/forerhund`;
@@ -22,6 +26,34 @@ function App() {
         <FormEdit
           options={{ src: "http://localhost:3001/forerhund" }}
           form={form}
+          options={{
+            builder: {
+              customFlesk: {
+                title: 'Alfa komponenter',
+                weight: 100,
+                components: {
+                  email: true,
+                  phoneNumber: true
+                }
+              },
+              customBasic: {
+                title: 'Generiske komponenter',
+                default: true,
+                weight: 100,
+                components: {
+                  toggleCustomComp: true,
+                  textfield: true,
+                  textarea: true,
+                  email: true,
+                  phoneNumber: true
+                }
+              },
+              basic: {
+                title: 'Basiske snutter'
+              },
+              advanced: false
+            }
+          }}
           saveForm={onSave}
           saveText="LAGRE"
         />
