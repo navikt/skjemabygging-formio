67a3d45a9704e9ddc540c94a5a55a40128c06de3
Sissel Fladby <sissel.fladby@nav.no>
2020-04-28 14:03:14 +0200

    Ferdigutfylt skjemaside med utskrift
    - Legger på onSubmit for å lagre submission i state
    - Mulighet for utskrift av resultat (window.print)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import "../node_modules/formiojs/dist/formio.full.min.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -12,6 +12,7 @@ Components.setComponents(components);
 
 function App() {
   const [forms, setForms] = useState();
+  const [submission, setSubmission] = useState();
 
   useEffect(() => {
     fetch(`${projectURL}/form?type=form&tags=nav-skjema`)
@@ -24,28 +25,55 @@ function App() {
 
   return (
     <div className="app">
-      <Router>
-        <nav>
-          {forms && (
-            <ul>
-              {forms.map(form => (
-                <li key={form._id}>
-                  <Link to={`/${form.path}`}>{form.title}</Link>
-                </li>
-              ))}
-            </ul>
+      <nav>
+        {forms && (
+          <ul>
+            {forms.map(form => (
+              <li key={form._id}>
+                <Link to={`/${form.path}`}>{form.title}</Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </nav>
+      <Switch>
+        <Route exact path="/" render={() => <h1>Velg et skjema</h1>} />
+        <Route
+          exact
+          path="/:formpath"
+          render={routeProps => (
+            <>
+              <h1>Fyll ut søknaden</h1>
+              <Form
+                key="1"
+                src={`${projectURL}${routeProps.match.params.formpath}`}
+                options={{ readOnly: false }}
+                onSubmit={submission => {
+                  setSubmission(submission);
+                  routeProps.history.push(
+                    `/${routeProps.match.params.formpath}/result/${submission._id}`
+                  );
+                }}
+              />
+            </>
           )}
-        </nav>
-        <Switch>
-          <Route exact path="/" render={() => <h1>Velg et skjema</h1>} />
-          <Route
-            path="/:formpath"
-            render={routeProps => (
-              <Form src={`${projectURL}${routeProps.match.params.formpath}`} />
-            )}
-          />
-        </Switch>
-      </Router>
+        />
+        <Route
+          path="/:formpath/result/:id"
+          render={routeProps => (
+            <>
+              <h1>Din søknad</h1>
+              <Form
+                key="2"
+                src={`${projectURL}${routeProps.match.params.formpath}`}
+                options={{ readOnly: true }}
+                submission={submission}
+              />
+              <button onClick={window.print}>Skriv ut</button>
+            </>
+          )}
+        />
+      </Switch>
     </div>
   );
 }
