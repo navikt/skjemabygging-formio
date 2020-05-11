3c2f0bf2b95778bd6a78d0f2c4dad545a48843f7
Sissel Fladby <sissel.fladby@nav.no>
2020-05-11 12:22:26 +0200

    Flytter ut autentiserte routes i egen komponent

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from "react";
-import FormEdit from "./react-formio/FormEdit";
 import Form from "./react-formio/Form";
 import Formiojs from "formiojs/Formio";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect,
-  Link
+  Redirect
 } from "react-router-dom";
+import { AuthenticatedContent } from "./AuthenticatedContent";
+
+const path = `http://localhost:3001/`;
+const formio = new Formiojs(path); //Context-kandidat?
 
 function App() {
-  const path = `http://localhost:3001/`;
-  const formio = new Formiojs(path);
-
   const [forms, setForms] = useState();
   const [authenticated, setAuthenticated] = useState(false);
 
@@ -21,7 +20,7 @@ function App() {
     if (Formiojs.getUser() && !authenticated) {
       setAuthenticated(true);
     }
-  }, []);
+  }, [authenticated]);
 
   useEffect(() => {
     if (authenticated && !forms) {
@@ -29,43 +28,14 @@ function App() {
         .loadForms({ params: { type: "form" } })
         .then(forms => setForms(forms));
     }
-  }, [authenticated]);
-
-  const onSave = form => formio.saveForm(form).then(form => console.log(form));
+  }, [authenticated, forms]);
 
   return (
     <Router>
       <Switch>
-        <Route path={"/forms/:formpath"}>
-          {forms && (
-            <FormEdit
-              key={forms[2]._id}
-              form={forms[2]}
-              options={{
-                src: "http://localhost:3001/forerhund"
-              }}
-              saveForm={onSave}
-              saveText="LAGRE"
-            />
-          )}
-        </Route>
         <Route path="/forms">
           {authenticated ? (
-            <>
-              {forms && (
-                <nav>
-                  <h3>Velg skjema:</h3>
-                  <ul>
-                    {forms.map(form => (
-                      <li key={form.path}>
-                        {console.log(form)}
-                        <Link to={"/forms/" + form.path}>{form.title}</Link>
-                      </li>
-                    ))}
-                  </ul>
-                </nav>
-              )}
-            </>
+            <AuthenticatedContent formio={formio} forms={forms} />
           ) : (
             <Redirect to="/" />
           )}
---
+++ src/AuthenticatedContent.jsx
@@ -0,0 +1,48 @@
+import {Link, Route, Switch} from "react-router-dom";
+import FormEdit from "./react-formio/FormEdit";
+import React from "react";
+
+export const AuthenticatedContent = (props) => {
+  const { forms } = props;
+  return (
+    <Switch>
+      <Route
+        path={"/forms/:formpath"}
+        render={props => {
+          const { formpath } = props.match.params;
+          if (forms) {
+            const form = getFormFromPath(forms, formpath);
+            return form && (
+              <FormEdit
+                key={form._id}
+                form={form}
+                options={{
+                  src: "http://localhost:3001/" + formpath
+                }}
+                saveForm={() => console.log(form)}
+                saveText="LAGRE"
+              />
+            );
+          }
+          return <h1>Laster...</h1>;
+        }}
+      />
+      <Route path="/forms">
+        {forms && (
+          <nav>
+            <h3>Velg skjema:</h3>
+            <ul>
+              {forms.map(form => (
+                <li key={form.path}>
+                  <Link to={"/forms/" + form.path}>{form.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        )}
+      </Route>
+    </Switch>
+  )
+};
+
+const getFormFromPath = (forms, path) => forms.find(form => form.path === path);
