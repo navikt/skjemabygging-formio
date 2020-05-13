1765bee9e9cd1617a79a889c9b8236d9b228ffc9
Sissel Fladby <sissel.fladby@nav.no>
2020-05-13 13:45:58 +0200

    Lagt til lenker til test form/edit form

--- src/Forms.jsx
+++ src/Forms.jsx
@@ -1,7 +1,17 @@
-import { Link, Route, Switch, useRouteMatch } from "react-router-dom";
+import { Link, Route, Switch, useRouteMatch, Redirect } from "react-router-dom";
 import FormEdit from "./react-formio/FormEdit";
+import Form from "./react-formio/Form.jsx";
 import React from "react";
 import { MenuLink, MenuItem, NavBar } from "./NavBar";
+import styled from "styled-jss";
+
+const Pagewrapper = styled("div")({
+  padding: "2rem"
+});
+
+const LinkWrapper = styled("div")({
+  padding: "1rem 0"
+});
 
 export const Forms = ({ forms, projectURL, onLogout }) => {
   let { path, url } = useRouteMatch();
@@ -9,7 +19,7 @@ export const Forms = ({ forms, projectURL, onLogout }) => {
   return (
     <Switch>
       <Route
-        path={`${path}/:formpath`}
+        path={`${path}/:formpath/edit`}
         render={({ match }) => {
           let { params } = match;
           if (forms) {
@@ -22,25 +32,65 @@ export const Forms = ({ forms, projectURL, onLogout }) => {
                     Logout
                   </MenuLink>
                 </NavBar>
-                <>
-                  {form && (
-                    <FormEdit
-                      key={form._id}
-                      form={form}
-                      options={{
-                        src: `${projectURL}/${params.formpath}`
-                      }}
-                      saveForm={() => console.log(form)}
-                      saveText="LAGRE"
-                    />
-                  )}
-                </>
+                <Pagewrapper>
+                  <LinkWrapper>
+                    <Link to={`${path}/${params.formpath}/view`}>
+                      Test skjema
+                    </Link>
+                  </LinkWrapper>
+                  <>
+                    {form && (
+                      <FormEdit
+                        key={form._id}
+                        form={form}
+                        options={{
+                          src: `${projectURL}/${params.formpath}`
+                        }}
+                        saveForm={() => console.log(form)}
+                        saveText="LAGRE"
+                      />
+                    )}
+                  </>
+                </Pagewrapper>
               </>
             );
           }
           return <h1>Laster...</h1>;
         }}
       />
+      <Route
+        path={`${path}/:formpath/view`}
+        render={({ match }) => {
+          let { params } = match;
+          if (forms) {
+            const form = getFormFromPath(forms, params.formpath);
+            return (
+              <>
+                <NavBar>
+                  <MenuLink to="/forms">Skjemaer</MenuLink>
+                  <MenuLink to="/" onClick={onLogout}>
+                    Logout
+                  </MenuLink>
+                </NavBar>
+                <Pagewrapper>
+                  <LinkWrapper>
+                    <Link to={`${path}/${params.formpath}/edit`}>
+                      Rediger skjema
+                    </Link>
+                  </LinkWrapper>
+                  <Form form={form} />
+                </Pagewrapper>
+              </>
+            );
+          }
+          return <h1>Laster...</h1>;
+        }}
+      />
+      <Route path={`${path}/:formpath`}>
+        {({ match }) => (
+          <Redirect to={`${path}/${match.params.formpath}/edit`} />
+        )}
+      </Route>
       <Route path={path}>
         <>
           <NavBar>
@@ -49,18 +99,20 @@ export const Forms = ({ forms, projectURL, onLogout }) => {
               Logout
             </MenuLink>
           </NavBar>
-          {forms && (
-            <nav>
-              <h3>Velg skjema:</h3>
-              <ul>
-                {forms.map(form => (
-                  <li key={form.path}>
-                    <Link to={`${url}/${form.path}`}>{form.title}</Link>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          )}
+          <Pagewrapper>
+            {forms && (
+              <nav>
+                <h3>Velg skjema:</h3>
+                <ul>
+                  {forms.map(form => (
+                    <li key={form.path}>
+                      <Link to={`${url}/${form.path}`}>{form.title}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            )}
+          </Pagewrapper>
         </>
       </Route>
     </Switch>
