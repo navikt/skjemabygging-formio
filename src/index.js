bcd08309666e3f50be76dd5daf02c3b9268a71a0
Sissel Fladby <sissel.fladby@nav.no>
2020-06-23 10:35:36 +0200

    Henter inn skjemapublisering-avhengighet Setter inn en env-variabel for å styre om man skal hente forms fra server eller fra skjemapublisering

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+
+const projectURL =
+  process.env.REACT_APP_FORMIO_PROJECT_URL ||
+  "https://protected-island-44773.herokuapp.com";
+const getFormsFromSkjemapublisering =
+  process.env.REACT_APP_USE_SKJEMAPUBLISERING || false;
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <App
+        projectURL={projectURL}
+        getFormsFromSkjemapublisering={getFormsFromSkjemapublisering}
+      />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
