1d5b38788ed33a886915e155fe34f6ae8d37330f
Syver Enstad <syver.enstad@gmail.com>
2020-06-24 13:31:34 +0200

    Prepare for heroku deployment.
    Changes based on https://github.com/mars/create-react-app-buildpack#customization
    
    Observe the following citation from the documentation:
    
    ```
    Warning: If replacing the default web process, please check this
    buildpack's Purpose to avoid misusing this buildpack (such as running
    a Node server) which can lead to confusing deployment issues.
    ```
    
    The warning here seems very relevant to the changes that we have
    introduced. But we haven't (at least not yet) created a separate node
    package with the node server so the different other recipes mentioned
    as a better alternative did not seem relevant to us at the time. See
    https://github.com/mars/heroku-cra-node

--- server/index.mjs
+++ server/index.mjs
@@ -1,18 +1,21 @@
 import express from 'express';
-const app = express();
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 import {dispatcherWithBackend, Backend} from '../src/backend/index.mjs';
 
-// app.get('/api/hey', (req, res) => res.json({message: 'ho'}));
+// node backward compatibility
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+const app = express();
 app.use('/api', dispatcherWithBackend(new Backend()));
 
 const nodeEnv = process.env.NODE_ENV;
 if (nodeEnv === 'production') {
   // serve built app in production (served by weback dev server in development)
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join(__dirname, '..', 'build')));
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    res.sendFile(path.join(__dirname,'..', 'build', 'index.html'))
   })
 }
 
