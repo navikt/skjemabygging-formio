5d7e998f0552317ad3e5562c8dbb8016f3f8b13a
Sissel Fladby <sissel.fladby@nav.no>
2020-06-30 10:02:43 +0200

    Henter maks 1000 skjema, og sorterer på tidspunkt de er opprettet

--- src/Forms/FormsListPage.jsx
+++ src/Forms/FormsListPage.jsx
@@ -1,41 +1,46 @@
-import {MenuItem, MenuLink, NavBar} from "../components/NavBar";
-import {Pagewrapper, SlettSkjemaKnapp} from "./components";
-import {Link} from "react-router-dom";
-import {Hovedknapp } from "nav-frontend-knapper";
+import { MenuItem, MenuLink, NavBar } from "../components/NavBar";
+import { Pagewrapper, SlettSkjemaKnapp } from "./components";
+import { Link } from "react-router-dom";
+import { Hovedknapp } from "nav-frontend-knapper";
 import React from "react";
 
-export function FormsListPage({logout, forms, url, onDelete, onNew}) {
+export function FormsListPage({ logout, forms, url, onDelete, onNew }) {
   /*const testApi = () => {
     fetch('/api/hey')
       .then(response => response.json())
       .then(json => console.log(json));
   };*/
-  return <>
-    <NavBar title={"Skjemabygger"}>
-      <MenuItem>Skjemaer</MenuItem>
-      <MenuLink to="/" onClick={logout}>
-        Logout
-      </MenuLink>
-    </NavBar>
-    <Pagewrapper>
-      {forms && (
-        <nav>
-          <h3>Velg skjema:</h3>
-          <ul>
-            {forms.map(form => (
-              <li key={form.path}>
-                <Link className="lenke" data-testid="editLink"
-                      to={`${url}/${form.path}/edit`}>{form.title}</Link>
-                <SlettSkjemaKnapp className="lenke" onClick={() => onDelete(form)}>
-                  Slett skjema
-                </SlettSkjemaKnapp>
-              </li>
-            ))}
-          </ul>
-          <Hovedknapp onClick={onNew}>Lag nytt skjema</Hovedknapp>
-          {/*<Knapp onClick={testApi}>Bruk api</Knapp>*/}
-        </nav>
-      )}
-    </Pagewrapper>
-  </>;
+  return (
+    <>
+      <NavBar title={"Skjemabygger"}>
+        <MenuItem>Skjemaer</MenuItem>
+        <MenuLink to="/" onClick={logout}>
+          Logout
+        </MenuLink>
+      </NavBar>
+      <Pagewrapper>
+        {forms && (
+          <nav>
+            <h3>Velg skjema:</h3>
+            <ul>
+              {forms
+                .sort((a, b) => (a.modified < b.modified ? 1 : -1))
+                .map((form) => (
+                  <li key={form.path}>
+                    <Link className="lenke" data-testid="editLink" to={`${url}/${form.path}/edit`}>
+                      {form.title}
+                    </Link>
+                    <SlettSkjemaKnapp className="lenke" onClick={() => onDelete(form)}>
+                      Slett skjema
+                    </SlettSkjemaKnapp>
+                  </li>
+                ))}
+            </ul>
+            <Hovedknapp onClick={onNew}>Lag nytt skjema</Hovedknapp>
+            {/*<Knapp onClick={testApi}>Bruk api</Knapp>*/}
+          </nav>
+        )}
+      </Pagewrapper>
+    </>
+  );
 }
