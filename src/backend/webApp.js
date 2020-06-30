5c94334dfe2bfd0d50e5dbda7e86afcbe4c17a3e
Sissel Fladby <sissel.fladby@nav.no>
2020-06-30 08:56:32 +0200

    Sjekker at brukeren som skal publisere er autentisert

--- src/backend/webApp.js
+++ src/backend/webApp.js
@@ -1,30 +1,53 @@
 import dispatch from "dispatch";
+import fetch from "node-fetch";
+
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
 
 export function dispatcherWithBackend(backend) {
-    return dispatch({
-        '/hey': {
-            GET: (req, res) => res.json(backend.ho())
-        },
-        '/publish/:formPath': {
-            PUT: (req, res, next, formPath) => {
-                console.log('publish ', formPath, req.body);
-                // gjør et eller annet lookup med jwt_tokenet.
-                // lag en ny publiserings commit og push den
-                // get latest publiserings repo
-                // lag ny commit på toppen av dette
+  return dispatch({
+    "/hey": {
+      GET: (req, res) => res.json(backend.ho()),
+    },
+    "/publish/:formPath": {
+      PUT: (req, res, next, formPath) => {
+        // Check that token is valid first
+        fetch(`${backend.getProjectURL()}/current`, {
+          headers: {
+            "Content-Type": "application/json",
+            "x-jwt-token": req.body.token,
+          },
+        })
+          .then(handleErrors)
+          .then(() => {
+            // backend.createAndPushCommit(req.body);
+            res.send("Authorized!");
+          })
+          .catch((err) => {
+            console.error("Error ", err);
+            res.send("Unauthorized!");
+          })
+        // gjør et eller annet lookup med jwt_tokenet.
+        // lag en ny publiserings commit og push den
+        // get latest publiserings repo
+        // lag ny commit på toppen av dette
 
-                // update eller create subfoler ${formPath} med fil form.json som er identisk med req.body
-                backend.clonePublishingRepo();
-                backend.createOrUpdateForm(formPath, req.body);
-                backend.commitChange();
-                backend.pushPublishingRepo();
-                // overskriv form-io-utvidelser filen med full git url til ${this-git-commit}
-                // publisering-test repo har en git submodule som er skjemabygging-formio.
-                // set submodule i commiten til å være oss selv
-                // eller ta form-io-utvidelses "staten" og kopier den inn i et directory
-                // push denne
-                res.send({message: 'Happy clappy', form: req.body});
-            }
-        }
-    });
+        // update eller create subfoler ${formPath} med fil form.json som er identisk med req.body
+        // backend.clonePublishingRepo();
+        // backend.createOrUpdateForm(formPath, req.body);
+        // backend.commitChange();
+        // backend.pushPublishingRepo();
+        // overskriv form-io-utvidelser filen med full git url til ${this-git-commit}
+        // publisering-test repo har en git submodule som er skjemabygging-formio.
+        // set submodule i commiten til å være oss selv
+        // eller ta form-io-utvidelses "staten" og kopier den inn i et directory
+        // push denne
+        //res.send({ message: "Happy clappy", form: req.body });
+      },
+    },
+  });
 }
--- src/useForms.js
+++ src/useForms.js
@@ -1,55 +1,61 @@
-import {useCallback, useEffect, useState} from "react";
+import { useCallback, useEffect, useState } from "react";
 import cloneDeep from "lodash.clonedeep";
+import Formiojs from "formiojs/Formio";
 
 export const useForms = (formio, store, flashSuccessMessage) => {
   const [forms, setFormsInternal] = useState(store.forms);
-  const setForms = useCallback((forms) => {
-    setFormsInternal(forms);
-    store.forms = forms;
-  }, [setFormsInternal, store.forms]);
+  const setForms = useCallback(
+    (forms) => {
+      setFormsInternal(forms);
+      store.forms = forms;
+    },
+    [setFormsInternal, store.forms]
+  );
 
   useEffect(() => {
     if (forms === null) {
-      formio.loadForms({params: {type: "form", tags: "nav-skjema"}}).then(forms => setForms(forms));
+      formio.loadForms({ params: { type: "form", tags: "nav-skjema" } }).then((forms) => setForms(forms));
     }
   }, [forms, setForms, formio]);
 
-  const onChangeForm = form => {
-    setForms([...forms.filter(each => each.path !== form.path), form]);
+  const onChangeForm = (form) => {
+    setForms([...forms.filter((each) => each.path !== form.path), form]);
   };
 
-  const onSave = callbackForm => {
-    formio.saveForm(callbackForm).then(form => {
-      flashSuccessMessage('Lagret skjema ' + form.title);
+  const onSave = (callbackForm) => {
+    formio.saveForm(callbackForm).then((form) => {
+      flashSuccessMessage("Lagret skjema " + form.title);
       onChangeForm(form);
     });
   };
 
-  const onCreate = form => {
-    return formio.saveForm(form)
-      .then(form => {
-        flashSuccessMessage('Opprettet skjemaet ' + form.title);
-        setForms(forms.concat([form]));
-        return form;
-      })
+  const onCreate = (form) => {
+    return formio.saveForm(form).then((form) => {
+      flashSuccessMessage("Opprettet skjemaet " + form.title);
+      setForms(forms.concat([form]));
+      return form;
+    });
   };
 
-  const onDelete = form => {
+  const onDelete = (form) => {
     const update = cloneDeep(form);
-    update.tags = update.tags.filter(each => each !== "nav-skjema");
-    formio.saveForm(update)
-      .then(() => {
-        flashSuccessMessage('Slettet skjemaet ' + form.title);
-        setForms(forms.filter(each => each !== form));
-      });
+    update.tags = update.tags.filter((each) => each !== "nav-skjema");
+    formio.saveForm(update).then(() => {
+      flashSuccessMessage("Slettet skjemaet " + form.title);
+      setForms(forms.filter((each) => each !== form));
+    });
   };
-  const onPublish = form => {
+  const onPublish = (form) => {
+    const payload = JSON.stringify({ form: form, token: Formiojs.getToken() });
+    console.log("payload ", payload);
     fetch(`/api/publish/${form.path}`, {
-      method: 'PUT',
-      headers: {'content-type': 'application/json'},
-      body: JSON.stringify(form)})
-      .then(response => response.json())
-      .then(console.log);
-  }
-  return {forms, onChangeForm, onSave, onCreate, onDelete, onPublish};
+      method: "PUT",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(form),
+    })
+      .then((response) => response.json())
+      .then(console.log)
+      .catch(() => console.log("Noe gikk galt"));
+  };
+  return { forms, onChangeForm, onSave, onCreate, onDelete, onPublish };
 };
