1f3b01ab475f6eb8df994c9b63c0219a5ac2a4e3
Sissel Fladby <sissel.fladby@nav.no>
2020-07-28 16:10:33 +0200

    Øker limit på server for å ta i mot store skjema

--- server/index.mjs
+++ server/index.mjs
@@ -14,8 +14,8 @@ const skjemapubliseringGHUrl = "https://api.github.com/repos/navikt/skjemapublis
 const GH_USER = process.env.GITHUB_USER;
 const GH_PASS = process.env.GITHUB_PASSWORD;
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({extended: true, limit: '50mb'}));
 app.use('/api', dispatcherWithBackend(new Backend(projectURL, skjemapubliseringGHUrl, GH_USER, GH_PASS)));
 
 const nodeEnv = process.env.NODE_ENV;
