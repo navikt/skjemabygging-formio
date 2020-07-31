2ddca1e20807dec7647f3045e6e6b9879a7f3430
Rune Rosseland <rune.rosseland@nav.no>
2020-07-31 14:49:47 +0200

    Posisjonering av knapper under header med css grid

--- src/Forms/EditFormPage.jsx
+++ src/Forms/EditFormPage.jsx
@@ -1,25 +1,64 @@
 import {NavBar} from "../components/NavBar";
 import {Pagewrapper, CenterAlignedActionRow} from "./components";
 import {Link} from "react-router-dom";
-// import {SkjemaVisningSelect} from "../components/FormMetadataEditor";
+import {SkjemaVisningSelect} from "../components/FormMetadataEditor";
 import NavFormBuilder from "../components/NavFormBuilder";
 import React from "react";
 import FormBuilderOptions from "./FormBuilderOptions";
 import {Hovedknapp, Knapp} from "nav-frontend-knapper";
+import {styled} from "@material-ui/styles";
+
+const EditWrapper = styled("div")({
+  display: "grid",
+  gridTemplateColumns: "1fr 4fr 1fr",
+  gridTemplateRows: "30px 50px 10px",
+  columnGap: "20px"
+});
+
+const MainCol = styled("div")({
+  gridColumn: "2",
+  gridRow: "2",
+  alignSelf: "end",
+  justifySelf: "center"
+});
+
+const LeftCol = styled("div") ({
+  gridColumn: "1",
+  gridRow: "2",
+  alignSelf: "end",
+  justifySelf: "center",
+  display: "flex",
+});
+
+// Ikke i bruk ennå, men jeg lar den stå til senere..
+const RightCol = styled("div") ({
+  gridColumn: "3",
+  gridRow: "2",
+  alignSelf: "end",
+  justifySelf: "center"
+});
+
+// Midlertidig styling av <SkjemaVisningSelect> :p
+var style = {
+  width: "150px",
+};
 
 export function EditFormPage({form, testFormUrl, logout, onSave, onChange, onPublish}) {
   const title = `${form.title}`
   return (
     <>
       <NavBar title={title} visSkjemaliste={true} />
-      <CenterAlignedActionRow>
-        {/*<SkjemaVisningSelect form={form} onChange={onChange} />*/}
-        <Link className="knapp" to={testFormUrl}>Test skjema</Link>
-        <Hovedknapp onClick={() => onSave(form)}>Lagre skjema</Hovedknapp>
-        <Knapp onClick={() => onPublish(form)}>Publiser skjema</Knapp>
-      </CenterAlignedActionRow>
+      <EditWrapper>
+        <LeftCol>
+          <div style={style}><SkjemaVisningSelect form={form} onChange={onChange} /></div>
+        </LeftCol>
+        <MainCol>
+          <Link className="knapp" to={testFormUrl}>Test skjema</Link>
+          <Hovedknapp onClick={() => onSave(form)}>Lagre skjema</Hovedknapp>
+          <Knapp onClick={() => onPublish(form)}>Publiser skjema</Knapp>
+        </MainCol>
+      </EditWrapper>
       <Pagewrapper>
-        {/*<FormMetadataEditor form={form} onChange={onChange}/>*/}
         <NavFormBuilder form={form} onChange={onChange} formBuilderOptions={FormBuilderOptions}/>
 
       </Pagewrapper>
