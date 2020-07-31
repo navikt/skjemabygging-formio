c0e53a1d3ee188c6ffd12ff58b07866253c02664
Rune Rosseland <rune.rosseland@nav.no>
2020-07-31 15:35:37 +0200

    Header og knapper scroller ikke og blir stående på toppen av siden

--- src/Forms/EditFormPage.jsx
+++ src/Forms/EditFormPage.jsx
@@ -6,18 +6,30 @@ import NavFormBuilder from "../components/NavFormBuilder";
 import React from "react";
 import FormBuilderOptions from "./FormBuilderOptions";
 import {Hovedknapp, Knapp} from "nav-frontend-knapper";
+import {styled} from "@material-ui/styles";
+import navCssVariables from "nav-frontend-core";
+
+const NoScrollWrapper = styled("div")({
+  backgroundColor: "white",
+  position: "-webkit-sticky", /* Safari */
+  position: "sticky",
+  top: "0",
+  zIndex: 1
+});
 
 export function EditFormPage({form, testFormUrl, logout, onSave, onChange, onPublish}) {
   const title = `${form.title}`
   return (
     <>
-      <NavBar title={title} visSkjemaliste={true} />
-      <CenterAlignedActionRow>
-        {/*<SkjemaVisningSelect form={form} onChange={onChange} />*/}
-        <Link className="knapp" to={testFormUrl}>Test skjema</Link>
-        <Hovedknapp onClick={() => onSave(form)}>Lagre skjema</Hovedknapp>
-        <Knapp onClick={() => onPublish(form)}>Publiser skjema</Knapp>
-      </CenterAlignedActionRow>
+      <NoScrollWrapper>
+        <NavBar title={title} visSkjemaliste={true} />
+        <CenterAlignedActionRow>
+          {/*<SkjemaVisningSelect form={form} onChange={onChange} />*/}
+          <Link className="knapp" to={testFormUrl}>Test skjema</Link>
+          <Hovedknapp onClick={() => onSave(form)}>Lagre skjema</Hovedknapp>
+          <Knapp onClick={() => onPublish(form)}>Publiser skjema</Knapp>
+        </CenterAlignedActionRow>
+      </NoScrollWrapper>
       <Pagewrapper>
         {/*<FormMetadataEditor form={form} onChange={onChange}/>*/}
         <NavFormBuilder form={form} onChange={onChange} formBuilderOptions={FormBuilderOptions}/>
