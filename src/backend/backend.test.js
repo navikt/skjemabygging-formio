8ee41172dbd04c8bfa6b67dc71ca747902f249aa
Sissel Fladby <sissel.fladby@nav.no>
2020-08-03 08:48:28 +0200

    Skall til testing av publiseringsapi

--- src/backend/backend.test.js
+++ src/backend/backend.test.js
@@ -1,11 +1,32 @@
 import {Backend} from "./index.js";
+// import sinon from "sinon"; - test ut stubbing herfra?
 
 describe('Backend', () => {
-  it('clones the repo to the disk', async () => {
-    const workingFolder = '/tmp/publishingBackend';
-    const gitUri = 'git@github.com:navikt/skjemapublisering-test.git';
-    console.log(Backend);
-    const backend = new Backend(workingFolder, gitUri);
-    await backend.cloneRepo();
+  xit('checks access', async () => {
+
+  });
+
+  xit('gets list of all published forms in github', async () => {
+
+  });
+
+  xit('gets sha of matching form if any', async () => {
+
+  });
+
+  xit('updates form if matching sha', async () => {
+
+  });
+
+  xit('creates new form if not matching sha', async () => {
+
+  });
+
+  xit('updates form if matching sha', async () => {
+
+  });
+
+  xit('creates new form if not matching sha', async () => {
+
   });
 });
