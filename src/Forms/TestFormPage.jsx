0352957cdab14853f7f9f3c3b601160113837d4e
Rune Rosseland <rune.rosseland@nav.no>
2020-08-13 16:02:49 +0200

    Fjernet utkommentert kode

--- src/Forms/TestFormPage.jsx
+++ src/Forms/TestFormPage.jsx
@@ -32,17 +32,3 @@ export function TestFormPage({onChange, onPublishClick, publiserer, logout, edit
         </>
     );
 }
-
-
-
-/*
-<NavBar title={form && form.title}>
-  <Link className="knapp" to="/forms">Skjemaer</Link>
-  <Link className="knapp" to="/" onClick={logout}>
-    Logout
-  </Link>
-</NavBar>
-<CenterAlignedActionRow>
-  <Link className="knapp" to={editFormUrl}>Rediger skjema</Link>
-  <Hovedknapp onClick={() => onSave(form)}>Lagre skjema</Hovedknapp>
-</CenterAlignedActionRow>*/
