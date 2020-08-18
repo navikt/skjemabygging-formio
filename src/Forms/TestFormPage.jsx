dbfc92e2f5756ab28be0923e27c8d2e3ecaff7ba
Syver Enstad <syver.enstad@gmail.com>
2020-08-18 17:16:57 +0200

    Port TestFormPage to use AppLayout

--- src/Forms/TestFormPage.jsx
+++ src/Forms/TestFormPage.jsx
@@ -1,30 +1,22 @@
-import {NavBar} from "../components/NavBar";
-import {Pagewrapper, NoScrollWrapper} from "./components";
 import {Link} from "react-router-dom";
 import React from "react";
 import NavForm from "../components/NavForm";
 import {Hovedknapp, Knapp} from "nav-frontend-knapper";
-import {ActionRow, ActionRowWrapper, MainCol} from "../components/ActionRow";
+import {AppLayoutWithContext} from "../components/AppLayout";
 
-export function TestFormPage({ onPublishClick, publiserer, editFormUrl, form, onSave, userAlerter}) {
+export function TestFormPage({onPublishClick, publiserer, editFormUrl, form, onSave, userAlerter}) {
   const title = `${form.title}`;
   return (
-        <>
-          <NoScrollWrapper>
-            <NavBar title={title} visSkjemaliste={true} />
-            <ActionRow userAlerter={userAlerter}>
-              <MainCol>
-                <Link className="knapp" to={editFormUrl}>Rediger skjema</Link>
-                <Hovedknapp onClick={() => onSave(form)}>Lagre skjema</Hovedknapp>
-                <Knapp onClick={() => onPublishClick(form)} spinner={publiserer}>
-                  Publiser skjema
-                </Knapp>
-              </MainCol>
-            </ActionRow>
-          </NoScrollWrapper>
-            <Pagewrapper>
-                <NavForm form={form}/>
-            </Pagewrapper>
-        </>
-    );
+    <AppLayoutWithContext
+      navBarProps={{title: title, visSkjemaliste: true}}
+      mainCol={<>
+      <Link className="knapp" to={editFormUrl}>Rediger skjema</Link>
+        <Hovedknapp onClick={() => onSave(form)}>Lagre skjema</Hovedknapp>
+        <Knapp onClick={() => onPublishClick(form)} spinner={publiserer}>
+        Publiser skjema
+        </Knapp>
+      </>}>
+      <NavForm form={form}/>
+    </AppLayoutWithContext>
+  );
 }
