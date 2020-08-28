87f4adcd41b60c7f685658e7dad50286e529eb79
Syver Enstad <syver.enstad@gmail.com>
2020-08-28 15:09:13 +0200

    WIP Meldinger ved workflow hendelser

--- bin/trigger-build-aborted.js
+++ bin/trigger-build-aborted.js
@@ -24,7 +24,33 @@ const pusher = new Pusher({
 const jsonString = fs.readFileSync(0, 'utf-8');
 const message = JSON.parse(jsonString);
 
-console.log('sending the following message', message);
 
-pusher.trigger('build-aborted', 'event', {commit: message.head_commit});
+const thisCommit = message.head_commit;
+const commitMessage = thisCommit.message;
+console.log('commit message', commitMessage);
+const publishCommitRe = /^Bump skjemapublisering dependency to navikt\/skjemapublisering-test.git#(?<gitHash>[0-9a-f]+)$/;
+const publishMessageResult = commitMessage.match(publishCommitRe);
 
+const packageJsonString = fs.readFileSync('package.json', 'utf-8');
+const packageJson = JSON.parse(packageJsonString);
+const skjemapubliseringsCommitUrl = packageJson.dependencies.skjemapublisering;
+
+const skjemapubliseringsUrlRe = /navikt\/skjemapublisering-test.git#(?<gitHash>[0-9a-f]+)$/
+const skjemapubliseringsCommitResult = skjemapubliseringsCommitUrl.match(skjemapubliseringsUrlRe);
+console.log('package json stuffs', skjemapubliseringsCommitResult);
+
+let event = 'other';
+if (publishMessageResult) {
+  event = 'publication';
+}
+
+
+const pusherMessage = {
+  'skjemautfyllerCommit': thisCommit,
+  'skjemapublisering': {
+    'commitUrl': skjemapubliseringsCommitUrl,
+    'commitHash': skjemapubliseringsCommitResult.groups.gitHash
+  },
+};
+console.log('sending the following message', pusherMessage);
+pusher.trigger('build-aborted', event, pusherMessage);
