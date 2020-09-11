f139ad5bc49843ccf72e784bb635a31314ec07f9
Sissel Fladby <sissel.fladby@nav.no>
2020-09-11 08:17:18 +0200

    Bruker html-element fremfor content for å lage tittel

--- src/Forms/NewFormPage.jsx
+++ src/Forms/NewFormPage.jsx
@@ -5,7 +5,7 @@ import { CreationFormMetadataEditor } from "../components/FormMetadataEditor";
 import cloneDeep from "lodash.clonedeep";
 import camelCase from "lodash/camelCase";
 import { AppLayoutWithContext } from "../components/AppLayout";
-import defaultTitleComponent from "../CustomFields/defaultTitleComponent.js";
+import defaultComponents from "../CustomFields/defaultForm.js";
 
 class NewFormPage extends Component {
   state = {
@@ -16,9 +16,7 @@ class NewFormPage extends Component {
       name: "",
       title: "",
       path: "",
-      components: [
-        defaultTitleComponent
-      ],
+      components: defaultComponents,
     },
   };
 
