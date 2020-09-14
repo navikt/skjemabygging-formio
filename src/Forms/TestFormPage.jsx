31c7258aa8aaabb5535c07634396ef80598bf887
Rune Rosseland <rune.rosseland@nav.no>
2020-09-14 14:16:10 +0200

    Oversettelser av feilmeldinger i egen fil

--- src/Forms/TestFormPage.jsx
+++ src/Forms/TestFormPage.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import NavForm from "../components/NavForm";
 import {Hovedknapp, Knapp} from "nav-frontend-knapper";
 import {AppLayoutWithContext} from "../components/AppLayout";
+import i18nData from "../i18nData";
 
 export function TestFormPage({onPublishClick, publiserer, editFormUrl, form, onSave }) {
   const title = `${form.title}`;
@@ -18,24 +19,7 @@ export function TestFormPage({onPublishClick, publiserer, editFormUrl, form, onS
       </>}>
       <NavForm form={form} options={{
         language: 'nb-NO',
-        i18n: {
-          'nb-NO': {
-            alertMessage: '{{label}}: {{message}}',
-            "error" : "Vennligst fiks følgende feil:",
-            "invalid_date" :"{{field}} er ikke en gyldig dato.",
-            "invalid_email" : "{{field}} må være en gyldig epost-adresse.",
-            "invalid_regex" : "{{field}} passer ikke til uttrykket {{regex}}.",
-            "mask" : "Dette er ikke et gyldig {{field}}.",
-            "max" : "{{field}} kan ikke være større enn {{max}}.",
-            "maxLength" : "{{field}} må være kortere enn {{length}} tegn.",
-            "min" : "{{field}} kan ikke være mindre enn {{min}}.",
-            "minLength" : "{{field}} må være lengre enn {{length}} tegn.",
-            "next" : "Neste",
-            "pattern" : "{{field}} stemmer ikke med {{pattern}}",
-            "previous" : "Forrige",
-            "required" : "Du må fylle ut dette feltet"
-          }
-        }
+        i18n: i18nData
       }}/>
     </AppLayoutWithContext>
   );
