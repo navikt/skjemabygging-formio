141e6b9511ab0247673d1cdd69d30c31c9ec3c0f
Rune Rosseland <rune.rosseland@nav.no>
2020-09-14 14:06:14 +0200

    Feilemldinger på norsk i egen fil

--- src/components/FormPage.js
+++ src/components/FormPage.js
@@ -2,6 +2,7 @@ import {useParams, useHistory} from "react-router-dom";
 import {Sidetittel} from "nav-frontend-typografi";
 import {Form} from "react-formio";
 import React from "react";
+import i18nData from "../i18nData";
 
 
 export const FormPage = ({forms, setSubmission}) => {
@@ -22,25 +23,9 @@ export const FormPage = ({forms, setSubmission}) => {
         options={{
           readOnly: false,
           language: 'nb-NO',
-          i18n: {
-            'nb-NO': {
-              alertMessage: '{{label}}: {{message}}',
-              "error" : "Vennligst fiks følgende feil:",
-              "invalid_date" :"{{field}} er ikke en gyldig dato.",
-              "invalid_email" : "{{field}} må være en gyldig epost-adresse.",
-              "invalid_regex" : "{{field}} passer ikke til uttrykket {{regex}}.",
-              "mask" : "Dette er ikke et gyldig {{field}}.",
-              "max" : "{{field}} kan ikke være større enn {{max}}.",
-              "maxLength" : "{{field}} må være kortere enn {{length}} tegn.",
-              "min" : "{{field}} kan ikke være mindre enn {{min}}.",
-              "minLength" : "{{field}} må være lengre enn {{length}} tegn.",
-              "next" : "Neste",
-              "pattern" : "{{field}} stemmer ikke med {{pattern}}",
-              "previous" : "Forrige",
-              "required" : "Du må fylle ut dette feltet"
-            }
+          i18n: i18nData
           }
-        }}
+        }
         onSubmit={(submission) => {
           setSubmission({[form.path]: submission});
           history.push(
---
+++ src/i18nData.js
@@ -0,0 +1,45 @@
+export default {
+  'nb-NO': {
+    //Generelle feilmeldinger
+    alertMessage: '{{label}}: {{message}}',
+    error: "Vennligst fiks følgende feil:",
+    invalid_email: "{{field}} må være en gyldig epost-adresse.",
+    invalid_regex: "{{field}} passer ikke til uttrykket {{regex}}.",
+    mask: "Dette er ikke et gyldig {{field}}.",
+    max: "{{field}} kan ikke være større enn {{max}}.",
+    min: "{{field}} kan ikke være mindre enn {{min}}.",
+    maxLength: "{{field}} må være kortere enn {{length}} tegn.",
+    minLength: "{{field}} må være lengre enn {{length}} tegn.",
+    pattern: "{{field}} stemmer ikke med {{pattern}}",
+    required: "Du må fylle ut dette feltet",
+
+    //Grensesnitt / navigering
+    previous: "Forrige",
+    next: "Neste",
+    cancel: 'Avbryt',
+    confirmCancel: 'Er du sikker på at du vil avbryte?',
+
+    //Dato / tid
+    invalid_date: "{{field}} er ikke en gyldig dato.",
+    invalid_day: '{{field}} er ikke en gyldig dag.', // eslint-disable-line camelcase
+    maxDate: '{{field}} kan ikke inneholde dato etter {{- maxDate}}',
+    minDate: '{{field}} kan ikke inneholde dato før {{- minDate}}',
+    maxYear: '{{field}} kan ikke være senere enn {{maxYear}}',
+    minYear: '{{field}} kan ikke være før {{minYear}}',
+    month: 'Måned',
+    day: 'Dag',
+    year: 'År',
+    january: 'Januar',
+    february: 'Februar',
+    march: 'Mars',
+    april: 'April',
+    may: 'Mai',
+    june: 'Juni',
+    july: 'Juli',
+    august: 'August',
+    september: 'September',
+    october: 'Oktober',
+    november: 'November',
+    december: 'Desember',
+  }
+}
