a5d0eca029531244f847918b6b58a9be8a36bac3
Sissel Fladby <sissel.fladby@nav.no>
2020-09-15 16:08:47 +0200

    Norsk språk på datepicker - mystisk bruk av moments

--- src/UnauthenticatedApp.jsx
+++ src/UnauthenticatedApp.jsx
@@ -3,12 +3,16 @@ import NavForm from "./components/NavForm";
 import React from "react";
 import {useAuth} from "./context/auth-context";
 import {Pagewrapper} from "./Forms/components";
+import i18nData from "./i18nData";
 
 const UnauthenticatedApp = ({ projectURL }) => {
   const { login } = useAuth();
   return (
     <>
-      <NavBar title={"Skjemabygger"} />
+      <NavBar title={"Skjemabygger"} options={{
+        language: 'nb-NO',
+        i18n: i18nData
+      }} />
       <Pagewrapper><NavForm src={`${projectURL}/user/login`} onSubmitDone={(user) => login(user)} /></Pagewrapper>
     </>
   );
