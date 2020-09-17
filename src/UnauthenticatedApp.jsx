1a5812cb93c186b9ad2c2cfaa12a8b9131e63f7e
Sissel Fladby <sissel.fladby@nav.no>
2020-09-17 11:28:37 +0200

    Fjernet unødvendig import (fordi den lå et annet sted) og sendte inn options til NavForm-innloggingsskjema

--- src/UnauthenticatedApp.jsx
+++ src/UnauthenticatedApp.jsx
@@ -1,19 +1,25 @@
 import { NavBar } from "./components/NavBar";
 import NavForm from "./components/NavForm";
 import React from "react";
-import {useAuth} from "./context/auth-context";
-import {Pagewrapper} from "./Forms/components";
+import { useAuth } from "./context/auth-context";
+import { Pagewrapper } from "./Forms/components";
 import i18nData from "./i18nData";
 
 const UnauthenticatedApp = ({ projectURL }) => {
   const { login } = useAuth();
   return (
     <>
-      <NavBar title={"Skjemabygger"} options={{
-        language: 'nb-NO',
-        i18n: i18nData
-      }} />
-      <Pagewrapper><NavForm src={`${projectURL}/user/login`} onSubmitDone={(user) => login(user)} /></Pagewrapper>
+      <NavBar title={"Skjemabygger"} />
+      <Pagewrapper>
+        <NavForm
+          src={`${projectURL}/user/login`}
+          onSubmitDone={(user) => login(user)}
+          options={{
+            language: "nb-NO",
+            i18n: i18nData,
+          }}
+        />
+      </Pagewrapper>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import Pusher from "pusher-js";
 
-require('../node_modules/moment/locale/nb.js'); // For datovelger
-
 const projectURL = process.env.REACT_APP_FORMIO_PROJECT_URL || "https://protected-island-44773.herokuapp.com";
 
 const store = { forms: null };
