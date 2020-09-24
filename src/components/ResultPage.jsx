cf50676f62a93a0774ec72ece8e77b83ba54a729
Sissel Fladby <sissel.fladby@nav.no>
2020-09-24 16:51:23 +0200

    - Lagt på norsk språk på oppsummeringsformet - Disabler "Gå videre" inntil man har lastet ned PDF for å forhindre at du "mister" innhold ved å gå videre for tidlig

--- src/components/ResultPage.jsx
+++ src/components/ResultPage.jsx
@@ -1,12 +1,14 @@
 import { Innholdstittel, Normaltekst, Sidetittel } from "nav-frontend-typografi";
 import { Form } from "react-formio";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Panel from "nav-frontend-paneler";
 import styled from "@material-ui/styles/styled";
 import Hovedknapp from "nav-frontend-knapper";
+import i18nData from "../i18nData";
 
 export function ResultPage({ form, submission }) {
+  const [kanGaVidere, setKanGaVidere] = useState(false);
   const resultForm = form.display === "wizard" ? { ...form, display: "form" } : form;
   return (
     <ResultContent>
@@ -15,7 +17,12 @@ export function ResultPage({ form, submission }) {
         <Innholdstittel>1. Se over svarene dine</Innholdstittel>
         <Normaltekst>Her kan du blablabla...</Normaltekst>
         <form action="/skjema/pdf-form" method="post" acceptCharset="utf-8" target="_blank">
-          <Form key="2" form={resultForm} options={{ readOnly: true }} submission={submission} />
+          <Form
+            key="2"
+            form={resultForm}
+            options={{ readOnly: true, language: "nb-NO", i18n: i18nData }}
+            submission={submission}
+          />
           <textarea hidden={true} name="submission" readOnly={true} required value={JSON.stringify(submission)} />
           <textarea hidden={true} name="form" readOnly={true} required value={JSON.stringify(form)} />
         </form>
@@ -26,12 +33,12 @@ export function ResultPage({ form, submission }) {
       <ResultPanel border>
         <Innholdstittel>2. Last ned som PDF</Innholdstittel>
         <Normaltekst>Lorem ipsum sit amet..</Normaltekst>
-        <input className="knapp" type="submit" value="Last ned PDF" />
+        <input className="knapp" type="submit" value="Last ned PDF" onClick={() => setKanGaVidere(true)} />
       </ResultPanel>
       <ResultPanel border>
         <Innholdstittel>3. Last opp i PDF-opplastingstjeneste</Innholdstittel>
         <Normaltekst>Lorem ipsum sit amet..</Normaltekst>
-        <Hovedknapp>Gå videre</Hovedknapp>
+        <Hovedknapp disabled={!kanGaVidere}>Gå videre</Hovedknapp>
       </ResultPanel>
     </ResultContent>
   );
