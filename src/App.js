67c2cfe4f6c28da72937797b929bc83da435467e
Sissel Fladby <sissel.fladby@nav.no>
2020-09-24 16:05:34 +0200

    - Redigerknapp på oppsummeringssiden for å kunne gå tilbake og redigere besvarelsen sin - Organisert oppsummeringen i panel som en skisse til mulig design - NAV-knappestyling

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
-import React, {useState} from "react";
-import {Route, Switch} from "react-router-dom";
-import {Components} from "react-formio";
+import React, { useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import { Components } from "react-formio";
 import components from "./custom";
 import "nav-frontend-typografi-style";
 import "formiojs/dist/formio.full.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import navdesign from "template";
-import {Formio} from "formiojs";
-import {FormPage} from "./components/FormPage";
-import {styled} from "@material-ui/styles";
-import {ResultPage} from "./components/ResultPage";
-import {AllForms} from "./components/AllForms";
+import { Formio } from "formiojs";
+import { FormPage } from "./components/FormPage";
+import { styled } from "@material-ui/styles";
+import { ResultPage } from "./components/ResultPage";
+import { AllForms } from "./components/AllForms";
 
 Components.setComponents(components);
 Formio.use(navdesign);
 
-
-function App({forms, className}) {
+function App({ forms, className }) {
   const [submission, setSubmission] = useState({});
   return (
     <div className={className}>
-      <MainContent>
       <Switch>
         <Route exact path="/">
           <AllForms forms={forms} />
         </Route>
-        <Route
-          exact
-          path="/:formpath">
-          <FormPage forms={forms} setSubmission={setSubmission} />
+        <Route exact path="/:formpath">
+          <FormPage forms={forms} setSubmission={setSubmission} submission={submission} />
         </Route>
         <Route
           path="/:formpath/result"
           render={(routeProps) => {
             const formPath = routeProps.match.params.formpath;
-            const form = forms.find(form => form.path === formPath);
+            const form = forms.find((form) => form.path === formPath);
             if (!form) {
               return <h1>Skjemaet {formPath} finnes ikke</h1>;
             }
@@ -42,22 +38,12 @@ function App({forms, className}) {
           }}
         />
       </Switch>
-      </MainContent>
     </div>
   );
 }
 
-const MainContent = styled("div") ({
-  maxWidth: 600
-})
-
 export default styled(App)({
-  display: "flex",
-  //flexDirection: "column",
-  //alignItems: "end",
-  justifyContent: "center",
-  //maxWidth: 600,
-  padding: "2rem"
-  }
-
-  );
+  margin: "0 auto",
+  padding: "2rem",
+  maxWidth: "800px",
+});
