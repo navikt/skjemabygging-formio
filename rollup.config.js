dcbe1c2e7e585e074b2ebd96f45337099a1b4bb3
Syver Enstad <syver.enstad@gmail.com>
2020-10-02 16:12:12 +0200

    Publish NavForm from the skjemabygging as a package

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,32 @@
-import babel from '@rollup/plugin-babel';
+import babel from "@rollup/plugin-babel";
 
-import pkg from './package.json';
+import pkg from "./package.json";
 
 export default [
-	// CommonJS (for Node) and ES module (for bundlers) build.
-	// (We could have three entries in the configuration array
-	// instead of two, but it's quicker to generate multiple
-	// builds from a single configuration where possible, using
-	// an array for the `output` option, where we can specify
-	// `file` and `format` for each target)
-	{
-		input: 'lib/index.js',
-		external: ['react', 'react-dom'],
-		output: [
-			{ file: pkg.module, format: 'es' }
-		],
-		plugins: babel({
-			exclude: 'node_modules/**',
-			presets: ['@babel/preset-react']
-		})
-	}
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: "lib/index.js",
+    external: [
+      "react",
+      "react-dom",
+      "prop-types",
+      "eventemitter2",
+      "formiojs",
+      "formiojs/components",
+      "nav-frontend-skjema-style",
+      "template",
+    ],
+    output: [{ file: pkg.module, format: "es" }],
+    plugins: babel({
+      exclude: "node_modules/**",
+      babelHelpers: "bundled", // documentation seems to indicate we should rather be using 'runtime' as the value here
+      plugins: ["@babel/plugin-proposal-class-properties"],
+      presets: ["@babel/preset-react"],
+    }),
+  },
 ];
