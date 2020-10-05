0c7023d6f38348aa84bbf14afe00d778b3e8b1cd
Vegard Haugstvedt <it.vegard@gmail.com>
2020-10-05 09:40:50 +0200

    Set page title as <form.title> | www.nav.no when loading FormPage

--- src/components/FormPage.js
+++ src/components/FormPage.js
@@ -1,12 +1,16 @@
+import React, { useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import { Sidetittel } from "nav-frontend-typografi";
 import NavForm from "./NavForm";
-import React from "react";
 
 export const FormPage = ({ forms, submission, setSubmission }) => {
   const params = useParams();
   const history = useHistory();
   const form = forms.find((form) => form.path === params.formpath);
+  const formTitle = form ? form.title : "";
+  useEffect(() => {
+    document.title = `${formTitle} | www.nav.no`;
+  }, [formTitle]);
   if (!form) {
     return <h1>Finner ikke skjemaet '{params.formpath}'</h1>;
   }
