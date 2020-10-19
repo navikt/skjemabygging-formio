5021e0a6084f4fe5cd7168ab7dc9657e102c5fd5
Syver Enstad <syver.enstad@gmail.com>
2020-10-19 16:53:43 +0200

    Provoser fram build

--- server/commit_version.js
+++ server/commit_version.js
@@ -5,12 +5,13 @@ import { buildDirectoryIndexHtml } from "./context.js";
 const { JSDOM } = jsdom;
 
 export function gitVersionFromIndexHtml(htmlString) {
-  const {document} = new JSDOM(htmlString).window;
+  const { document } = new JSDOM(htmlString).window;
   const gitVersionMeta = document.querySelector('meta[name="git-version"]');
-  return gitVersionMeta.getAttribute('content');
+  return gitVersionMeta.getAttribute("content");
 }
 
 export function gitVersionFromBuild() {
   const indexHtml = fs.readFileSync(buildDirectoryIndexHtml);
+  console.log("got here version from build");
   return gitVersionFromIndexHtml(indexHtml);
 }
