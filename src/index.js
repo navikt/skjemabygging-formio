62b4261ebf66536618f7c9867b93179d403ab445
Sissel Fladby <sisfla@gmail.com>
2020-10-22 11:36:18 +0200

    - Fetch config at top level before rendering React - Adapting AppContext to receiving data from the top level config fetch

--- src/index.js
+++ src/index.js
@@ -7,20 +7,35 @@ import { BrowserRouter } from "react-router-dom";
 import { forms } from "skjemapublisering";
 import { AppConfigProvider } from "./configContext";
 
-if (process.env.REACT_APP_SENTRY_DSN) {
-  Sentry.init({ dsn: process.env.REACT_APP_SENTRY_DSN });
+try {
+  fetch("/skjema/config")
+    .then((config) => config.json())
+    .then((json) => {
+      if (json.REACT_APP_SENTRY_DSN) {
+        Sentry.init({ dsn: json.REACT_APP_SENTRY_DSN });
+      }
+      renderReact(json.NAIS_CLUSTER_NAME);
+    });
+} catch {
+  if (process.env.NODE_ENV === "development") {
+    renderReact();
+  } else {
+    console.error("Could not fetch config from server");
+  }
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <AppConfigProvider>
-      <BrowserRouter basename="/fyllut">
-        <App forms={forms} />
-      </BrowserRouter>
-    </AppConfigProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+function renderReact(naisClusterName) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <AppConfigProvider naisClusterName={naisClusterName}>
+        <BrowserRouter basename="/fyllut">
+          <App forms={forms} />
+        </BrowserRouter>
+      </AppConfigProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
