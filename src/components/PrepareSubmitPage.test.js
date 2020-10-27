76e9c5776ecd23316460cb0e24ae1f9ad6c44210
it-vegard <it.vegard@gmail.com>
2020-10-27 14:37:06 +0100

    Updated test for disabled submit button on the final step to work with new flow.

---
+++ src/components/PrepareSubmitPage.test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { AppConfigProvider } from "../configContext";
+import { PrepareSubmitPage } from "./PrepareSubmitPage";
+
+test("Gå videre (til dokumentinnsending) er ikke tillatt før brukeren har krysset av på at de har lest instruksjonene", () => {
+  render(
+    <AppConfigProvider>
+      <BrowserRouter>
+        <PrepareSubmitPage form={{ title: "Test form" }} submission={{}} />
+      </BrowserRouter>
+    </AppConfigProvider>
+  );
+  const harLestVilkaarInput = screen.getByLabelText("Jeg har lest instruksjonene");
+  expect(harLestVilkaarInput).not.toBeChecked();
+  const mustConfirmUserHasReadInstructionsWarningBeforeConfirmation = screen.getByText(
+    "Du må bekrefte at du har lest instruksjonene over før du kan gå videre."
+  );
+  expect(mustConfirmUserHasReadInstructionsWarningBeforeConfirmation).not.toBeUndefined();
+  harLestVilkaarInput.click();
+  const mustConfirmUserHasReadInstructionsWarningAfterConfirmation = screen.queryAllByText(
+    "Du må bekrefte at du har lest instruksjonene over før du kan gå videre."
+  );
+  expect(mustConfirmUserHasReadInstructionsWarningAfterConfirmation).toHaveLength(0);
+  const nextButton = screen.getByRole("link", { name: "Gå videre" });
+  harLestVilkaarInput.click(); // Remove confirmation
+  nextButton.click(); // Try to progress
+  const mustConfirmUserHasReadInstructionsWarningAfterConfirmationIsRemoved = screen.getByText(
+    "Du må bekrefte at du har lest instruksjonene over før du kan gå videre."
+  );
+  expect(mustConfirmUserHasReadInstructionsWarningAfterConfirmationIsRemoved).not.toBeUndefined();
+});
