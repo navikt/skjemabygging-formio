23f8bf3428f82ccebecd0b6e2ee59fdc6d158678
it-vegard <it.vegard@gmail.com>
2020-11-04 10:21:28 +0100

    scrollIntoView is apparently not implemented in whatever browser the tests run in.

--- src/util/focus-management.js
+++ src/util/focus-management.js
@@ -1,10 +1,12 @@
 export function scrollToAndSetFocus(selector, alignTarget = "center") {
   const targetTag = document.querySelector(selector);
   if (targetTag) {
-    targetTag.scrollIntoView({
-      behavior: "smooth",
-      block: alignTarget,
-    });
+    if (typeof targetTag.scrollIntoView === "function") {
+      targetTag.scrollIntoView({
+        behavior: "smooth",
+        block: alignTarget,
+      });
+    }
     targetTag.focus({ preventScroll: true });
   }
 }
