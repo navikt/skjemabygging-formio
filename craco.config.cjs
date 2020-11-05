225d958bb5873c7ebc761eb867fe9c0db7cec249
Syver Enstad <syver.enstad@gmail.com>
2020-11-05 16:54:23 +0100

    Configure webpack to use the same react always
    Uses the technique described here:
    https://medium.com/@harshverma04111989/creating-monorepo-using-lerna-dd431d0db072

--- craco.config.cjs
+++ craco.config.cjs
@@ -1,5 +1,13 @@
-const CracoLessPlugin = require("craco-less")
+const CracoLessPlugin = require("craco-less");
+
+const path = require("path");
+const resolvePackage = (relativePath) => path.resolve(__dirname, relativePath);
 
 module.exports = {
   plugins: [{ plugin: CracoLessPlugin }],
+  webpack: {
+    alias: {
+      react: resolvePackage("./node_modules/react"),
+    },
+  },
 };
