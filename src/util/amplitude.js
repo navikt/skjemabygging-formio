9ff95a4c307f57d8aae785664802bad7b71dec4c
it-vegard <it.vegard@gmail.com>
2020-12-18 14:01:18 +0100

    Removed unneccessary checks for whether amplitude (imported using require) and form (passed as argument) exists.

--- src/util/amplitude.js
+++ src/util/amplitude.js
@@ -1,15 +1,13 @@
 const amplitude = require("amplitude-js");
 
 export const initAmplitude = () => {
-  if (amplitude) {
-    amplitude.getInstance().init("default", "", {
-      apiEndpoint: "amplitude.nav.no/collect-auto",
-      saveEvents: false,
-      includeUtm: true,
-      includeReferrer: true,
-      platform: window.location.toString(),
-    });
-  }
+  amplitude.getInstance().init("default", "", {
+    apiEndpoint: "amplitude.nav.no/collect-auto",
+    saveEvents: false,
+    includeUtm: true,
+    includeReferrer: true,
+    platform: window.location.toString(),
+  });
 };
 
 function createEventData(form, customProperties = {}) {
@@ -23,9 +21,7 @@ function createEventData(form, customProperties = {}) {
 export function logAmplitudeEvent(eventName, eventData) {
   setTimeout(() => {
     try {
-      if (amplitude) {
-        amplitude.getInstance().logEvent(eventName, eventData);
-      }
+      amplitude.getInstance().logEvent(eventName, eventData);
     } catch (error) {
       console.error(error);
     }
@@ -33,21 +29,17 @@ export function logAmplitudeEvent(eventName, eventData) {
 }
 
 export function loggSkjemaApnet(form) {
-  if (form) {
-    logAmplitudeEvent("skjema åpnet", createEventData(form));
-    console.log("Skjema åpnet");
-  }
+  logAmplitudeEvent("skjema åpnet", createEventData(form));
+  console.log("Skjema åpnet");
 }
 
 export function loggSkjemaStartet(form) {
-  if (form) {
-    logAmplitudeEvent("skjema startet", createEventData(form));
-    console.log("Skjema startet");
-  }
+  logAmplitudeEvent("skjema startet", createEventData(form));
+  console.log("Skjema startet");
 }
 
 export function loggSkjemaSporsmalBesvart(form, sporsmal, id, svar, pakrevd) {
-  if (form && sporsmal && svar) {
+  if (sporsmal && svar) {
     logAmplitudeEvent(
       "skjemaspørsmål besvart",
       createEventData(form, {
@@ -61,31 +53,21 @@ export function loggSkjemaSporsmalBesvart(form, sporsmal, id, svar, pakrevd) {
 }
 
 export function loggSkjemaStegFullfort(form, steg) {
-  if (form) {
-    logAmplitudeEvent("skjemasteg fullført", createEventData(form, { steg }));
-    console.log("Fullført steg " + steg);
-  }
+  logAmplitudeEvent("skjemasteg fullført", createEventData(form, { steg }));
+  console.log("Fullført steg " + steg);
 }
 
 export function loggSkjemaFullfort(form, innsendingsType) {
-  if (form) {
-    logAmplitudeEvent("skjema fullført", createEventData(form, { innsendingsType: innsendingsType }));
-    console.log("Fullført skjema");
-  } else {
-    console.log("Form is missing");
-  }
+  logAmplitudeEvent("skjema fullført", createEventData(form, { innsendingsType: innsendingsType }));
+  console.log("Fullført skjema");
 }
 
 export function loggSkjemaValideringFeilet(form) {
+  logAmplitudeEvent("skjemavalidering feilet", createEventData(form));
   console.log("Skjemavalidering feilet");
-  if (form) {
-    logAmplitudeEvent("skjemavalidering feilet", createEventData(form));
-  }
 }
 
 export function loggSkjemaInnsendingFeilet(form) {
+  logAmplitudeEvent("skjemainnsending feilet", createEventData(form));
   console.log("Skjemainnsending feilet");
-  if (form) {
-    logAmplitudeEvent("skjemainnsending feilet", createEventData(form));
-  }
 }
