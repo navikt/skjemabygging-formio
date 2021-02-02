0df23cd42ff28cf96aa69031eff325e0d104ebc0
Vegard Haugstvedt <it.vegard@gmail.com>
2021-02-02 14:10:37 +0100

    Style datagrid component and make it accessible

--- src/components/NavForm.jsx
+++ src/components/NavForm.jsx
@@ -220,9 +220,25 @@ export default styled(withAmplitudeHooks(NavForm))({
     content: '""',
   },
   "& .skjemaelement__label:not(.field-required)::after": {
-    content: '"(valgfritt)"',
+    content: '" (valgfritt)"',
   },
   "& .checkboks[required] + .skjemaelement__label::after": {
     content: '""',
+  },
+  "& .data-grid__row": {
+    border: "2px solid white",
+    borderRadius: "7px",
+    marginBottom: "1rem",
+    padding: "1.5rem 2rem 0",
+
+    "& .knapp--fjern": {
+      marginBottom: "1.5rem",
+    },
+  },
+  "& .formio-error-wrapper, & .formio-warning-wrapper": {
+    padding: "0",
+  },
+  "& .data-grid__row > .skjemagruppe__legend": {
+    float: "left",
   },
 });
