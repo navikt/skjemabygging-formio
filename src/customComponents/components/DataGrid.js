12a162ea75843a42f057896c1fff576cbbf26744
Vegard Haugstvedt <it.vegard@gmail.com>
2021-02-03 10:32:01 +0100

    Simplify DataGrid edit form extension even more

--- src/customComponents/components/DataGrid.js
+++ src/customComponents/components/DataGrid.js
@@ -1,15 +1,9 @@
 import DataGrid from "formiojs/components/datagrid/DataGrid";
-import BaseComponent from "formiojs/components/_classes/component/Component.form";
+import DataGridEditForm from "formiojs/components/datagrid/DataGrid.form";
 import DataGridDisplayEditForm from "formiojs/components/datagrid/editForm/DataGrid.edit.display";
-import DataGridDataEditForm from "formiojs/components/datagrid/editForm/DataGrid.edit.data";
-import DataGridValidationEditForm from "formiojs/components/datagrid/editForm/DataGrid.edit.validation";
-import DataGridAPIEditForm from "formiojs/components/_classes/component/editForm/Component.edit.api";
-import DataGridConditionalEditForm from "formiojs/components/_classes/component/editForm/Component.edit.conditional";
-import DataGridLogicEditForm from "formiojs/components/_classes/component/editForm/Component.edit.logic";
-import DataGridLayoutEditForm from "formiojs/components/_classes/component/editForm/Component.edit.layout";
 
 DataGrid.editForm = () => {
-  return BaseComponent([
+  return DataGridEditForm([
     {
       label: "Display",
       key: "display",
@@ -21,41 +15,10 @@ DataGrid.editForm = () => {
           key: "removeAnother",
           tooltip: "Set the text of the Remove button.",
           placeholder: "Remove",
-          weight: 410,
+          weight: 412,
           input: true,
         },
       ],
-    },
-    {
-      label: "Data",
-      key: "data",
-      components: DataGridDataEditForm,
-    },
-    {
-      label: "Validation",
-      key: "validation",
-      components: DataGridValidationEditForm,
-    },
-    {
-      label: "API",
-      key: "api",
-      components: DataGridAPIEditForm,
-    },
-    {
-      label: "Conditional",
-      key: "conditional",
-      components: DataGridConditionalEditForm,
-    },
-
-    {
-      label: "Logic",
-      key: "logic",
-      components: DataGridLogicEditForm,
-    },
-    {
-      label: "Layout",
-      key: "layout",
-      components: DataGridLayoutEditForm,
     },
   ]);
 };
