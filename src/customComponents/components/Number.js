919ba743c37712e88368659bef6cf2a8978dfd22
Vegard Haugstvedt <it.vegard@gmail.com>
2021-02-11 15:34:20 +0100

    Share fieldSize config in a fields.js shared file

--- src/customComponents/components/Number.js
+++ src/customComponents/components/Number.js
@@ -1,33 +1,14 @@
 import Number from "formiojs/components/number/Number";
 import NumberEditForm from "formiojs/components/number/Number.form";
 import NumberDisplayEditForm from "formiojs/components/number/editForm/Number.edit.display";
+import { fieldSizeField } from "./fields";
 
 Number.editForm = () => {
   return NumberEditForm([
     {
       label: "Display",
       key: "display",
-      components: [
-        ...NumberDisplayEditForm,
-        {
-          type: "select",
-          input: true,
-          label: "Feltstørrelse",
-          key: "fieldSize",
-          dataSrc: "values",
-          data: {
-            values: [
-              { label: "Standard", value: "input--xxl" },
-              { label: "XL", value: "input--xl" },
-              { label: "L", value: "input--l" },
-              { label: "M", value: "input--m" },
-              { label: "S", value: "input--s" },
-              { label: "XS", value: "input--xs" },
-            ],
-          },
-          weight: 1,
-        },
-      ],
+      components: [...NumberDisplayEditForm, fieldSizeField],
     },
   ]);
 };
--- src/customComponents/components/TextField.js
+++ src/customComponents/components/TextField.js
@@ -1,33 +1,14 @@
 import TextField from "formiojs/components/textfield/TextField";
 import TextFieldEditForm from "formiojs/components/textfield/TextField.form";
 import TextFieldDisplayEditForm from "formiojs/components/textfield/editForm/TextField.edit.display";
+import { fieldSizeField } from "./fields";
 
 TextField.editForm = () => {
   return TextFieldEditForm([
     {
       label: "Display",
       key: "display",
-      components: [
-        ...TextFieldDisplayEditForm,
-        {
-          type: "select",
-          input: true,
-          label: "Feltstørrelse",
-          key: "fieldSize",
-          dataSrc: "values",
-          data: {
-            values: [
-              { label: "Standard", value: "input--xxl" },
-              { label: "XL", value: "input--xl" },
-              { label: "L", value: "input--l" },
-              { label: "M", value: "input--m" },
-              { label: "S", value: "input--s" },
-              { label: "XS", value: "input--xs" },
-            ],
-          },
-          weight: 1,
-        },
-      ],
+      components: [...TextFieldDisplayEditForm, fieldSizeField],
     },
   ]);
 };
