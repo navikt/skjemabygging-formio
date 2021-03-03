219a11be62adfd797cacaa515e3a6d2ae1cbb7c0
Wenqi Lin <wenqi.lin@nav.no>
2021-03-03 11:10:12 +0100

    delete unused function

--- src/util/focus-management.js
+++ src/util/focus-management.js
@@ -9,18 +9,3 @@ export function scrollToAndSetFocus(selector, alignTarget = "center") {
   });
   targetTag.focus({ preventScroll: true });
 }
-
-export function scrollToMainAndFocusOnForm(scrollToSelector, focusOnSelector, scrollToAlignTarget = "center") {
-  const scrollToTag = document.querySelector(scrollToSelector);
-  const focusOnTag = document.querySelector(focusOnSelector);
-
-  if (!scrollToTag || !focusOnTag) return;
-
-  scrollToTag.scrollIntoView({
-    behavior: "smooth",
-    block: scrollToAlignTarget,
-  });
-
-  focusOnTag.tabIndex = -1;
-  focusOnTag.focus({ preventScroll: true });
-}
