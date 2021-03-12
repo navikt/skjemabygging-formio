ba865fb4b56285b34077e2f0a4720861e416e35c
Vegard Haugstvedt <it.vegard@gmail.com>
2021-03-12 10:45:01 +0100

    Close Select when escape key is pressed
    Reset focus to the open/close button after

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import { Collapse, Expand } from "@navikt/ds-icons";
+
+const closeOnEscape = (event, close, buttonRef) => {
+  if (event.key === "Escape") {
+    close();
+    buttonRef.current.focus();
+  }
+};
 
 const Select = ({ label, className, onChange, options }) => {
+  const buttonRef = useRef(null);
   const [showItems, setShowItems] = useState(false);
   return (
-    <nav className={className}>
-      <button className="select-button" onClick={() => setShowItems(!showItems)}>
+    <nav className={className} onKeyUp={(event) => closeOnEscape(event, () => setShowItems(false), buttonRef)}>
+      <button className="select-button" onClick={() => setShowItems(!showItems)} ref={buttonRef}>
         {label}
         {showItems ? <Collapse className="select__chevron" /> : <Expand className="select__chevron" />}
       </button>
@@ -20,7 +29,7 @@ const Select = ({ label, className, onChange, options }) => {
                   onChange(value);
                 }}
               >
-              {optionLabel}
+                {optionLabel}
               </a>
             </li>
           ))}
