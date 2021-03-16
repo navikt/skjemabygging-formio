ad4a6edd68f62af08bc300f54651ef26ef7db5ce
Vegard Haugstvedt <it.vegard@gmail.com>
2021-03-16 11:22:50 +0100

    Extract all texts for the form and show input fields for them

--- src/translations/TranslationsByFormPage.jsx
+++ src/translations/TranslationsByFormPage.jsx
@@ -2,9 +2,45 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import { SletteKnapp } from "../Forms/components";
 import { AppLayoutWithContext } from "../components/AppLayout";
+import { flattenComponents } from "../util/forsteside";
+import { Input } from "nav-frontend-skjema";
+import { Sidetittel } from "nav-frontend-typografi";
+
+const getAllTextsForForm = (form) =>
+  flattenComponents(form.components)
+    .map(({ content, title, label, html, type, values }) => ({
+      title,
+      label: ["panel", "htmlelement", "content"].indexOf(type) === -1 ? label : undefined,
+      html,
+      values: values ? values.map((value) => value.label) : undefined,
+      content,
+    }))
+    .reduce((allTextsForForm, component) => {
+      return [
+        ...allTextsForForm,
+        ...Object.keys(component)
+          .filter((key) => component[key] !== undefined)
+          .reduce((textsForComponent, key) => {
+            console.log("Key", component[key]);
+            if (key === "values") {
+              return [...textsForComponent, ...component[key].map((value) => ({ text: value, type: "text" }))];
+            } else if (key === "html" || key === "content") {
+              return [...textsForComponent, { text: component[key], type: "textarea" }];
+            } else {
+              return [...textsForComponent, { text: component[key], type: "text" }];
+            }
+          }, []),
+      ];
+    }, []);
 
 const TranslationsByFormPage = ({ deleteLanguage, form, resourceId }) => {
   const history = useHistory();
+  const {
+    title,
+    //path,
+    properties: { skjemanummer },
+  } = form;
+  const flattenedComponents = getAllTextsForForm(form);
   return (
     <AppLayoutWithContext
       navBarProps={{
@@ -22,8 +58,14 @@ const TranslationsByFormPage = ({ deleteLanguage, form, resourceId }) => {
         </SletteKnapp>
       }
     >
-      <h1>Hello World!</h1>
-      <pre>{JSON.stringify(form, null, 2)}</pre>
+      <Sidetittel className="margin-bottom-large">
+        {skjemanummer} {title}
+      </Sidetittel>
+      <form>
+        {flattenedComponents.map(({ text, type }) => (
+          <Input className="margin-bottom-default" label={text} type={type} />
+        ))}
+      </form>
     </AppLayoutWithContext>
   );
 };
