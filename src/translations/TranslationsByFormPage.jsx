df80bd798c817bc9ea953faffa25965c0167a06d
wen7i <wenqi.lin@nav.no>
2021-03-17 09:08:04 +0100

    remove unsed import

--- src/translations/TranslationsByFormPage.jsx
+++ src/translations/TranslationsByFormPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { SletteKnapp } from "../Forms/components";
 import { AppLayoutWithContext } from "../components/AppLayout";
