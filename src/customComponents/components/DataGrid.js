87d870823e1c8fc3725ae7ff636708fa7c6d0e73
Vegard Haugstvedt <it.vegard@gmail.com>
2021-04-06 13:42:24 +0200

    Split navDataGrid from Formio DataGrid
    Only make NavDataGrid selectable in the Form Builder

--- src/customComponents/components/DataGrid.js
+++ src/customComponents/components/DataGrid.js
@@ -1,27 +1,42 @@
 import DataGrid from "formiojs/components/datagrid/DataGrid";
 import DataGridEditForm from "formiojs/components/datagrid/DataGrid.form";
 import DataGridDisplayEditForm from "formiojs/components/datagrid/editForm/DataGrid.edit.display";
+import FormioReactComponent from "../FormioReactComponent";
+import FormBuilderOptions from "../../Forms/FormBuilderOptions";
 
-DataGrid.editForm = (...extend) => {
-  return DataGridEditForm([
-    {
-      label: "Display",
-      key: "display",
-      components: [
-        ...DataGridDisplayEditForm,
-        {
-          type: "textfield",
-          label: "Remove Text",
-          key: "removeAnother",
-          tooltip: "Set the text of the Remove button.",
-          placeholder: "Remove",
-          weight: 412,
-          input: true,
-        },
-      ],
-    },
-    ...extend,
-  ]);
-};
+class NavDataGrid extends DataGrid {
+  static get builderInfo() {
+    return FormBuilderOptions.builder.data.components.navDataGrid;
+  }
 
-export default DataGrid;
+  static editForm(...extend) {
+    return DataGridEditForm([
+      {
+        label: "Display",
+        key: "display",
+        components: [
+          ...DataGridDisplayEditForm,
+          {
+            type: "textfield",
+            label: "Remove Text",
+            key: "removeAnother",
+            tooltip: "Set the text of the Remove button.",
+            placeholder: "Remove",
+            weight: 412,
+            input: true,
+          },
+        ],
+      },
+      ...extend,
+    ]);
+  }
+
+  static schema(...extend) {
+    return FormioReactComponent.schema({
+      ...FormBuilderOptions.builder.data.components.navDataGrid,
+      ...extend,
+    });
+  }
+}
+
+export default NavDataGrid;
