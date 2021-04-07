4d7162c5243d5b1397a4ad62d691b7544c4458a9
Vegard Haugstvedt <it.vegard@gmail.com>
2021-04-07 13:14:49 +0200

    Hide Tooltip, Tab Index and "Edit in Modal" options for Skjemagruppe

--- src/customComponents/components/NavSkjemagruppe.jsx
+++ src/customComponents/components/NavSkjemagruppe.jsx
@@ -1,7 +1,34 @@
 import Fieldset from "formiojs/components/fieldset/Fieldset";
+import FieldsetForm from "formiojs/components/fieldset/Fieldset.form";
+import FieldsetDisplayForm from "formiojs/components/fieldset/editForm/Fieldset.edit.display";
 import FormBuilderOptions from "../../Forms/FormBuilderOptions";
 
 class Skjemagruppe extends Fieldset {
+  static editForm(...extend) {
+    return FieldsetForm([
+      {
+        label: "Display",
+        key: "display",
+        components: [
+          ...FieldsetDisplayForm,
+          {
+            key: "tooltip",
+            ignore: true,
+          },
+          {
+            key: "tabindex",
+            ignore: true,
+          },
+          {
+            key: "modalEdit",
+            ignore: true,
+          },
+        ],
+      },
+      ...extend,
+    ]);
+  }
+
   static schema(...extend) {
     return Fieldset.schema({
       ...FormBuilderOptions.builder.layout.components.navSkjemagruppe.schema,
