b80eaa295e2b8dfd30902f018ac78344f735d7e0
Vegard Haugstvedt <it.vegard@gmail.com>
2021-04-28 10:35:10 +0200

    Refactor SummaryPage.test.js for easier testing of handleComponents

--- src/Forms/SummaryPage.test.js
+++ src/Forms/SummaryPage.test.js
@@ -1,4 +1,4 @@
-import { createFormSummaryObject } from "./SummaryPage";
+import { createFormSummaryObject, handleComponent } from "./SummaryPage";
 
 const dummyTextfield = {
   label: "Tekstfelt",
@@ -30,13 +30,6 @@ const dummyContainerElement = {
   key: "container",
 };
 
-const dummyFieldset = {
-  label: "Fieldset-label",
-  legend: "Fieldset-legend",
-  type: "fieldset",
-  key: "fieldset",
-};
-
 const dummyNavSkjemagruppe = {
   label: "NavSkjemagruppe-label",
   legend: "NavSkjemagruppe-legend",
@@ -44,146 +37,115 @@ const dummyNavSkjemagruppe = {
   key: "navSkjemagruppe",
 };
 
-const createFormObject = (componentsForPanel1) => ({
-  components: [
-    {
-      label: "Panel 1",
-      key: "panel1",
-      type: "panel",
-      components: componentsForPanel1 ? componentsForPanel1 : undefined,
-    },
-    {
-      label: "Panel 2",
-      key: "panel1",
-      type: "panel",
-    },
-    {
-      label: "Panel 3",
-      key: "panel1",
-      type: "panel",
-    },
-  ],
-});
-
 const submissionData = {
   email: "email-verdi",
   tekstfelt: "tekstfelt-verdi",
 };
 
-const expectedFormSummaryObject = {};
+const createPanelObject = (label, components) => ({
+  label,
+  key: label.toLowerCase().replace(" ", ""),
+  type: "panel",
+  components,
+});
 
-describe("When creating form summary object", () => {
-  it("panels are added as arrays on the top level", () => {
-    const actual = createFormSummaryObject(createFormObject(), submissionData);
-    expect(actual).toBeInstanceOf(Array);
-    expect(actual.length).toEqual(3);
-  });
+const createFormObject = (panels = []) => ({
+  components: panels,
+});
 
-  it("form fields in panels are added with value from submission", () => {
-    const actual = createFormSummaryObject(createFormObject([dummyTextfield]), submissionData);
-    expect(actual[0].components).toContainEqual({
-      label: "Tekstfelt",
-      type: "textfield",
-      value: "tekstfelt-verdi",
-    });
-  });
-
-  it("form fields in panels are not added if they don't have a submission value", () => {
-    const actual = createFormSummaryObject(createFormObject([dummyTextfield]), {});
-    expect(actual[0].components.find((component) => component.type === "textfield")).toBeUndefined();
-    expect(actual[0].components).not.toContainEqual({
-      label: "Tekstfelt",
-      type: "textfield",
-      value: undefined,
-    });
-  });
-
-  describe("[content, htmlelement]", () => {
-    it("is filtered away", () => {
-      const actual = createFormSummaryObject(createFormObject([dummyContentElement, dummyHTMLElement]), submissionData);
-      expect(actual[0].components).not.toContainEqual({
-        label: "Content",
-        type: "content",
-        value: undefined,
-      });
-      expect(actual[0].components).not.toContainEqual({
-        label: "HTMLelement",
-        type: "htmlelement",
-        value: undefined,
-      });
-    });
-  });
-
-  describe("container", () => {
-    it("is ignored if it does not contain any subcomponents", () => {
-      const actual = createFormSummaryObject(createFormObject([dummyContainerElement]), submissionData);
-      expect(actual[0].components.find((component) => component.type === "container")).toBeUndefined();
-    });
-
-    it("is ignored, and subcomponents that should not be included are also ignored", () => {
-      const actual = createFormSummaryObject(
-        createFormObject([
-          {
-            ...dummyContainerElement,
-            components: [dummyContentElement, dummyTextfield],
-          },
-        ]),
-        {} // Textfield has no value, so it should not be included
-      );
-      expect(actual[0].components).not.toContainEqual({
-        label: "Tekstfelt",
-        type: "textfield",
-        value: undefined,
-      });
-    });
-
-    it("is ignored, but subcomponents that should be included are added", () => {
-      const actual = createFormSummaryObject(
-        createFormObject([
-          {
-            ...dummyContainerElement,
-            components: [dummyContentElement, dummyTextfield],
-          },
-        ]),
-        submissionData // Textfield has no value, so it should not be included
-      );
-      expect(actual[0].components).toContainEqual({
+describe("When handling component", () => {
+  describe("form fields", () => {
+    it("are added with value from submission", () => {
+      const actual = handleComponent(dummyTextfield, submissionData, []);
+      expect(actual).toContainEqual({
         label: "Tekstfelt",
         type: "textfield",
         value: "tekstfelt-verdi",
       });
     });
+
+    it("are not added if they don't have a submission value", () => {
+      const actual = handleComponent(dummyTextfield, {}, []);
+      expect(actual.find((component) => component.type === "textfield")).toBeUndefined();
+    });
   });
 
-  describe("[fieldset, navSkjemagruppe]", () => {
+  describe("content", () => {
+    it("is filtered away", () => {
+      const actual = handleComponent(dummyContentElement, submissionData, []);
+      expect(actual.find((component) => component.type === "content")).toBeUndefined();
+    });
+  });
+  describe("htmlelement", () => {
+    it("is filtered away", () => {
+      const actual = handleComponent(dummyHTMLElement, submissionData, []);
+      expect(actual.find((component) => component.type === "htmlelement")).toBeUndefined();
+    });
+  });
+
+  describe("container", () => {
+    it("is never included", () => {
+      const actual = handleComponent(dummyContainerElement, submissionData, []);
+      expect(actual.find((component) => component.type === "container")).toBeUndefined();
+    });
+
+    it("is ignored, and subcomponents that should not be included are also ignored", () => {
+      const actual = handleComponent(
+        {
+          ...dummyContainerElement,
+          components: [dummyContentElement, dummyTextfield],
+        },
+        {},
+        []
+      );
+      expect(actual.find((component) => component.type === "container")).toBeUndefined();
+      expect(actual.find((component) => component.type === "content")).toBeUndefined();
+      expect(actual.find((component) => component.type === "textfield")).toBeUndefined();
+    });
+
+    it("is ignored, but subcomponents that should be included are added", () => {
+      const actual = handleComponent(
+        {
+          ...dummyContainerElement,
+          components: [dummyContentElement, dummyTextfield],
+        },
+        submissionData,
+        []
+      );
+      expect(actual).toEqual([
+        {
+          label: "Tekstfelt",
+          type: "textfield",
+          value: "tekstfelt-verdi",
+        },
+      ]);
+    });
+  });
+
+  describe("[navSkjemagruppe]", () => {
     it("is ignored if they have no subcomponents", () => {
-      const actual = createFormSummaryObject(createFormObject([dummyFieldset, dummyNavSkjemagruppe]), {});
-      expect(actual[0].components.find((component) => component.type === "fieldset")).toBeUndefined();
-      expect(actual[0].components.find((component) => component.type === "navSkjemagruppe")).toBeUndefined();
+      const actual = handleComponent(dummyNavSkjemagruppe, {}, []);
+      expect(actual.find((component) => component.type === "navSkjemagruppe")).toBeUndefined();
     });
 
     it("uses legend and not label", () => {
+      const actual = handleComponent({ ...dummyNavSkjemagruppe, components: [dummyTextfield] }, submissionData, []);
+      const actualNavSkjemagruppe = actual.find((component) => component.type === "navSkjemagruppe");
+      expect(actualNavSkjemagruppe).toBeDefined();
+      expect(actualNavSkjemagruppe.label).toEqual("NavSkjemagruppe-legend");
+    });
+  });
+});
+
+describe("When creating form summary object", () => {
+  describe("panels", () => {
+    it("are added as arrays on the top level", () => {
       const actual = createFormSummaryObject(
-        createFormObject([
-          {
-            ...dummyFieldset,
-            components: [dummyTextfield],
-          },
-          {
-            ...dummyNavSkjemagruppe,
-            components: [dummyEmail],
-          },
-        ]),
+        createFormObject([createPanelObject("Panel 1"), createPanelObject("Panel 2"), createPanelObject("Panel 3")]),
         submissionData
       );
-      const actualFieldsetInFormSummaryObject = actual[0].components.find((component) => component.type === "fieldset");
-      const actualNavSkjemagruppeInFormSummaryObject = actual[0].components.find(
-        (component) => component.type === "navSkjemagruppe"
-      );
-      expect(actualFieldsetInFormSummaryObject).toBeDefined();
-      expect(actualFieldsetInFormSummaryObject.label).toEqual("Fieldset-legend");
-      expect(actualNavSkjemagruppeInFormSummaryObject).toBeDefined();
-      expect(actualNavSkjemagruppeInFormSummaryObject.label).toEqual("NavSkjemagruppe-legend");
+      expect(actual).toBeInstanceOf(Array);
+      expect(actual.length).toEqual(3);
     });
   });
 });
