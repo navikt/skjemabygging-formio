b7cfcce67de310936f01287bda78d30123ecf0b0
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2021-04-30 15:55:00 +0200

    mulighet for å oppgi plassering av description i forhold til label

--- src/customComponents/components/TextField.js
+++ src/customComponents/components/TextField.js
@@ -2,13 +2,14 @@ import TextField from "formiojs/components/textfield/TextField";
 import TextFieldEditForm from "formiojs/components/textfield/TextField.form";
 import TextFieldDisplayEditForm from "formiojs/components/textfield/editForm/TextField.edit.display";
 import { fieldSizeField } from "./fields";
+import { descriptionPositionField } from "./fields/descriptionPositionField";
 
 TextField.editForm = () => {
   return TextFieldEditForm([
     {
       label: "Display",
       key: "display",
-      components: [...TextFieldDisplayEditForm, fieldSizeField],
+      components: [...TextFieldDisplayEditForm, fieldSizeField, descriptionPositionField],
     },
   ]);
 };
--- src/customComponents/index.js
+++ src/customComponents/index.js
@@ -4,6 +4,7 @@ import RadioPanelGruppeComponent from "./components/RadioPanelGruppe";
 import CheckboxComponent from "./components/Checkbox";
 import DataGrid from "./components/DataGrid";
 import TextField from "./components/TextField";
+import Textarea from "./components/TextArea";
 import Number from "./components/Number";
 import NavSkjemagruppe from "./components/NavSkjemagruppe";
 
@@ -14,6 +15,7 @@ export default {
   navCheckbox: CheckboxComponent,
   datagrid: DataGrid,
   textfield: TextField,
+  textArea: Textarea,
   number: Number,
   navSkjemagruppe: NavSkjemagruppe,
 };
