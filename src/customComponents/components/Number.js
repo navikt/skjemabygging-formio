62723654e3d0b90c992382dcbd4bb40894394338
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2021-05-03 14:33:46 +0200

    flytte fieldSize-definisjonen inn i samme mappe som descriptionPosition

--- src/customComponents/components/Number.js
+++ src/customComponents/components/Number.js
@@ -1,7 +1,7 @@
 import Number from "formiojs/components/number/Number";
 import NumberEditForm from "formiojs/components/number/Number.form";
 import NumberDisplayEditForm from "formiojs/components/number/editForm/Number.edit.display";
-import { fieldSizeField } from "./fields";
+import { fieldSizeField } from "./fields/fieldSize";
 import { descriptionPositionField } from "./fields/descriptionPositionField";
 
 Number.editForm = () => {
--- src/customComponents/components/TextField.js
+++ src/customComponents/components/TextField.js
@@ -1,7 +1,7 @@
 import TextField from "formiojs/components/textfield/TextField";
 import TextFieldEditForm from "formiojs/components/textfield/TextField.form";
 import TextFieldDisplayEditForm from "formiojs/components/textfield/editForm/TextField.edit.display";
-import { fieldSizeField } from "./fields";
+import { fieldSizeField } from "./fields/fieldSize";
 import { descriptionPositionField } from "./fields/descriptionPositionField";
 
 TextField.editForm = () => {
