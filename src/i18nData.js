0a0be73a584f49ee51c353bce16909aa9f390610
Wenqi Lin <wenqi.lin@nav.no>
2021-05-11 14:49:53 +0200

    delete example i18n translation

--- src/i18nData.js
+++ src/i18nData.js
@@ -42,11 +42,5 @@ export default {
     october: "Oktober",
     november: "November",
     december: "Desember",
-    valgfritt: "optional",
-    Ja: "yes",
-    Nei: "no",
-    Avkryssingsboks: "checkbox",
-    "Dato (dd.mm.åååå)": "Date (dd.mm.yyyy)",
-    "Fødselsnummer / D-nummer": "Social security number",
   },
 };
