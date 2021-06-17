ff6324e5f8df54be7dcc8588e84fed37ff57a623
Wenqi Lin <wenqi.lin@nav.no>
2021-06-17 13:01:02 +0200

    extract code with same logic into a helper function

--- src/context/i18n/translationsMapper.js
+++ src/context/i18n/translationsMapper.js
@@ -1,29 +1,25 @@
 import GlobalTranslations from "../../i18nData";
 
+const flattenTranslationObject = (translationObject) => {
+  return Object.keys(translationObject).reduce(
+    (acc, val) => ({
+      ...acc,
+      [val]: translationObject[val].value ? translationObject[val].value : translationObject[val],
+    }),
+    {}
+  );
+};
+
 const mapTranslationsToFormioI18nObject = (translations) => {
   return Object.keys(translations).reduce(
     (formioI18nObject, languageCode) => ({
       ...formioI18nObject,
       [languageCode]: {
-        ...Object.keys(translations[languageCode].translations || {}).reduce(
-          (translationsForLanguageCode, originalText) => ({
-            ...translationsForLanguageCode,
-            [originalText]: translations[languageCode].translations[originalText].value,
-          }),
-          {}
-        ),
+        ...flattenTranslationObject(translations[languageCode].translations || {}),
       },
     }),
     {
-      ...Object.keys(GlobalTranslations).reduce(
-        (languages, languageCode) => ({
-          ...languages,
-          [languageCode]: {
-            ...GlobalTranslations[languageCode],
-          },
-        }),
-        {}
-      ),
+      ...flattenTranslationObject(GlobalTranslations),
     }
   );
 };
