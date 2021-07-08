a84afb93451eee39b63b3c4ada367053e1444bcc
Rune Rosseland <rune.rosseland@nav.no>
2021-07-08 11:00:58 +0200

    Remove unused editForm options for f.nr and textField

--- packages/shared-components/src/customComponents/components/TextField.js
+++ packages/shared-components/src/customComponents/components/TextField.js
@@ -9,7 +9,128 @@ TextField.editForm = () => {
     {
       label: "Display",
       key: "display",
-      components: [...TextFieldDisplayEditForm, fieldSizeField, descriptionPositionField],
+      components: [
+        ...TextFieldDisplayEditForm,
+        fieldSizeField,
+        descriptionPositionField,
+        {
+          // You can ignore existing fields.
+          key: "placeholder",
+          ignore: true,
+        },
+        {
+          key: "labelPosition",
+          ignore: true,
+        },
+        {
+          key: "tabindex",
+          ignore: true,
+        },
+        {
+          key: "tooltip",
+          ignore: true,
+        },
+        {
+          key: "customClass",
+          ignore: true,
+        },
+        {
+          key: "hidden",
+          ignore: true,
+        },
+        {
+          key: "hideLabel",
+          ignore: true,
+        },
+        {
+          key: "autofocus",
+          ignore: true,
+        },
+        {
+          key: "tableView",
+          ignore: true,
+        },
+        {
+          key: "modalEdit",
+          ignore: true,
+        },
+        {
+          key: "widget.type",
+          ignore: true,
+        },
+        {
+          key: "inputMask",
+          ignore: true,
+        },
+        {
+          key: "allowMultipleMasks",
+          ignore: true,
+        },
+      ],
+    },
+    {
+      key: "data",
+      components: [
+        {
+          key: "multiple",
+          ignore: true,
+        },
+        {
+          key: "persistent",
+          ignore: true,
+        },
+        {
+          key: "inputFormat",
+          ignore: true,
+        },
+        {
+          key: "protected",
+          ignore: true,
+        },
+        {
+          key: "dbIndex",
+          ignore: true,
+        },
+        {
+          key: "case",
+          ignore: true,
+        },
+        {
+          key: "encrypted",
+          ignore: true,
+        },
+        {
+          key: "redrawOn",
+          ignore: true,
+        },
+        {
+          key: "calculateServer",
+          ignore: true,
+        },
+        {
+          key: "allowCalculateOverride",
+          ignore: true,
+        },
+      ],
+    },
+    {
+      key: "validation",
+      components: [
+        {
+          key: "unique",
+          ignore: true,
+        },
+      ],
+    },
+    {
+      key: "logic",
+      ignore: true,
+      components: false,
+    },
+    {
+      key: "layout",
+      ignore: true,
+      components: false,
     },
   ]);
 };
