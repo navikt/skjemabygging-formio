22c97e29769001c52fdd4e74d86c006183ca7088
Magnus Røed Hestvik <mrh@computas.com>
2021-08-06 09:38:28 +0200

    Add infobox to describe how to limit earliest and latet allowed dates

--- packages/shared-components/src/customComponents/components/NavDatepicker.jsx
+++ packages/shared-components/src/customComponents/components/NavDatepicker.jsx
@@ -178,16 +178,28 @@ export default class NavDatepicker extends FormioReactComponent {
                   ]
                 },
                 {
-                  type: "number",
-                  label: "Dato kan ikke være tidligere enn (dagens dato pluss x dager)",
-                  key: "earliestAllowedDate",
-                  input: true,
-                },
-                {
-                  type: "number",
-                  label: "Dato kan ikke være senere enn (dagens dato pluss x dager)",
-                  key: "latestAllowedDate",
-                  input: true,
+                  type: "panel",
+                  title: "Begrens periode for tillatt dato",
+                  components: [
+                    {
+                      type: "number",
+                      label: "Tidligst tillatt dato (antall dager fram/bak i tid)",
+                      key: "earliestAllowedDate",
+                      input: true,
+                    },
+                    {
+                      type: "number",
+                      label: "Senest tillatt dato (antall dager fram/bak i tid)",
+                      key: "latestAllowedDate",
+                      input: true,
+                    },
+                    {
+                      type: "alertstripe",
+                      key: "begrensTillattDatoInfo",
+                      content: "<div><p>Oppgi antall dager for å sette tidligste og seneste tillatte dato. Begrensningen er relativ til datoen skjemaet fylles ut. Bruk positive tall for å oppgi dager fram i tid, negative tall for å sette tillatt dato bakover i tid, og 0 for å sette dagens dato som tidligst/senest tillatt.</p><p>Eksempel: hvis tidligst tillatt er satt til -5, vil datoer før 10. august 2022 gi feilmelding når skjemaet fylles ut 15. august 2022</p></div>",
+                      alerttype: "info"
+                    },
+                  ]
                 },
                 ...validationEditForm,
               ],
