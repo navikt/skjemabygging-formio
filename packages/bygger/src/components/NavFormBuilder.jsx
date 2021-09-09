46078d930b05fe27426ca529a3071b2e31f095a9
Vegard Haugstvedt <it.vegard@gmail.com>
2021-09-09 10:16:29 +0200

    Style step-selectors (.wizard-page-label) in builder

--- packages/bygger/src/components/NavFormBuilder.jsx
+++ packages/bygger/src/components/NavFormBuilder.jsx
@@ -79,11 +79,63 @@ const BuilderMountElement = styled("div")({
   "& .btn": {
     textAlign: "left",
   },
-  "& .mr-2, .mx-2": {
-    marginBottom: "0.5rem",
-  },
-  "& .badge": {
-    fontSize: "1rem",
+  "& .breadcrumb": {
+    display: "flex",
+    flexWrap: "wrap",
+    padding: "0.75rem 1rem",
+    margin: "0 0 1rem",
+    listStyle: "none",
+    backgroundColor: "#e9ecef",
+    borderRadius: "0.25rem",
+
+    "& .wizard-page-label": {
+      alignItems: "center",
+      backgroundColor: "#17a2b8",
+      border: "none",
+      borderRadius: "0.25rem",
+      color: "white",
+      cursor: "pointer",
+      display: "flex",
+      padding: "0.3rem 0.5rem",
+      fontSize: "1rem",
+      fontWeight: "700",
+      lineHeight: "1",
+      marginBottom: "0.5rem",
+      textAlign: "center",
+      textDecoration: "none",
+      verticalAlign: "baseline",
+      whiteSpace: "nowrap",
+
+      "&:focus": {
+        border: "0.1rem solid #17a2b8",
+        backgroundColor: "white",
+        color: "#17a2b8",
+        outline: "none",
+        padding: "0.2rem 0.4rem",
+      },
+
+      "&[aria-current]": {
+        backgroundColor: "#007bff",
+
+        "&:focus": {
+          border: "0.1rem solid #007bff",
+          backgroundColor: "white",
+          color: "#007bff",
+        },
+      },
+      "&__add-new": {
+        backgroundColor: "#28a745",
+
+        "&:focus": {
+          border: "0.1rem solid #28a745",
+          backgroundColor: "white",
+          color: "#28a745",
+        },
+      },
+    },
+    "& li:not(:last-child) .wizard-page-label": {
+      marginRight: "0.5rem",
+    },
   },
   "& .panel-body, & .tab-pane": {
     "& >.drag-container.formio-builder-components": {
