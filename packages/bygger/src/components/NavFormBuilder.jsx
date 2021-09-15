35fab21a69ef2e57da76d9861dcf54b40c91bdbc
Vegard Haugstvedt <it.vegard@gmail.com>
2021-09-15 15:59:03 +0200

    Style formio-dialog, including layout

--- packages/bygger/src/components/NavFormBuilder.jsx
+++ packages/bygger/src/components/NavFormBuilder.jsx
@@ -248,7 +248,72 @@ const useBuilderMountElementStyles = makeStyles({
     // End card styling
     // Start formio-dialog
     ".formio-dialog": {
-      zIndex: 900,
+      position: "fixed",
+      overflow: "auto",
+      zIndex: "10000",
+      top: "0",
+      right: "0",
+      bottom: "0",
+      left: "0",
+      background: "rgba(0,0,0,.4)",
+      animation: "formio-dialog-fadein .5s",
+      boxSizing: "border-box",
+      fontSize: ".8em",
+      color: "#666",
+      paddingTop: "20px",
+      paddingBottom: "20px",
+
+      "&-overlay": {
+        position: "fixed",
+        top: "0",
+        right: "0",
+        bottom: "0",
+        left: "0",
+        marginRight: "15px",
+        background: "0 0",
+      },
+
+      "&-content": {
+        background: "#f0f0f0",
+        borderRadius: "5px",
+        fontFamily: "Helvetica,sans-serif",
+        fontSize: "1.1em",
+        lineHeight: "1.5em",
+        margin: "0 auto",
+        maxWidth: "100%",
+        padding: "1em",
+        position: "relative",
+        width: "80%",
+      },
+
+      "& .float-right": {
+        float: "right",
+      },
+
+      "& .row": {
+        display: "flex",
+        flexWrap: "wrap",
+        margin: "0 -15px",
+      },
+
+      "& .col": {
+        flexBasis: "0",
+        flexGrow: "1",
+        maxWidth: "100%",
+        position: "relative",
+        width: "100%",
+        padding: "0 15px",
+
+        "&-sm-6": {
+          flex: "0 0 50%",
+          maxWidth: "50%",
+        },
+
+        "&-sm-12": {
+          flex: "0 0 100%",
+          maxWidth: "100%",
+        },
+      },
     },
     // End formio-dialog
     // Start miscellaneous styling
