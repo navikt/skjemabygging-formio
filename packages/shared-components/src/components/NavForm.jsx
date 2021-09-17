665a6f5988be01d8f8f5066aa3bae3527dc225a5
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2021-09-17 12:53:38 +0200

    Skriver om NavForm til en funksjonskomponent

--- packages/shared-components/src/components/NavForm.jsx
+++ packages/shared-components/src/components/NavForm.jsx
@@ -22,7 +22,7 @@
  * SOFTWARE.
  * */
 
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import EventEmitter from "eventemitter2";
 import { Form as FormioForm, Utils } from "formiojs";
@@ -36,158 +36,156 @@ import { checkConditionOverride, overrideFormioWizardNextPageAndSubmit } from ".
 
 Utils.checkCondition = checkConditionOverride;
 
-class NavForm extends Component {
-  static propTypes = {
-    className: PropTypes.string,
-    src: PropTypes.string,
-    url: PropTypes.string,
-    form: PropTypes.object,
-    submission: PropTypes.object,
-    options: PropTypes.shape({
-      language: PropTypes.string,
-      readOnly: PropTypes.bool,
-      noAlerts: PropTypes.bool,
-      i18n: PropTypes.object,
-      template: PropTypes.string,
-      saveDraft: PropTypes.bool,
-    }),
-    onPrevPage: PropTypes.func,
-    onNextPage: PropTypes.func,
-    onCancel: PropTypes.func,
-    onChange: PropTypes.func,
-    onCustomEvent: PropTypes.func,
-    onComponentChange: PropTypes.func,
-    onSubmit: PropTypes.func,
-    onSubmitDone: PropTypes.func,
-    onFormLoad: PropTypes.func,
-    onError: PropTypes.func,
-    onRender: PropTypes.func,
-    onAttach: PropTypes.func,
-    onBuild: PropTypes.func,
-    onFocus: PropTypes.func,
-    onBlur: PropTypes.func,
-    onInitialized: PropTypes.func,
-    formioform: PropTypes.any,
-    loggSkjemaStegFullfort: PropTypes.func,
-    loggSkjemaValideringFeilet: PropTypes.func,
-  };
-
-  static defaultProps = {
-    options: {
-      language: "nb-NO",
-      i18n: i18nData,
-    },
-    onNextPage: () => scrollToAndSetFocus(".wizard-page input, .wizard-page textarea, .wizard-page select", "center"),
-    onPrevPage: () => scrollToAndSetFocus(".wizard-page input, .wizard-page textarea, .wizard-page select", "center"),
-  };
-
-  static getDefaultEmitter() {
-    return new EventEmitter({
-      wildcard: false,
-      maxListeners: 0,
-    });
-  }
-
-  componentDidMount = () => {
-    const { options, src, url, form } = this.props;
-
-    if (!options.events) {
-      options.events = NavForm.getDefaultEmitter();
-    }
-
-    if (src) {
-      this.instance = new (this.props.formioform || FormioForm)(this.element, src, options);
-      this.createPromise = this.instance.ready.then((formio) => {
-        this.formio = formio;
-        this.formio.src = src;
-      });
-    }
-    if (form) {
-      this.instance = new (this.props.formioform || FormioForm)(this.element, form, options);
-      this.createPromise = this.instance.ready.then((formio) => {
-        this.formio = formio;
-        this.formio.form = form;
-        if (url) {
-          this.formio.url = url;
-        }
-        window.setLanguage = (lang) => {
-          this.formio.language = lang;
-        };
-        return this.formio;
-      });
-    }
-
-    this.initializeFormio();
-  };
-
-  componentWillUnmount = () => {
-    if (this.formio !== undefined) {
-      this.formio.destroy(true);
-    }
-  };
-
-  initializeFormio = () => {
-    if (this.createPromise) {
-      this.instance.onAny((event, ...args) => {
-        if (event.startsWith("formio.")) {
-          const funcName = `on${event.charAt(7).toUpperCase()}${event.slice(8)}`;
-          if (this.props.hasOwnProperty(funcName) && typeof this.props[funcName] === "function") {
-            this.props[funcName](...args);
-          }
-        }
-      });
-      this.createPromise.then(() => {
-        if (this.props.submission) {
-          this.formio.submission = this.props.submission;
-        }
-      });
-    }
-    overrideFormioWizardNextPageAndSubmit(
-      this.props.form,
-      this.props.loggSkjemaStegFullfort,
-      this.props.loggSkjemaValideringFeilet
-    );
-  };
-
-  UNSAFE_componentWillReceiveProps = (nextProps) => {
-    const { options = {}, src, form, submission } = this.props;
-
-    if (!options.events) {
-      options.events = NavForm.getDefaultEmitter();
-    }
-
-    if (src !== nextProps.src) {
-      this.instance = new (this.props.formioform || FormioForm)(this.element, nextProps.src, options);
-      this.createPromise = this.instance.ready.then((formio) => {
-        this.formio = formio;
-        this.formio.src = nextProps.src;
-      });
-      this.initializeFormio();
-    }
-    if (form !== nextProps.form) {
-      this.instance = new (this.props.formioform || FormioForm)(this.element, nextProps.form, options);
-      this.createPromise = this.instance.ready.then((formio) => {
-        this.formio = formio;
-        this.formio.form = nextProps.form;
-      });
-      this.initializeFormio();
-    }
-
-    if (submission !== nextProps.submission && this.formio) {
-      this.formio.submission = nextProps.submission;
-    }
-  };
-
-  render = () => {
-    return (
-      <div
-        className={this.props.className}
-        data-testid="formMountElement"
-        ref={(element) => (this.element = element)}
-      />
-    );
-  };
+const defaultProps = {
+  options: {
+    language: "nb-NO",
+    i18n: i18nData,
+  },
+  onNextPage: () => scrollToAndSetFocus(".wizard-page input, .wizard-page textarea, .wizard-page select", "center"),
+  onPrevPage: () => scrollToAndSetFocus(".wizard-page input, .wizard-page textarea, .wizard-page select", "center"),
 }
+
+const NavForm = (props = defaultProps) => {
+
+  let instance;
+  let createPromise;
+  let element;
+  const [formio, setFormio] = useState(undefined);
+
+  useEffect(() => () => formio ? formio.destroy(true) : null, [formio]);
+
+  const createWebformInstance = (srcOrForm) => {
+    const {options = {}, formioform, formReady} = props;
+    instance = new (formioform || FormioForm)(element, srcOrForm, options);
+    createPromise = instance.ready.then(formioInstance => {
+      setFormio(formioInstance);
+      if (formReady) {
+        formReady(formioInstance);
+      }
+    });
+
+    return createPromise;
+  };
+
+  const onAnyEvent = (event, ...args) => {
+    if (event.startsWith('formio.')) {
+      const funcName = `on${event.charAt(7).toUpperCase()}${event.slice(8)}`;
+      // eslint-disable-next-line no-prototype-builtins
+      if (props.hasOwnProperty(funcName) && typeof (props[funcName]) === 'function') {
+        props[funcName](...args);
+      }
+    }
+  };
+
+  const initializeFormio = () => {
+    const {submission} = props;
+    if (createPromise) {
+      instance.onAny(onAnyEvent);
+      createPromise.then(() => {
+        if (formio && submission) {
+          formio.submission = submission;
+        }
+      });
+    }
+  };
+
+  useEffect(() => {
+    const {src} = props;
+    if (src) {
+      createWebformInstance(src).then(() => {
+        if (formio) {
+          formio.src = src;
+        }
+      });
+      initializeFormio();
+    }
+  }, [props.src]);
+
+  useEffect(() => {
+    const {form, url} = props;
+    if (form) {
+      createWebformInstance(form).then(() => {
+        if (formio) {
+          formio.form = form;
+          if (url) {
+            formio.url = url;
+          }
+          return formio;
+        }
+      });
+      initializeFormio();
+    }
+  }, [props.form]);
+
+  useEffect(() => {
+    overrideFormioWizardNextPageAndSubmit(
+      props.loggSkjemaStegFullfort,
+      props.loggSkjemaValideringFeilet
+    );
+  }, [props.loggSkjemaStegFullfort, props.loggSkjemaValideringFeilet]);
+
+  useEffect(() => {
+    const {options = {}} = props;
+    if (!options.events) {
+      options.events = NavForm.getDefaultEmitter();
+    }
+  }, [props.options]);
+
+  useEffect(() => {
+    const {submission} = props;
+    if (formio && submission) {
+      formio.submission = submission;
+    }
+  }, [props.submission, formio]);
+
+  return (
+    <div
+      className={props.className}
+      data-testid="formMountElement"
+      ref={el => element = el}
+    />
+  );
+};
+
+NavForm.propTypes = {
+  className: PropTypes.string,
+  src: PropTypes.string,
+  url: PropTypes.string,
+  form: PropTypes.object,
+  submission: PropTypes.object,
+  options: PropTypes.shape({
+    readOnly: PropTypes.boolean,
+    noAlerts: PropTypes.boolean,
+    i18n: PropTypes.object,
+    template: PropTypes.string,
+    saveDraft: PropTypes.boolean,
+  }),
+  onPrevPage: PropTypes.func,
+  onNextPage: PropTypes.func,
+  onCancel: PropTypes.func,
+  onChange: PropTypes.func,
+  onCustomEvent: PropTypes.func,
+  onComponentChange: PropTypes.func,
+  onSubmit: PropTypes.func,
+  onSubmitDone: PropTypes.func,
+  onFormLoad: PropTypes.func,
+  onError: PropTypes.func,
+  onRender: PropTypes.func,
+  onAttach: PropTypes.func,
+  onBuild: PropTypes.func,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
+  onInitialized: PropTypes.func,
+  formReady: PropTypes.func,
+  formioform: PropTypes.any
+};
+
+NavForm.getDefaultEmitter = () => {
+  return new EventEmitter({
+    wildcard: false,
+    maxListeners: 0
+  });
+};
 
 const withAmplitudeHooks = (Component) => {
   return (props) => {
