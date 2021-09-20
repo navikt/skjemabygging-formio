5d0ecac12da2a454c5367ab03a99688d8ffdb525
Wenqi Lin <wenqi.lin@nav.no>
2021-09-20 08:58:03 +0200

    show value from day component with only month and year

--- packages/shared-domain/src/texts/common.js
+++ packages/shared-domain/src/texts/common.js
@@ -2,5 +2,17 @@ const common = {
   yes: "Ja",
   no: "Nei",
   optional: "valgfritt",
+  january: "Januar",
+  february: "Februar",
+  march: "Mars",
+  april: "April",
+  may: "Mai",
+  june: "Juni",
+  july: "Juli",
+  august: "August",
+  september: "September",
+  october: "Oktober",
+  november: "November",
+  december: "Desember",
 };
 export default common;
