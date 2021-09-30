c45b9a0453611256090908ae643f59c8ef804136
Vegard Haugstvedt <it.vegard@gmail.com>
2021-09-30 12:00:27 +0200

    Delete commented-out code from FormBuilderOptions

--- packages/shared-components/src/Forms/form-builder-options/index.js
+++ packages/shared-components/src/Forms/form-builder-options/index.js
@@ -1,28 +1,6 @@
 import { SANITIZE_CONFIG } from "../../template/sanitizeConfig";
 import builderEditForm from "./builderEditForm";
 import builderPalett from "./builder-palett";
-//import { defaultFormFields } from "../../../bygger/src/Forms/DefaultForm";
-
-const postboksPrefix = "postboks";
-const utlandPrefix = "utland";
-
-/*const personaliaSchema = (keyPostfix = "") => ({
-  label: "Personalia",
-  hideLabel: true,
-  type: "container",
-  key: "personalia",
-  input: true,
-  components: [fodselsNummerDNummerSchema(keyPostfix), firstNameSchema(keyPostfix), surnameSchema(keyPostfix)],
-});*/
-
-/*const komplettKontaktInfoSchema = (keyPostfix = "") => ({
-  label: "Komplett kontaktinfo",
-  hideLabel: true,
-  type: "container",
-  key: `komplettKontaktinfo${keyPostfix}`,
-  input: true,
-  components: [],
-});*/
 
 const FormBuilderOptions = {
   builder: builderPalett,
