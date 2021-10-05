8232cc70be5a8cb4616b34ab39f62c81cff037d0
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2021-10-05 20:31:18 +0200

    Teste visning av conditional alert inne i edit-dialogen

--- packages/bygger/src/components/NavFormBuilder.test.jsx
+++ packages/bygger/src/components/NavFormBuilder.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {render, screen, within, waitFor} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import {render, screen, within, waitFor} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import {InprocessQuipApp} from "../fakeBackend/InprocessQuipApp";
 import {dispatcherWithBackend} from "../fakeBackend/fakeWebApp";
 import {FakeBackend} from "../fakeBackend/FakeBackend";
@@ -42,7 +42,7 @@ describe("NavFormBuilder", () => {
     window.confirm = undefined;
   });
 
-  describe('A form with conditional dependencies', () => {
+  describe("A form with conditional dependencies", () => {
 
     let onChangeMock;
     let onReadyMock;
@@ -66,7 +66,7 @@ describe("NavFormBuilder", () => {
       await waitFor(() => expect(onChangeMock.mock.calls).toHaveLength(1));
     });
 
-    describe('remove button', () => {
+    describe("remove button", () => {
 
       it("removes a component which no other component depends on", async () => {
         const checkbox = screen.queryByLabelText("Oppgi din favorittfarge");
@@ -131,6 +131,42 @@ describe("NavFormBuilder", () => {
         expect(window.confirm.mock.calls).toHaveLength(2);
         expect(window.confirm.mock.calls[0][0]).toEqual("En eller flere andre komponenter har avhengighet til denne. Vil du fremdeles slette den?");
         expect(window.confirm.mock.calls[1][0]).toEqual("Removing this component will also remove all of its children. Are you sure you want to do this?");
+      });
+
+    });
+
+    describe("conditional alert message in edit component", () => {
+
+      it("is not visible when component is not depended upon by other components", async () => {
+        const textInput = screen.queryByLabelText("Oppgi din favorittfarge");
+        expect(textInput).toBeInTheDocument();
+
+        const builderComponent = findClosestWithAttribute(textInput, BUILDER_COMP_TESTID_ATTR);
+
+        const editComponentButton = await within(builderComponent).findByTitle("Rediger");
+        userEvent.click(editComponentButton);
+
+        const conditionalAlert = screen.queryByRole("list", {
+          name: "Følgende komponenter har avhengighet til denne:",
+        });
+        expect(conditionalAlert).not.toBeInTheDocument();
+      });
+
+      it("is visible when component is depended upon by other components", async () => {
+        const fieldset = screen.queryByRole("group", { name: "Hva er din favorittårstid?" });
+        expect(fieldset).toBeInTheDocument();
+
+        const builderComponent = findClosestWithAttribute(fieldset, BUILDER_COMP_TESTID_ATTR);
+
+        const editComponentButton = await within(builderComponent).findByTitle("Rediger");
+        userEvent.click(editComponentButton);
+
+        const conditionalAlert = screen.queryByRole("list", {
+          name: "Følgende komponenter har avhengighet til denne:",
+        });
+        expect(conditionalAlert).toBeInTheDocument();
+        const dependentComponents = within(conditionalAlert).getAllByRole("listitem");
+        expect(dependentComponents).toHaveLength(2);
       });
 
     });
