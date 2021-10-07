a838b4e248106ab8fb921de5683dcb4824bbad41
Wenqi Lin <wenqi.lin@nav.no>
2021-10-07 08:36:51 +0200

    add test for global translation page

---
+++ packages/bygger/src/translations/global/GlobalTranslationsPage.test.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import GlobalTranslationsPage from "./GlobalTranslationsPage";
+import { MemoryRouter } from "react-router-dom";
+import { UserAlerterContext } from "../../userAlerting";
+import I18nProvider from "../../context/i18n";
+import { AppConfigProvider } from "@navikt/skjemadigitalisering-shared-components";
+import userEvent from "@testing-library/user-event";
+
+describe("GlobalTranslationsPage", () => {
+  describe("Render global translation page without available languages", () => {
+    const mockedLoadGlobalTranslations = jest.fn(() => Promise.resolve({}));
+    const mockedDeleteTranslation = jest.fn();
+    const mockedSaveTranslations = jest.fn();
+    const userAlerter = {
+      flashSuccessMessage: jest.fn(),
+      alertComponent: jest.fn(),
+    };
+    beforeEach(() => {
+      render(
+        <AppConfigProvider featureToggles={{ enableTranslations: true }}>
+          <MemoryRouter>
+            <UserAlerterContext.Provider value={userAlerter}>
+              <I18nProvider loadTranslations={mockedLoadGlobalTranslations}>
+                <GlobalTranslationsPage
+                  loadGlobalTranslations={mockedLoadGlobalTranslations}
+                  projectURL={""}
+                  deleteTranslation={mockedDeleteTranslation}
+                  saveTranslation={mockedSaveTranslations}
+                  languageCode={""}
+                />
+              </I18nProvider>
+            </UserAlerterContext.Provider>
+          </MemoryRouter>
+        </AppConfigProvider>
+      );
+    });
+
+    afterEach(() => {
+      mockedLoadGlobalTranslations.mockClear();
+      mockedDeleteTranslation.mockClear();
+      mockedSaveTranslations.mockClear();
+    });
+
+    it("renders empty header and languageSelector with velg språk label", async () => {
+      const languageSelectorLabel = await screen.getByRole("button", { name: "Velg språk" });
+      const addNewTranslationButton = await screen.getByRole("button", { name: "Legg til ny tekst" });
+      const languageHeading = await screen.getByRole("heading", { level: 1, name: "" });
+      expect(languageSelectorLabel).toBeDefined();
+      expect(addNewTranslationButton).toBeDefined();
+      expect(languageHeading).toBeDefined();
+      userEvent.click(screen.getByRole("button", { name: "Grensesnitt" }));
+      expect(screen.getByRole("button", { name: "Legg til ny tekst" })).toBeUndefined();
+    });
+    /*    it("renders legg til ny tekst button only in skjematekst panel", () => {
+      userEvent.click(screen.getByRole("button", { name: "Grensesnitt" }));
+      const addNewTranslationButton = screen.getByRole("button", { name: "Legg til ny tekst" });
+      expect(addNewTranslationButton).toBeUndefined();
+    });*/
+  });
+});
