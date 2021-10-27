2cf9ba2e1a8d82883a88f851f28228323064fb32
Wenqi Lin <wenqi.lin@nav.no>
2021-10-27 11:28:08 +0200

    replace get currentLanguage code from url with a hook we already have

--- packages/bygger/src/Forms/TestFormPage.jsx
+++ packages/bygger/src/Forms/TestFormPage.jsx
@@ -1,15 +1,13 @@
 import React, { useMemo } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { AppLayoutWithContext } from "../components/AppLayout";
-import { FyllUtRouter, useAppConfig } from "@navikt/skjemadigitalisering-shared-components";
+import { FyllUtRouter, useAppConfig, useLanguageCodeFromURL } from "@navikt/skjemadigitalisering-shared-components";
 import { useTranslations } from "../context/i18n";
 import ActionRow from "../components/layout/ActionRow";
 
 export function TestFormPage({ editFormUrl, form, formSettingsUrl, onLogout }) {
   const { featureToggles } = useAppConfig();
-  const history = useHistory();
-  const params = new URLSearchParams(history.location.search);
-  const currentLanguage = params.get("lang");
+  const currentLanguage = useLanguageCodeFromURL();
   const { getTranslationsForNavForm, getCountryNameTranslations } = useTranslations();
 
   const translationsForNavForm = useMemo(() => getTranslationsForNavForm(), [getTranslationsForNavForm]);
