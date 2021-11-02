8bba16f91a38aca235bf1196f0a7bbcb97635446
Wenqi Lin <wenqi.lin@nav.no>
2021-11-02 13:45:36 +0100

    add test for PublishSettingsModal

--- packages/bygger/src/Forms/PublishSettingsModal.test.tsx
+++ packages/bygger/src/Forms/PublishSettingsModal.test.tsx
@@ -1,5 +1,101 @@
 import React from "react";
-import { getCompleteTranslationLanguageCodeList } from "./PublishSettingsModal";
+import PublishSettingsModal, { getCompleteTranslationLanguageCodeList } from "./PublishSettingsModal";
+import { render, screen } from "@testing-library/react";
+import { MockedComponentObjectForTest } from "@navikt/skjemadigitalisering-shared-components";
+import userEvent from "@testing-library/user-event";
+
+const { createDummyRadioPanel, createFormObject, createPanelObject } = MockedComponentObjectForTest;
+
+jest.mock("../context/i18n/index", () => {
+  const languagesInNorwegian = {
+    "nn-NO": "Norsk nynorsk",
+    en: "Engelsk",
+    pl: "Polsk",
+  };
+
+  const mockGetTranslationsForNavForm = () => ({
+    en: {
+      "Bor du i Norge?": "Do you live in Norway?",
+      Veiledning: "Guidance",
+      "NO-label": "no",
+      "YES-label": "yes",
+      "Dine opplysninger": "Your information",
+      Personinformasjon: "Personal information",
+    },
+    "nn-NO": { "YES-label": "yes" },
+  });
+  const mockUseTranslation = () => ({
+    getTranslationsForNavForm: mockGetTranslationsForNavForm,
+  });
+  return {
+    useTranslations: mockUseTranslation,
+    languagesInNorwegian,
+  };
+});
+
+describe("PublishSettingsModal", () => {
+  let mockedCloseModal;
+  let mockedPublishModal;
+  const renderPublishSettingsModal = (form) => {
+    mockedCloseModal = jest.fn();
+    mockedPublishModal = jest.fn();
+    render(
+      <PublishSettingsModal
+        openModal={true}
+        closeModal={mockedCloseModal}
+        publishModal={mockedPublishModal}
+        form={form}
+      />
+    );
+  };
+
+  it("not render checkbox when there is no complete translation", () => {
+    const form = createFormObject([createPanelObject("test", [createDummyRadioPanel("Bor du i Sverige?")])]);
+    renderPublishSettingsModal(form);
+    expect(screen.queryByRole("checkbox")).not.toBeInTheDocument();
+  });
+
+  it("render only checkbox with English label when only complete English translation is available", async () => {
+    const form = createFormObject(
+      [createPanelObject("Dine opplysninger", [createDummyRadioPanel("Bor du i Norge?")])],
+      "Veiledning"
+    );
+    renderPublishSettingsModal(form);
+    expect(await screen.findByRole("checkbox", { name: "Engelsk (EN)" })).toBeTruthy();
+    expect(screen.queryByRole("checkbox", { name: "Norsk nynorsk (NN-NO)" })).not.toBeInTheDocument();
+  });
+
+  it("publish will send en-languageCode if the English checkbox is checked ", async () => {
+    const form = createFormObject(
+      [createPanelObject("Personinformasjon", [createDummyRadioPanel("Bor du i Norge?")])],
+      "Veiledning"
+    );
+    renderPublishSettingsModal(form);
+    const englishCheckbox = await screen.findByRole("checkbox", { name: "Engelsk (EN)" });
+    expect(englishCheckbox).toBeTruthy();
+    expect(screen.queryByRole("checkbox", { name: "Norsk nynorsk (NN-NO)" })).not.toBeInTheDocument();
+    userEvent.click(englishCheckbox);
+    userEvent.click(screen.getByRole("button", { name: "Publiser" }));
+    expect(mockedPublishModal).toBeCalled();
+    expect(mockedPublishModal.mock.calls[0][0]).toEqual(["en"]);
+  });
+
+  it("publish will send empty array when the English checkbox is checked and checked off", async () => {
+    const form = createFormObject(
+      [createPanelObject("Personinformasjon", [createDummyRadioPanel("Bor du i Norge?")])],
+      "Veiledning"
+    );
+    renderPublishSettingsModal(form);
+    const englishCheckbox = await screen.findByRole("checkbox", { name: "Engelsk (EN)" });
+    expect(englishCheckbox).toBeTruthy();
+    expect(screen.queryByRole("checkbox", { name: "Norsk nynorsk (NN-NO)" })).not.toBeInTheDocument();
+    userEvent.click(englishCheckbox);
+    userEvent.click(englishCheckbox);
+    userEvent.click(screen.getByRole("button", { name: "Publiser" }));
+    expect(mockedPublishModal).toBeCalled();
+    expect(mockedPublishModal.mock.calls[0][0]).toEqual([]);
+  });
+});
 
 describe("getCompleteTranslationLanguageCodeList", () => {
   it("return empty list when there is no form text and translation", () => {
