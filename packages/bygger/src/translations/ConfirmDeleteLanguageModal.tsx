6136e99f0f18879869181882b3eb261ecc954358
Magnus Røed Hestvik <mrh@computas.com>
2021-11-10 14:03:58 +0100

    Add modal for confirming delete language for form

---
+++ packages/bygger/src/translations/ConfirmDeleteLanguageModal.tsx
@@ -0,0 +1,68 @@
+import Modal from "nav-frontend-modal";
+import { Normaltekst, Undertittel } from "nav-frontend-typografi";
+import { Hovedknapp, Knapp } from "nav-frontend-knapper";
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
+
+type ConfirmDeleteLanguageModalProps = {
+  isOpen: boolean;
+  closeModal: () => void;
+  onConfirm: () => void;
+  language: string;
+  isGlobal: boolean;
+};
+
+const useStyles = makeStyles({
+  modal: {
+    width: "50rem",
+    minHeight: "13rem",
+    height: "auto",
+    maxWidth: "90%",
+    padding: "2rem 2.5rem",
+  },
+  buttonRow: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    gap: "1.5rem",
+    margin: "0 auto",
+  },
+  modal_button: {},
+});
+
+const ConfirmDeleteLanguageModal = ({
+  isOpen,
+  closeModal,
+  onConfirm,
+  language,
+  isGlobal = false,
+}: ConfirmDeleteLanguageModalProps): JSX.Element => {
+  const styles = useStyles();
+  return (
+    <Modal
+      className={styles.modal}
+      onRequestClose={closeModal}
+      isOpen={isOpen}
+      contentLabel={"Bekreft sletting av språk"}
+      closeButton={true}
+      shouldCloseOnOverlayClick={false}
+      ariaHideApp={false}
+    >
+      <Undertittel className="margin-bottom-double">Er du sikker på at du ønsker å slette språket?</Undertittel>
+      <Normaltekst className="margin-bottom-default">
+        {`Ved å klikke på "slett språk" fjerner du alle oversettelser til ${language?.toLowerCase()} for dette skjemaet, for godt.
+        Denne handlingen kan ikke angres.`}
+      </Normaltekst>
+      <div className={styles.buttonRow}>
+        <Knapp className={styles.modal_button} onClick={closeModal}>
+          Angre
+        </Knapp>
+        <Hovedknapp className={styles.modal_button} onClick={onConfirm}>
+          Slett språk
+        </Hovedknapp>
+      </div>
+    </Modal>
+  );
+};
+
+export default ConfirmDeleteLanguageModal;
--- packages/bygger/src/translations/TranslationsByFormPage.jsx
+++ packages/bygger/src/translations/TranslationsByFormPage.jsx
@@ -6,13 +6,15 @@ import TranslationsFormPage from "./TranslationsFormPage";
 import useRedirectIfNoLanguageCode from "../hooks/useRedirectIfNoLanguageCode";
 import { getTextsAndTranslationsForForm, getTextsAndTypeForForm, getTextsAndTranslationsHeaders } from "./utils";
 import FormBuilderLanguageSelector from "../context/i18n/FormBuilderLanguageSelector";
-import { useTranslations } from "../context/i18n";
+import { languagesInNorwegian, useTranslations } from "../context/i18n";
 import ActionRow from "../components/layout/ActionRow";
 import Row from "../components/layout/Row";
 import Column from "../components/layout/Column";
 import { makeStyles } from "@material-ui/styles";
 import { UserAlerterContext } from "../userAlerting";
 import { CSVLink } from "react-csv";
+import ConfirmDeleteLanguageModal from "./ConfirmDeleteLanguageModal";
+import { useModal } from "../util/useModal";
 
 const useStyles = makeStyles({
   mainCol: {
@@ -28,6 +30,8 @@ const useStyles = makeStyles({
 });
 
 const TranslationsByFormPage = ({ deleteTranslation, saveTranslation, form, languageCode, projectURL, onLogout }) => {
+  const [isDeleteLanguageModalOpen, setIsDeleteLanguageModalOpen] = useModal();
+
   const userAlerter = useContext(UserAlerterContext);
   const alertComponent = userAlerter.alertComponent();
   const history = useHistory();
@@ -42,68 +46,74 @@ const TranslationsByFormPage = ({ deleteTranslation, saveTranslation, form, lang
   const translationId = (translations[languageCode] || {}).id;
   const styles = useStyles();
   return (
-    <AppLayoutWithContext
-      navBarProps={{
-        title: "Rediger oversettelse",
-        visSkjemaliste: false,
-        visLagNyttSkjema: false,
-        visOversettelseliste: true,
-        logout: onLogout,
-      }}
-    >
-      <ActionRow>
-        <Link className="knapp" to={`/forms/${path}/edit`}>
-          Rediger skjema
-        </Link>
-        <Link className="knapp" to={`/forms/${path}/view${languageCode ? `?lang=${languageCode}` : ""}`}>
-          Forhåndsvis
-        </Link>
-      </ActionRow>
-      <Row>
-        <Column className={styles.mainCol}>
-          <TranslationsFormPage
-            skjemanummer={skjemanummer}
-            translations={translations}
-            languageCode={languageCode}
-            title={title}
-            flattenedComponents={flattenedComponents}
-            setTranslations={setTranslations}
-          />
-        </Column>
-        <div className={styles.sideBarContainer}>
-          <Column className={styles.stickySideBar}>
-            <FormBuilderLanguageSelector formPath={path} label={""} />
-            <Knapp onClick={() => deleteTranslation(translationId).then(() => history.push("/translations"))}>
-              Slett språk
-            </Knapp>
-            <Hovedknapp
-              onClick={() => {
-                saveTranslation(
-                  projectURL,
-                  translationId,
-                  languageCode,
-                  translations[languageCode]?.translations,
-                  path,
-                  title
-                );
-              }}
-            >
-              Lagre
-            </Hovedknapp>
-            {alertComponent && <aside aria-live="polite">{alertComponent()}</aside>}
-            <CSVLink
-              data={getTextsAndTranslationsForForm(form, translations)}
-              filename={`${title}(${path})_Oversettelser.csv`}
-              className="knapp knapp--standard"
-              separator={";"}
-              headers={getTextsAndTranslationsHeaders(translations)}
-            >
-              Eksporter
-            </CSVLink>
+    <>
+      <AppLayoutWithContext
+        navBarProps={{
+          title: "Rediger oversettelse",
+          visSkjemaliste: false,
+          visLagNyttSkjema: false,
+          visOversettelseliste: true,
+          logout: onLogout,
+        }}
+      >
+        <ActionRow>
+          <Link className="knapp" to={`/forms/${path}/edit`}>
+            Rediger skjema
+          </Link>
+          <Link className="knapp" to={`/forms/${path}/view${languageCode ? `?lang=${languageCode}` : ""}`}>
+            Forhåndsvis
+          </Link>
+        </ActionRow>
+        <Row>
+          <Column className={styles.mainCol}>
+            <TranslationsFormPage
+              skjemanummer={skjemanummer}
+              translations={translations}
+              languageCode={languageCode}
+              title={title}
+              flattenedComponents={flattenedComponents}
+              setTranslations={setTranslations}
+            />
           </Column>
-        </div>
-      </Row>
-    </AppLayoutWithContext>
+          <div className={styles.sideBarContainer}>
+            <Column className={styles.stickySideBar}>
+              <FormBuilderLanguageSelector className={styles.languageSelector} formPath={path} label={""} />
+              <Knapp onClick={() => setIsDeleteLanguageModalOpen(true)}>Slett språk</Knapp>
+              <Hovedknapp
+                onClick={() => {
+                  saveTranslation(
+                    projectURL,
+                    translationId,
+                    languageCode,
+                    translations[languageCode]?.translations,
+                    path,
+                    title
+                  );
+                }}
+              >
+                Lagre
+              </Hovedknapp>
+              {alertComponent && <aside aria-live="polite">{alertComponent()}</aside>}
+              <CSVLink
+                data={getTextsAndTranslationsForForm(form, translations)}
+                filename={`${title}(${path})_Oversettelser.csv`}
+                className="knapp knapp--standard"
+                separator={";"}
+                headers={getTextsAndTranslationsHeaders(translations)}
+              >
+                Eksporter
+              </CSVLink>
+            </Column>
+          </div>
+        </Row>
+      </AppLayoutWithContext>
+      <ConfirmDeleteLanguageModal
+        language={languagesInNorwegian[languageCode]}
+        isOpen={isDeleteLanguageModalOpen}
+        closeModal={() => setIsDeleteLanguageModalOpen(false)}
+        onConfirm={() => deleteTranslation(translationId).then(() => history.push("/translations"))}
+      />
+    </>
   );
 };
 
