de239088b14e3e2038a34cc5db8bbcde71fec8e6
Magnus Røed Hestvik <mrh@computas.com>
2021-11-26 17:40:24 +0100

    Add tests for validateIban

--- packages/shared-domain/src/texts/validering.js
+++ packages/shared-domain/src/texts/validering.js
@@ -26,5 +26,5 @@ export const validering = {
   noIBANCountry: "Oppgitt IBAN inneholder ugyldig landkode (to store bokstaver i starten av IBAN-koden)",
   checksumNotNumber: "Oppgitt IBAN er ugyldig fordi sjekksummen ikke er et tall",
   wrongIBANChecksum: "Oppgitt IBAN har feil sjekksum. Sjekk at du har tastet riktig.",
-  invalidIBAN: "Oppgitt IBAN er ugyldig. Sjekk at du har tastet riktig",
+  invalidIBAN: "Oppgitt IBAN er ugyldig. Sjekk at du har tastet riktig.",
 };
