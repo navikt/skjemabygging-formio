f87b4d3c20a0f06195893b0ab8f75f7167eed184
Wenqi Lin <wenqi.lin@nav.no>
2021-11-30 16:33:53 +0100

    add tests for AllForms in Fyllut

---
+++ packages/fyllut/src/components/AllForms.test.jsx
@@ -0,0 +1,57 @@
+import { render, screen } from "@testing-library/react";
+import fetchMock from "jest-fetch-mock";
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+import { AllForms } from "./AllForms";
+
+describe("AllForms", () => {
+  afterEach(() => fetchMock.resetMocks());
+
+  it("Show loading when fetching forms from backend", async () => {
+    fetchMock.mockImplementation((url) => {
+      if (url === "/fyllut/allforms") {
+        return Promise.resolve(new Response(JSON.stringify([])));
+      }
+    });
+
+    render(
+      <MemoryRouter initialEntries={["/fyllut"]}>
+        <AllForms />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByRole("heading", { name: "Laster..." })).toBeInTheDocument();
+  });
+
+  it("Show no form founded when there is no form from backend", async () => {
+    fetchMock.mockImplementation((url) => {
+      if (url === "/fyllut/allforms") return Promise.resolve(new Response(JSON.stringify([])));
+    });
+
+    render(
+      <MemoryRouter initialEntries={["/fyllut"]}>
+        <AllForms />
+      </MemoryRouter>
+    );
+    expect(await screen.findByRole("heading", { name: "Finner ingen skjemaer" })).toBeInTheDocument();
+  });
+
+  it("Show form lists when there are forms", async () => {
+    const mockedForm = [
+      { _id: "000", path: "newform", title: "New form", modified: "2021-11-30T14:10:21.487Z" },
+      { _id: "111", path: "testnewform", title: "Test new form", modified: "2021-11-29T14:10:21.487Z" },
+    ];
+    fetchMock.mockImplementation((url) => {
+      if (url === "/fyllut/allforms") return Promise.resolve(new Response(JSON.stringify(mockedForm)));
+    });
+
+    render(
+      <MemoryRouter initialEntries={["/fyllut"]}>
+        <AllForms />
+      </MemoryRouter>
+    );
+
+    expect(await screen.findByText("New form")).toBeInTheDocument();
+    expect(await screen.findByText("Test new form")).toBeInTheDocument();
+  });
+});
