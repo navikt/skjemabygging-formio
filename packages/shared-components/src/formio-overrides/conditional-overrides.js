2ab88038afcbe2bc536cf5a8b7be377774a7fbeb
Vegard Haugstvedt <it.vegard@gmail.com>
2021-12-01 11:26:26 +0100

    Remove checkCondition override, as the same override is now done in the override of FormUtils.evaluate

--- packages/shared-components/src/formio-overrides/conditional-overrides.js
+++ packages/shared-components/src/formio-overrides/conditional-overrides.js
@@ -1,16 +1,3 @@
-import { Utils } from "formiojs";
-import cloneDeep from "lodash.clonedeep";
-import set from "lodash.set";
-
-const {
-  getComponentPath,
-  getDataParentComponent,
-  getComponentPathWithoutIndicies,
-  checkCustomConditional,
-  checkSimpleConditional,
-  checkJsonConditional,
-} = Utils;
-
 function addNullChecksToChainedLookup(chainedLookup, originalString) {
   const chainedLookupParts = chainedLookup.split(".");
   let safeChainedLookup = chainedLookupParts[0];
@@ -37,55 +24,4 @@ function sanitizeJavaScriptCode(text) {
   return mapChainedLookups(text);
 }
 
-function setPathToComponentAndPerentSchema(component) {
-  component.path = getComponentPath(component);
-  const dataParent = getDataParentComponent(component);
-  if (dataParent && typeof dataParent === "object") {
-    dataParent.path = getComponentPath(dataParent);
-  }
-}
-
-function getRow(component, row, instance, conditional) {
-  const condition = conditional || component.conditional;
-  // If no component's instance passed (happens only in 6.x server), calculate its path based on the schema
-  if (!instance) {
-    instance = cloneDeep(component);
-    setPathToComponentAndPerentSchema(instance);
-  }
-  const dataParent = getDataParentComponent(instance);
-  const parentPathWithoutIndicies =
-    dataParent && dataParent.path ? getComponentPathWithoutIndicies(dataParent.path) : null;
-  if (dataParent && condition.when && condition.when.startsWith(parentPathWithoutIndicies)) {
-    const newRow = {};
-    set(newRow, parentPathWithoutIndicies, row);
-    row = newRow;
-  }
-
-  return row;
-}
-
-function checkConditionOverride(component, row, data, form, instance) {
-  const { customConditional, conditional } = component;
-  if (customConditional) {
-    return checkCustomConditional(
-      component,
-      sanitizeJavaScriptCode(customConditional),
-      row,
-      data,
-      form,
-      "show",
-      true,
-      instance
-    );
-  } else if (conditional && conditional.when) {
-    row = getRow(component, row, instance);
-    return checkSimpleConditional(component, conditional, row, data);
-  } else if (conditional && conditional.json) {
-    return checkJsonConditional(component, conditional.json, row, data, form, true);
-  }
-
-  // Default to show.
-  return true;
-}
-
-export { sanitizeJavaScriptCode, checkConditionOverride };
+export { sanitizeJavaScriptCode };
