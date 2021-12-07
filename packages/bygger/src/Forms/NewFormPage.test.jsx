fc0e6387c51fc1603784f5acf36319e38568d689
Vegard Haugstvedt <it.vegard@gmail.com>
2021-12-07 09:47:40 +0100

    Fix NewFormPage.test.jsx, so it handles sending in Formio instead of an onCreate function

--- packages/bygger/src/Forms/NewFormPage.test.jsx
+++ packages/bygger/src/Forms/NewFormPage.test.jsx
@@ -1,36 +1,35 @@
-import React from 'react'
-import {MemoryRouter} from "react-router-dom";
-import {render, waitFor, screen} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import {AppConfigProvider} from "@navikt/skjemadigitalisering-shared-components";
-import NewFormPage from "./NewFormPage";
-import {UserAlerterContext} from "../userAlerting";
+import { AppConfigProvider } from "@navikt/skjemadigitalisering-shared-components";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
 import featureToggles from "../featureToggles";
+import { UserAlerterContext } from "../userAlerting";
+import NewFormPage from "./NewFormPage";
 
-describe('NewFormPage', () => {
-
-  it('should create a new form with correct path, title and name', async () => {
-    const userAlerter = {flashSuccessMessage: jest.fn(), alertComponent: jest.fn()};
-    const onCreate = jest.fn();
+describe("NewFormPage", () => {
+  it("should create a new form with correct path, title and name", async () => {
+    const userAlerter = { flashSuccessMessage: jest.fn(), alertComponent: jest.fn() };
+    const saveForm = jest.fn(() => Promise.resolve(new Response(JSON.stringify({}))));
     const onLogout = jest.fn();
     render(
       <MemoryRouter>
         <UserAlerterContext.Provider value={userAlerter}>
           <AppConfigProvider featureToggles={featureToggles}>
-            <NewFormPage onCreate={onCreate} onLogout={onLogout}/>
+            <NewFormPage formio={{ saveForm }} onLogout={onLogout} />
           </AppConfigProvider>
         </UserAlerterContext.Provider>
       </MemoryRouter>
     );
-    await waitFor(() => screen.getByText('Opprett nytt skjema'))
+    await waitFor(() => screen.getByText("Opprett nytt skjema"));
 
-    await userEvent.type(screen.getByLabelText('Skjemanummer'), 'NAV 10-20.30');
-    await userEvent.type(screen.getByLabelText('Tittel'), 'Et testskjema');
-    await userEvent.type(screen.getByLabelText('Temakode'), 'BIL');
-    await userEvent.click(screen.getByRole('button', {name: 'Opprett'}));
+    await userEvent.type(screen.getByLabelText("Skjemanummer"), "NAV 10-20.30");
+    await userEvent.type(screen.getByLabelText("Tittel"), "Et testskjema");
+    await userEvent.type(screen.getByLabelText("Temakode"), "BIL");
+    await userEvent.click(screen.getByRole("button", { name: "Opprett" }));
 
-    expect(onCreate.mock.calls).toHaveLength(1);
-    const savedForm = onCreate.mock.calls[0][0];
+    expect(saveForm).toHaveBeenCalledTimes(1);
+    const savedForm = saveForm.mock.calls[0][0];
     expect(savedForm).toMatchObject({
       type: "form",
       path: "nav102030",
@@ -38,10 +37,9 @@ describe('NewFormPage', () => {
       name: "nav102030",
       title: "Et testskjema",
       tags: ["nav-skjema", ""],
-    });
-    expect(savedForm.properties).toMatchObject({
-      skjemanummer: "NAV 10-20.30"
+      properties: {
+        skjemanummer: "NAV 10-20.30",
+      },
     });
   });
-
 });
