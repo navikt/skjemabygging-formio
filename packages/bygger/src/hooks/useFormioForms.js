33b972260c7e24e712c320d00217103b231f57f7
Vegard Haugstvedt <it.vegard@gmail.com>
2021-12-10 14:49:36 +0100

    Memoize functions returned by useFormioForms - this prevents re-renders because functions change
    Specifically, changes on EditFormPage was lost when navigating to TestFormPage before this change.

--- packages/bygger/src/hooks/useFormioForms.js
+++ packages/bygger/src/hooks/useFormioForms.js
@@ -1,7 +1,8 @@
 import Formiojs from "formiojs/Formio";
+import { useCallback } from "react";
 
 export const useFormioForms = (formio, userAlerter) => {
-  const loadFormsList = () => {
+  const loadFormsList = useCallback(() => {
     return formio.loadForms({
       params: {
         type: "form",
@@ -10,51 +11,65 @@ export const useFormioForms = (formio, userAlerter) => {
         select: "title, path, tags, properties, modified, _id",
       },
     });
-  };
+  }, [formio]);
 
-  const loadForm = (formPath) => {
-    return formio
-      .loadForms({
-        params: {
-          type: "form",
-          tags: "nav-skjema",
-          path: formPath,
-          limit: 1,
-        },
-      })
-      .then((forms) => forms[0]);
-  };
+  const loadForm = useCallback(
+    (formPath) => {
+      return formio
+        .loadForms({
+          params: {
+            type: "form",
+            tags: "nav-skjema",
+            path: formPath,
+            limit: 1,
+          },
+        })
+        .then((forms) => forms[0]);
+    },
+    [formio]
+  );
 
-  const onSave = (callbackForm) => {
-    formio.saveForm({ ...callbackForm, display: "wizard" }).then((form) => {
-      userAlerter.flashSuccessMessage("Lagret skjema " + form.title);
-      return form;
-    });
-  };
+  const onSave = useCallback(
+    (callbackForm) => {
+      formio.saveForm({ ...callbackForm, display: "wizard" }).then((form) => {
+        userAlerter.flashSuccessMessage("Lagret skjema " + form.title);
+        return form;
+      });
+    },
+    [formio, userAlerter]
+  );
 
-  const deleteForm = async (formId, tags, title) => {
-    formio.saveForm({ _id: formId, tags: tags.filter((each) => each !== "nav-skjema") }).then(() => {
-      userAlerter.flashSuccessMessage("Slettet skjemaet " + title);
-    });
-  };
-  const onPublish = async (form, translations) => {
-    const payload = JSON.stringify({
-      form: form,
-      translations: translations,
-      token: Formiojs.getToken(),
-    });
-    const response = await fetch(`/api/publish/${form.path}`, {
-      method: "PUT",
-      headers: { "content-type": "application/json" },
-      body: payload,
-    });
-    if (response.ok) {
-      userAlerter.flashSuccessMessage("Satt i gang publisering, dette kan ta noen minutter.");
-    } else {
-      userAlerter.setErrorMessage("Publisering feilet " + response.status);
-      console.error("Publisering feilet " + response.status);
-    }
-  };
+  const deleteForm = useCallback(
+    async (formId, tags, title) => {
+      formio.saveForm({ _id: formId, tags: tags.filter((each) => each !== "nav-skjema") }).then(() => {
+        userAlerter.flashSuccessMessage("Slettet skjemaet " + title);
+      });
+    },
+    [formio, userAlerter]
+  );
+
+  const onPublish = useCallback(
+    async (form, translations) => {
+      const payload = JSON.stringify({
+        form: form,
+        translations: translations,
+        token: Formiojs.getToken(),
+      });
+      const response = await fetch(`/api/publish/${form.path}`, {
+        method: "PUT",
+        headers: { "content-type": "application/json" },
+        body: payload,
+      });
+      if (response.ok) {
+        userAlerter.flashSuccessMessage("Satt i gang publisering, dette kan ta noen minutter.");
+      } else {
+        userAlerter.setErrorMessage("Publisering feilet " + response.status);
+        console.error("Publisering feilet " + response.status);
+      }
+    },
+    [userAlerter]
+  );
+
   return {
     deleteForm,
     loadForm,
