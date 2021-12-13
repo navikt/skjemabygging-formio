10a7588b451a568d1675518d4ffb5ebcc497cc0e
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2021-12-13 15:04:16 +0100

    Trekk ut jest-config i egen fil

---
+++ packages/shared-components/jest.config.js
@@ -0,0 +1,24 @@
+import {defaults} from "jest-config";
+
+const config = {
+  testEnvironment: "jsdom",
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ejs'],
+
+  setupFilesAfterEnv: [
+    "<rootDir>/setupTests.js"
+  ],
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}"
+  ],
+  moduleNameMapper: {
+    "nav-(.*)-style": "<rootDir>/__mocks__/styleMock.js",
+    "nav-frontend-core": "<rootDir>/__mocks__/styleMock.js",
+    "nav-datovelger": "<rootDir>/__mocks__/styleMock.js",
+    "@navikt/ds-icons": "<rootDir>/__mocks__/dsIconsMock.js",
+  },
+  transform: {
+    "\\.[j|t]sx?$": "babel-jest"
+  }
+};
+
+export default config;
