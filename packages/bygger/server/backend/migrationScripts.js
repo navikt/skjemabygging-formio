712e21307c6cad651eb2128cbb4be7988f95d118
Vegard Haugstvedt <it.vegard@gmail.com>
2022-01-11 15:59:58 +0100

    Add functionality for migrating subcomponents

--- packages/bygger/server/backend/migrationScripts.js
+++ packages/bygger/server/backend/migrationScripts.js
@@ -2,15 +2,28 @@ function componentMatchesSearchFilters(component, searchFilters) {
   return Object.keys(searchFilters).every((property) => component[property] === searchFilters[property]);
 }
 
-function migrateForm(form, options, script) {
+function recursivelyMigrateComponentAndSubcomponents(component, searchFilters, script) {
+  let modifiedComponent = component;
+  if (componentMatchesSearchFilters(component, searchFilters)) {
+    modifiedComponent = script(component);
+  }
+  if (modifiedComponent.components) {
+    return {
+      ...modifiedComponent,
+      components: modifiedComponent.components.map((subComponent) =>
+        recursivelyMigrateComponentAndSubcomponents(subComponent, searchFilters, script)
+      ),
+    };
+  }
+  return modifiedComponent;
+}
+
+function migrateForm(form, searchFilters, script) {
   return {
     ...form,
-    components: form.components.map((component) => {
-      if (componentMatchesSearchFilters(component, options)) {
-        return script(component);
-      }
-      return component;
-    }),
+    components: form.components.map((component) =>
+      recursivelyMigrateComponentAndSubcomponents(component, searchFilters, script)
+    ),
   };
 }
 
--- packages/bygger/server/backend/migrationScripts.test.js
+++ packages/bygger/server/backend/migrationScripts.test.js
@@ -1,5 +1,35 @@
 import { migrateForm } from "./migrationScripts";
 
+const originalPanelComponent = {
+  title: "Veiledning",
+  breadcrumbClickable: true,
+  buttonSettings: {
+    previous: true,
+    cancel: true,
+    next: true,
+  },
+  navigateOnEnter: false,
+  saveOnEnter: false,
+  scrollToTop: false,
+  collapsible: false,
+  key: "veiledning",
+  type: "panel",
+  label: "Veiledning",
+  input: false,
+  components: [],
+  tableView: false,
+};
+
+const originalSkjemaGruppeComponent = {
+  legend: "Skjemagruppe",
+  key: "navSkjemagruppe",
+  type: "navSkjemagruppe",
+  label: "Skjemagruppe",
+  input: false,
+  tableView: false,
+  components: [],
+};
+
 const originalTextFieldComponent = {
   label: "Fornavn",
   fieldSize: "input--xxl",
@@ -36,16 +66,18 @@ const originalForm = {
   components: [originalFodselsnummerComponent, originalTextFieldComponent],
 };
 
+const migrateFnrFieldFunction = (component) => ({
+  ...component,
+  validate: {
+    ...component.validate,
+    custom: "valid = instance.newValidateFnr(input)",
+  },
+});
+
 describe("Migration scripts", () => {
   describe("migrateForm", () => {
     it("can update component based on type", () => {
-      const actual = migrateForm(originalForm, { type: "fnrfield" }, (component) => ({
-        ...component,
-        validate: {
-          ...component.validate,
-          custom: "valid = instance.newValidateFnr(input)",
-        },
-      }));
+      const actual = migrateForm(originalForm, { type: "fnrfield" }, migrateFnrFieldFunction);
       expect(actual).toEqual({
         path: "test-form",
         components: [
@@ -57,6 +89,83 @@ describe("Migration scripts", () => {
             },
           },
           originalTextFieldComponent,
+        ],
+      });
+    });
+
+    it("can migrate subcomponents", () => {
+      const actual = migrateForm(
+        {
+          path: "test-form",
+          components: [
+            {
+              ...originalPanelComponent,
+              components: [originalFodselsnummerComponent],
+            },
+          ],
+        },
+        { type: "fnrfield" },
+        migrateFnrFieldFunction
+      );
+
+      expect(actual).toEqual({
+        path: "test-form",
+        components: [
+          {
+            ...originalPanelComponent,
+            components: [
+              {
+                ...originalFodselsnummerComponent,
+                validate: {
+                  custom: "valid = instance.newValidateFnr(input)",
+                  required: true,
+                },
+              },
+            ],
+          },
+        ],
+      });
+    });
+
+    it("can migrate subcomponents of a migrated component", () => {
+      const actual = migrateForm(
+        {
+          path: "test-form",
+          components: [
+            {
+              ...originalSkjemaGruppeComponent,
+              components: [
+                {
+                  ...originalSkjemaGruppeComponent,
+                  key: "subSkjemaGruppe",
+                  components: [originalTextFieldComponent],
+                },
+              ],
+            },
+          ],
+        },
+        { type: "navSkjemagruppe" },
+        (component) => ({
+          ...component,
+          modifiedByTest: true,
+        })
+      );
+
+      expect(actual).toEqual({
+        path: "test-form",
+        components: [
+          {
+            ...originalSkjemaGruppeComponent,
+            modifiedByTest: true,
+            components: [
+              {
+                ...originalSkjemaGruppeComponent,
+                key: "subSkjemaGruppe",
+                modifiedByTest: true,
+                components: [originalTextFieldComponent],
+              },
+            ],
+          },
         ],
       });
     });
