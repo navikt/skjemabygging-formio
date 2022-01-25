4cf733aa7a19080d437f908ee0b23c81fd1aa3dc
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2022-01-25 09:28:16 +0100

    Validering av fødselsnummer og D-nummer

--- packages/shared-components/src/customComponents/components/Fodselsnummer.test.js
+++ packages/shared-components/src/customComponents/components/Fodselsnummer.test.js
@@ -1,11 +1,48 @@
-import { computeK1, computeK2 } from "./Fodselsnummer";
+import { TEXTS } from "@navikt/skjemadigitalisering-shared-domain";
+import Fodselsnummer from "./Fodselsnummer";
 
-it("should generate a valid D-nummer", () => {
-  const rawData = "410170000";
-  const k1 = computeK1(rawData);
-  const slightlyCooked = rawData + k1.toString();
-  expect(slightlyCooked).toEqual("4101700001");
-  const k2 = computeK2(slightlyCooked);
-  const completelyCooked = slightlyCooked + k2.toString();
-  expect(completelyCooked).toEqual("41017000010");
+describe("Fodselsnummer", () => {
+  describe("validering", () => {
+    let fnrComp;
+
+    beforeEach(() => {
+      fnrComp = new Fodselsnummer();
+      jest.spyOn(Fodselsnummer.prototype, "t").mockImplementation((text) => text);
+    });
+
+    it("successfully validates a fnr", () => {
+      expect(fnrComp.validateFnr("13097248022")).toBe(true);
+      expect(fnrComp.validateFnrNew("13097248022")).toBe(true);
+    });
+
+    it("successfully validates empty fnr", () => {
+      expect(fnrComp.validateFnr("")).toBe(true);
+      expect(fnrComp.validateFnrNew("")).toBe(true);
+    });
+
+    it("fails validation for invalid fnr", () => {
+      expect(fnrComp.validateFnr("13097248023")).toBe(false);
+      expect(fnrComp.validateFnrNew("13097248023")).toEqual(TEXTS.validering.fodselsnummerDNummer);
+    });
+
+    it("successfully validates a fnr containing space", () => {
+      expect(fnrComp.validateFnr("130972 48022")).toBe(true);
+      expect(fnrComp.validateFnrNew("130972 48022")).toBe(true);
+    });
+
+    it("successfully validates a dnr", () => {
+      expect(fnrComp.validateFnr("53097248016")).toBe(true);
+      expect(fnrComp.validateFnrNew("53097248016")).toBe(true);
+    });
+
+    it("fails validation for 12345678911", () => {
+      expect(fnrComp.validateFnr("12345678911")).toBe(false);
+      expect(fnrComp.validateFnrNew("12345678911")).toEqual(TEXTS.validering.fodselsnummerDNummer);
+    });
+
+    it("fails validation for 00000000000", () => {
+      expect(fnrComp.validateFnr("00000000000")).toBe(false);
+      expect(fnrComp.validateFnrNew("00000000000")).toEqual(TEXTS.validering.fodselsnummerDNummer);
+    });
+  });
 });
