563875e4a6908ee29518c68f7d051490e9142bad
Vegard Haugstvedt <it.vegard@gmail.com>
2022-01-27 15:30:44 +0100

    Make the diff-tool for migrations more fine-grained: Only compare actual values that have changed, not entire objects
    Makes it so changing validate.custom only compares validate.custom old and new, instead of the entire validate object.

--- packages/bygger/server/backend/migrationScripts.js
+++ packages/bygger/server/backend/migrationScripts.js
@@ -35,10 +35,21 @@ function migrateForm(form, searchFilters, script) {
 }
 
 function generateDiff(originalComponent, editedComponent) {
-  return Object.keys(originalComponent).reduce((acc, key) => {
+  return Object.keys({... originalComponent, ...editedComponent}).reduce((acc, key) => {
     if (key === "id") return { ...acc, [key]: originalComponent[key] };
-    if (originalComponent[key] !== editedComponent[key])
-      return { ...acc, [key]: { _ORIGINAL: originalComponent[key], _NEW: editedComponent[key] } };
+    if (originalComponent[key] !== editedComponent[key]) {
+      if (typeof originalComponent[key] === "object" && !Array.isArray(originalComponent[key]) && originalComponent[key] !== null) {
+        return {
+          ...acc,
+          [key]: generateDiff(originalComponent[key], editedComponent[key])
+        }
+      }
+      return {
+        ...acc,
+        [`${key}_ORIGINAL`]: originalComponent[key],
+        [`${key}_NEW`]: editedComponent[key]
+      };
+    }
     if (key === "key" || key === "label") return { ...acc, [key]: originalComponent[key] };
     return acc;
   }, {});
