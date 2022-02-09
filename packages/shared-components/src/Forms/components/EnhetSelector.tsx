7593d6ae2005680e6c0a8a4eadd72dad526497a3
Magnus Røed Hestvik <mrh@computas.com>
2022-02-09 12:16:32 +0100

    Add react-select for enhetnummer

--- packages/shared-components/src/Forms/components/EnhetSelector.tsx
+++ packages/shared-components/src/Forms/components/EnhetSelector.tsx
@@ -1,41 +1,56 @@
+import { makeStyles } from "@material-ui/styles";
 import { TEXTS } from "@navikt/skjemadigitalisering-shared-domain";
-import { Select } from "nav-frontend-skjema";
 import React from "react";
+import ReactSelect from "react-select";
 import { EnhetInkludertKontaktinformasjon } from "../../api/Enhet";
 import { useLanguages } from "../../context/languages";
 
-interface EnhetSelectorProps {
-  enhetsListe: EnhetInkludertKontaktinformasjon[];
-  onSelectEnhet: (enhet?: EnhetInkludertKontaktinformasjon) => void;
-  error?: string;
-}
+const useStyles = makeStyles({
+  enhetsListe: {
+    maxWidth: "20rem",
+  },
+});
 
 const EnhetSelector = ({ enhetsListe = [], onSelectEnhet, error }: EnhetSelectorProps) => {
   const { translate } = useLanguages();
+  const styles = useStyles();
 
   if (enhetsListe.length === 0) {
     return <></>;
   }
 
+  const options = enhetsListe.map((enhet) => ({ label: enhet.enhet.navn, value: enhet.enhet.enhetNr }));
   return (
-    <Select
-      className="margin-bottom-default"
-      bredde="l"
-      label={translate(TEXTS.statiske.prepareLetterPage.chooseEntity)}
-      description={translate(TEXTS.statiske.prepareLetterPage.chooseEntityDescription)}
-      feil={error}
-      onChange={(event) => {
-        onSelectEnhet(enhetsListe.find((enhet) => `${enhet.enhet.enhetId}` === event.target.value));
-      }}
-    >
-      <option>{translate(TEXTS.statiske.prepareLetterPage.selectEntityDefault)}</option>
-      {enhetsListe.map((enhet) => (
-        <option key={enhet.enhet.enhetId} value={enhet.enhet.enhetId}>
-          {enhet.enhet.navn}
-        </option>
-      ))}
-    </Select>
+    <div className="skjemaelement margin-bottom-default">
+      <label id="enhetSelectLabel" className="skjemaelement__label">
+        {translate(TEXTS.statiske.prepareLetterPage.chooseEntity)}
+      </label>
+      <div className="skjemaelement__description">
+        {translate(TEXTS.statiske.prepareLetterPage.chooseEntityDescription)}
+      </div>
+      <ReactSelect
+        className={styles.enhetsListe}
+        options={options}
+        placeholder={translate(TEXTS.statiske.prepareLetterPage.selectEntityDefault)}
+        aria-errormessage="enhetSelectError"
+        aria-labelledby="enhetSelectLabel"
+        onChange={(event) => {
+          onSelectEnhet(event && event.value);
+        }}
+      />
+      {error && (
+        <div id="enhetSelectError" className="skjemaelement__feilmelding">
+          <p className="typo-feilmelding">{error}</p>
+        </div>
+      )}
+    </div>
   );
 };
+
+interface EnhetSelectorProps {
+  enhetsListe: EnhetInkludertKontaktinformasjon[];
+  onSelectEnhet: (enhetNummer: string | null) => void;
+  error?: string;
+}
 
 export default EnhetSelector;
