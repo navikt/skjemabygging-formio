fe2221d12e4a36afb3339ec11c8c1790cb98c81a
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2022-03-07 12:17:52 +0100

    Omstruktuerering av fyllut backend. Trekke ut logikk for hvert endepunkt, og plasser de under routers-mappen. Eksponere en del av endepunktene også under /api slik at vi gradvis kan gå over til disse; dette for å tydeliggjøre hva som er tjenester, og hva som returnerer statiske ressurseri, og minske sjansen for at det håndteres feilaktig.

---
+++ packages/fyllut/server/middleware/httpRequestLogger.js
@@ -0,0 +1,20 @@
+import ecsFormat from "@elastic/ecs-morgan-format";
+import correlator from "express-correlation-id";
+import morgan from "morgan";
+import { clean } from "../utils/logCleaning.js";
+
+const INTERNAL_PATHS = /.*\/internal\/.*/i;
+const httpRequestLogger = morgan(
+  (token, req, res) => {
+    const logEntry = JSON.parse(ecsFormat({ apmIntegration: false })(token, req, res));
+    logEntry.correlation_id = correlator.getId();
+    return JSON.stringify(clean(logEntry));
+  },
+  {
+    skip: (req) => {
+      return INTERNAL_PATHS.test(req.originalUrl);
+    },
+  }
+);
+
+export default httpRequestLogger;
---
+++ packages/fyllut/server/routers/api/translations.js
@@ -0,0 +1,28 @@
+import fetch from "node-fetch";
+import { config } from "../../config/config.js";
+import { loadFileFromDirectory } from "../../utils/forms.js";
+
+const { useFormioApi, formioProjectUrl, translationDir } = config;
+
+const fetchTranslationsFromFormioApi = async (formPath) => {
+  const response = await fetch(`${formioProjectUrl}/language/submission?data.form=${formPath}&limit=1000`, {
+    method: "GET",
+  });
+  if (response.ok) {
+    const translationsForForm = await response.json();
+    return translationsForForm.reduce((acc, obj) => ({ ...acc, [obj.data.language]: { ...obj.data.i18n } }), {});
+  }
+  return {};
+};
+
+const loadTranslations = async (formPath) => {
+  return useFormioApi
+    ? await fetchTranslationsFromFormioApi(formPath)
+    : await loadFileFromDirectory(translationDir, formPath);
+};
+
+const translations = {
+  get: async (req, res) => res.json(await loadTranslations(req.params.form)),
+};
+
+export default translations;
