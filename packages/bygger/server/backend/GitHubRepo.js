b2760121fd266396ff10af29fe66129f9c0a6423
Magnus Røed Hestvik <mrh@computas.com>
2022-03-17 12:45:17 +0100

    Create class for performing actions on a specific repo

---
+++ packages/bygger/server/backend/GitHubRepo.js
@@ -0,0 +1,71 @@
+import { Octokit } from "octokit";
+
+export class GitHubRepo {
+  constructor(owner, repo, personalAccessToken) {
+    this.owner = owner;
+    this.repo = repo;
+    this.octokit = new Octokit({ auth: personalAccessToken });
+  }
+
+  getRef(branch) {
+    return this.octokit.rest.git.getRef({
+      owner: this.owner,
+      repo: this.repo,
+      ref: `heads/${branch}`,
+    });
+  }
+
+  createRef(branch, sha) {
+    return this.octokit.rest.git.createRef({
+      owner: this.owner,
+      repo: this.repo,
+      ref: `refs/heads/${branch}`,
+      sha,
+    });
+  }
+
+  deleteRef(branch) {
+    return this.octokit.rest.git.deleteRef({ owner: this.owner, repo: this.repo, ref: `heads/${branch}` });
+  }
+
+  async getFileIfItExists(ref, path) {
+    let remoteFileContent;
+    try {
+      remoteFileContent = await this.octokit.rest.repos.getContent({ owner: this.owner, repo: this.repo, ref, path });
+    } catch (e) {}
+    return remoteFileContent;
+  }
+
+  createOrUpdateFileContents(branch, path, message, content, sha) {
+    let parameters = {
+      owner: this.owner,
+      repo: this.repo,
+      branch,
+      path,
+      message,
+      content,
+    };
+    if (sha) {
+      parameters = { ...parameters, sha };
+    }
+    return this.octokit.rest.repos.createOrUpdateFileContents(parameters);
+  }
+
+  createPullRequest(title, head, base) {
+    return this.octokit.rest.pulls.create({
+      owner: this.owner,
+      repo: this.repo,
+      title,
+      head,
+      base,
+    });
+  }
+
+  mergePullRequest(pull_number) {
+    return this.octokit.rest.pulls.merge({
+      owner: this.owner,
+      repo: this.repo,
+      pull_number,
+    });
+  }
+}
--- packages/bygger/server/backend/index.js
+++ packages/bygger/server/backend/index.js
@@ -1,17 +1,17 @@
 import { guid } from "nav-frontend-js-utils";
-import { Octokit } from "octokit";
 import qs from "qs";
-import { promisify } from "util";
-import { gunzip, gzip } from "zlib";
+// import { promisify } from "util";
+// import { gunzip, gzip } from "zlib";
 import { fetchWithErrorHandling, stringTobase64 } from "./fetchUtils.js";
-const promisifiedGzip = promisify(gzip);
-const promisifiedGunzip = promisify(gunzip);
+import { GitHubRepo } from "./GitHubRepo.js";
+// const promisifiedGzip = promisify(gzip);
+// const promisifiedGunzip = promisify(gunzip);
 
 export class Backend {
   constructor(projectURL, config) {
     this.projectURL = projectURL;
     this.config = config;
-    this.octokit = new Octokit({ auth: config.workflowDispatchToken });
+    this.skjemaUtfylling = new GitHubRepo("navikt", "skjemapublisering-monorepo-poc", config.workflowDispatchToken);
   }
 
   ho() {
@@ -26,7 +26,7 @@ export class Backend {
     return this.config.workflowDispatchURL;
   }
 
-  async toBase64GzipAndJson(data) {
+  /*  async toBase64GzipAndJson(data) {
     const buffer = Buffer.from(JSON.stringify(data), "utf-8");
     const zippedBuffer = await promisifiedGzip(buffer);
     return zippedBuffer.toString("base64");
@@ -36,9 +36,9 @@ export class Backend {
     const buffer = Buffer.from(string, "base64");
     const inflated = await promisifiedGunzip(buffer);
     return JSON.parse(inflated.toString());
-  }
+  }*/
 
-  async payload(formJsonFileTitle, form, translations) {
+  /*  async payload(formJsonFileTitle, form, translations) {
     const encodedForm = await this.toBase64GzipAndJson(form);
     const encodedTranslations = await this.toBase64GzipAndJson(translations);
     return {
@@ -51,7 +51,7 @@ export class Backend {
         monorepoGitHash: this.config.gitSha,
       },
     };
-  }
+  }*/
 
   async checkUpdateAndPublishingAccess(userToken) {
     //Her kan vi vurdere nærmere sjekk, men man når ikke denne siden uten å være pålogget.
@@ -81,30 +81,14 @@ export class Backend {
     );
   }
 
-  async getRemoteFileIfItExists(owner, repo, ref, path) {
-    let remoteFileContent;
-    try {
-      remoteFileContent = await this.octokit.rest.repos.getContent({ owner, repo, ref, path });
-    } catch (e) {}
-    return remoteFileContent;
-  }
-
   areEqualWithoutWhiteSpaces(string1, string2) {
     return string1.replace(/\s/g, "") === string2.replace(/\s/g, "");
   }
 
-  async pushJsonFileToRepo(owner, repo, branch, path, message, file) {
+  async pushJsonFileToRepo(branch, path, message, file) {
     const content = stringTobase64(JSON.stringify(file), "utf-8");
-    const remoteFile = await this.getRemoteFileIfItExists(owner, repo, branch, path);
+    const remoteFile = await this.skjemaUtfylling.getFileIfItExists(branch, path);
     const sha = remoteFile && remoteFile.data && remoteFile.data.sha;
-    const params = {
-      owner,
-      repo,
-      branch,
-      path,
-      message,
-      content,
-    };
 
     if (
       remoteFile &&
@@ -116,71 +100,40 @@ export class Backend {
       return Promise.resolve(undefined);
     }
 
-    if (sha) {
-      // The file exists remotely. Update content.
-      const updateResult = await this.octokit.rest.repos.createOrUpdateFileContents({ ...params, sha });
-      return updateResult.data.commit.sha;
-    }
-
-    // The file doesn't exist remotely. Create new file.
-    const createResult = await this.octokit.rest.repos.createOrUpdateFileContents(params);
+    const createResult = await this.skjemaUtfylling.createOrUpdateFileContents(branch, path, message, content, sha);
     return createResult.data.commit.sha;
   }
 
-  async performChangesOnSeparateBranch(owner, repo, base, branch, performChanges) {
-    const baseBranch = await this.octokit.rest.git.getRef({
-      owner,
-      repo,
-      ref: `heads/${base}`,
-    });
+  async performChangesOnSeparateBranch(base, branch, performChanges) {
+    const baseBranch = await this.skjemaUtfylling.getRef(base);
+    await this.skjemaUtfylling.createRef(branch, baseBranch.data.object.sha);
 
-    await this.octokit.rest.git.createRef({
-      owner,
-      repo,
-      ref: `refs/heads/${branch}`,
-      sha: baseBranch.data.object.sha,
-    });
+    await performChanges(branch);
 
-    await performChanges(owner, repo, branch);
-
-    const currentRef = await this.octokit.rest.git.getRef({ owner, repo, ref: `heads/${branch}` });
+    const currentRef = await this.skjemaUtfylling.getRef(branch);
     let updatedBaseSha;
 
     if (baseBranch.data.object.sha !== currentRef.data.object.sha) {
       // Only create and merge pull request if the branch contains changes, compared to the base branch
-      const pullRequest = await this.octokit.rest.pulls.create({
-        owner,
-        repo,
-        title: "Automatic publishing job",
-        head: branch,
-        base,
-      });
-      await this.octokit.rest.pulls.merge({
-        owner,
-        repo,
-        pull_number: pullRequest.data.number,
-      });
-      const updatedBase = await this.octokit.rest.git.getRef({ owner, repo, ref: `heads/${base}` });
+      const pullRequest = await this.skjemaUtfylling.createPullRequest("Automatic publishing job", branch, base);
+      await this.skjemaUtfylling.mergePullRequest(pullRequest.data.number);
+      const updatedBase = await this.skjemaUtfylling.getRef(base);
       updatedBaseSha = updatedBase.data.object.sha;
     }
 
-    await this.octokit.rest.git.deleteRef({ owner, repo, ref: `heads/${branch}` });
+    await this.skjemaUtfylling.deleteRef(branch);
     return updatedBaseSha;
   }
 
   pushFormAndTranslationsCallback(formPath, form, translations) {
-    return async (owner, repo, branch) => {
+    return async (branch) => {
       await this.pushJsonFileToRepo(
-        owner,
-        repo,
         branch,
         `translations/${formPath}.json`,
         `[Publisering] skjema "${form.title}", monorepo ref: ${this.config.gitSha}`,
         translations
       );
       await this.pushJsonFileToRepo(
-        owner,
-        repo,
         branch,
         `forms/${formPath}.json`,
         `[Publisering] oversettelse "${form.title}", monorepo ref: ${this.config.gitSha}`,
@@ -192,8 +145,6 @@ export class Backend {
   async publishForm(userToken, form, translations, formPath) {
     await this.checkUpdateAndPublishingAccess(userToken);
     return this.performChangesOnSeparateBranch(
-      "navikt",
-      "skjemapublisering-monorepo-poc",
       "test-publish",
       `publish-${formPath}--${guid()}`,
       this.pushFormAndTranslationsCallback(formPath, form, translations)
@@ -203,8 +154,6 @@ export class Backend {
   async publishResource(userToken, resourceName, resourceContent) {
     await this.checkUpdateAndPublishingAccess(userToken);
     return this.pushJsonFileToRepo(
-      "navikt",
-      "skjemapublisering-monorepo-poc",
       "test-publish",
       `resources/${resourceName}.json`,
       `[Publisering] ressurs "${resourceName}", monorepo ref: ${this.config.gitSha}`,
