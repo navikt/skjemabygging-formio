6680ced5287b73f0f4315848958b26f2c40547d4
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2022-03-22 11:12:44 +0100

    useLocation instead of window.location to retrieve search params

--- packages/shared-components/src/Forms/SubmissionWrapper.jsx
+++ packages/shared-components/src/Forms/SubmissionWrapper.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
+import { Redirect, useLocation } from "react-router-dom";
 
 export const SubmissionWrapper = ({ submission, url, children }) => {
+  const { search } = useLocation();
   if (!submission) {
-    return <Redirect to={`${url}${window.location.search}`} />;
+    return <Redirect to={`${url}${search}`} />;
   }
   return children(submission);
 };
