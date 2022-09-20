b6c93955bb38c5ae0424b0eb135fedd3c45210ba
Iselin Gulli <iselingulli@Iselins-MacBook-Pro.local>
2022-09-20 10:12:33 +0200

    added mod11 validation for account number

---
+++ packages/shared-components/src/customComponents/components/AccountNumber.jsx
@@ -0,0 +1,56 @@
+import { validatorUtils } from "@navikt/skjemadigitalisering-shared-domain";
+import baseEditForm from "formiojs/components/_classes/component/Component.form";
+import FormBuilderOptions from "../../Forms/form-builder-options";
+import { advancedDescription } from "./fields/advancedDescription.js";
+import Number from "./Number.js";
+
+export default class AccountNumber extends Number {
+  validateAccountNumber(accountNumber) {
+    if (accountNumber === "") {
+      return true;
+    }
+    return validatorUtils.isAccountNumber(accountNumber + "");
+  }
+
+  static get builderInfo() {
+    return FormBuilderOptions.builder.pengerOgKonto.components.bankAccount;
+  }
+
+  static editForm(...extend) {
+    return baseEditForm([
+      ...extend,
+      {
+        key: "display",
+        components: [
+          ...advancedDescription,
+          { key: "placeholder", ignore: true },
+          { key: "tabindex", ignore: true },
+          { key: "tooltip", ignore: true },
+          { key: "customClass", ignore: true },
+          { key: "hidden", ignore: true },
+          { key: "hideLabel", ignore: true },
+          { key: "autofocus", ignore: true },
+          { key: "disabled", ignore: true },
+          { key: "tableView", ignore: true },
+          { key: "modalEdit", ignore: true },
+        ],
+      },
+      {
+        key: "validation",
+        components: [
+          { key: "validateOn", ignore: true },
+          { key: "unique", ignore: true },
+          { key: "errorLabel", ignore: true },
+          { key: "validate.customMessage", ignore: true },
+          { key: "custom-validation-js", ignore: true },
+          { key: "json-validation-json", ignore: true },
+          { key: "errors", ignore: true },
+        ],
+      },
+      { key: "data", ignore: true },
+      { key: "logic", ignore: true },
+      { key: "layout", ignore: true },
+      { key: "addons", ignore: true },
+    ]);
+  }
+}
--- packages/shared-domain/src/utils/validatorUtils.ts
+++ packages/shared-domain/src/utils/validatorUtils.ts
@@ -2,7 +2,13 @@ const isOrganizationNumber = (organizationNumber: string) => {
   if (/^\d{9}$/.test(organizationNumber)) {
     return mod11(organizationNumber, [3, 2, 7, 6, 5, 4, 3, 2]);
   }
+  return false;
+};
 
+const isAccountNumber = (accountNumber: string) => {
+  if (/^\d{11}$/.test(accountNumber)) {
+    return mod11(accountNumber, [5, 4, 3, 2, 7, 6, 5, 4, 3, 2]);
+  }
   return false;
 };
 
@@ -11,7 +17,6 @@ const mod11 = (value: string, weights: Array<number>) => {
   weights.forEach((element, index) => {
     sum += parseInt(value.charAt(index), 10) * element;
   });
-
   const checkDigit = parseInt(value.slice(-1), 10);
   const remainder = sum % 11;
   return checkDigit === (remainder === 0 ? 0 : 11 - remainder);
@@ -19,6 +24,7 @@ const mod11 = (value: string, weights: Array<number>) => {
 
 const validatorUtils = {
   isOrganizationNumber,
+  isAccountNumber,
 };
 
 export default validatorUtils;
