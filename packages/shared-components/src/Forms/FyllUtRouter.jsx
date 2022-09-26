624ecac157f640cfc5983edd150e99c4fcb9bdb4
Lars Olav Torvik <lars.olav.torvik@nav.no>
2022-09-26 12:04:20 +0200

    Remove .js extension on unload import.

--- packages/shared-components/src/Forms/FyllUtRouter.jsx
+++ packages/shared-components/src/Forms/FyllUtRouter.jsx
@@ -5,7 +5,7 @@ import { Prompt, Redirect, Route, Switch, useRouteMatch } from "react-router-dom
 import { useAppConfig } from "../configContext";
 import { useAmplitude } from "../context/amplitude";
 import { LanguageSelector, LanguagesProvider } from "../context/languages";
-import { addBeforeUnload, removeBeforeUnload } from "../util/unload.js";
+import { addBeforeUnload, removeBeforeUnload } from "../util/unload";
 import { FillInFormPage } from "./FillInFormPage.jsx";
 import { bootstrapStyles } from "./fyllUtRouterBootstrapStyles";
 import { IntroPage } from "./IntroPage.tsx";
