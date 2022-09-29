d4316355d8eb710fd41f14593bac0fe132a641d2
Iselin Gulli <iselingulli@nrkmci70505.localdomain>
2022-09-29 11:17:24 +0200

    removed comment

--- packages/fyllut/src/components/FormPageWrapper.jsx
+++ packages/fyllut/src/components/FormPageWrapper.jsx
@@ -38,7 +38,6 @@ export const FormPageWrapper = () => {
     for (let i = 0; i < form?.components.length; i++) {
       if (form?.components[i]?.components?.find((j) => j.key === "beskrivelsetekst")) {
         const descriptionTxt = form?.components[i]?.components?.find((j) => j.key === "beskrivelsetekst")?.content;
-        //const descriptionTxt = descriptionComponent?.content?;
         setHeaderProp(metaNameDescr, descriptionTxt);
         setHeaderProp(metaNameOgDescr, descriptionTxt);
         break;
