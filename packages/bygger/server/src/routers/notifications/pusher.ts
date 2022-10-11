bc70694ca17cfd92a6f527a2a9c89332338ee738
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2022-10-11 10:00:24 +0200

    Helper method for flattening log meta object with given prefix

--- packages/bygger/server/src/routers/notifications/pusher.ts
+++ packages/bygger/server/src/routers/notifications/pusher.ts
@@ -2,16 +2,18 @@ import { NextFunction, Request, Response } from "express";
 import { logger } from "../../logging/logger";
 import { pusherService } from "../../services";
 import { PusherEvent } from "../../services/PusherService";
+import { toMeta } from "../../util/logUtils";
 import { ApiError } from "../api/helpers/errors";
 
 const pusher = {
   post: async (req: Request, res: Response, next: NextFunction) => {
     const pusherEvent: PusherEvent = req.body;
+    const logMeta = toMeta("pusherEvent", pusherEvent);
     try {
-      logger.info("Trigger pusher event", { pusherEvent });
+      logger.info("Trigger pusher event", logMeta);
       await pusherService.trigger(pusherEvent);
     } catch (error) {
-      logger.warn("Failed to trigger pusher event", { pusherEvent, error });
+      logger.warn("Failed to trigger pusher event", { ...logMeta, error });
       return next(new ApiError("Failed to trigger pusher event", true, error as Error));
     }
     return res.sendStatus(200);
