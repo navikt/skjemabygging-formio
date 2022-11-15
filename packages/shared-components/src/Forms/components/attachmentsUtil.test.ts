3828ae3dd6dabdc58996a16d3095f401c58013ee
Lars Olav Torvik <lars.olav.torvik@nav.no>
2022-11-15 17:39:25 +0100

    Add test for otherDocumentation

--- packages/shared-components/src/Forms/components/attachmentsUtil.test.ts
+++ packages/shared-components/src/Forms/components/attachmentsUtil.test.ts
@@ -1,4 +1,4 @@
-import { getRelevantAttachments } from "./attachmentsUtil";
+import { getRelevantAttachments, hasOtherDocumentation } from "./attachmentsUtil";
 import {
   borDuINorgeRadiopanel,
   panelForsteSide,
@@ -140,6 +140,35 @@ describe("attachmentUtil", () => {
         const attachments = getRelevantAttachments(vedleggConditional.form, submissionCopy);
         expect(attachments).toHaveLength(0);
       });
+    });
+  });
+
+  describe("Test if attachments have the property otherDocumentation set and take into account conditions", () => {
+    const form = {
+      components: [
+        {
+          ...panelVedleggsliste,
+          components: [
+            {
+              ...vedleggBekreftelseBostedsadresse,
+              otherDocumentation: true,
+              customConditional: `show = data.ukjentpanel.svar === "nei" || data.${borDuINorgeRadiopanel.key} === "nei"`,
+            },
+          ],
+        },
+      ],
+    };
+
+    it("does not return attachment which is not relevant", () => {
+      const submission = { data: { [borDuINorgeRadiopanel.key]: "ja" } };
+      const attachments = hasOtherDocumentation(form, submission);
+      expect(attachments).toBe(false);
+    });
+
+    it("does not return attachment which is not relevant", () => {
+      const submission = { data: { [borDuINorgeRadiopanel.key]: "nei" } };
+      const attachments = hasOtherDocumentation(form, submission);
+      expect(attachments).toBe(true);
     });
   });
 });
