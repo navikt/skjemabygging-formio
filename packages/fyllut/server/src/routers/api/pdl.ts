5192ad45d0b2272f27153f32f0efbe7ac3314073
Lars Olav Torvik <lars.olav.torvik@nav.no>
2022-11-16 16:09:24 +0100

    Pdl test

---
+++ packages/fyllut/server/src/routers/api/pdl.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express";
+
+const pdl = {
+  get: async (req: Request, res: Response, next: NextFunction) => {
+    fetch("https://pdl-api.dev.intern.nav.no/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: `
+          query($ident: ID!) {
+            hentPerson(ident: $ident) {
+              navn(historikk: false) {
+                fornavn
+                mellomnavn
+                etternavn
+              }
+            }
+          }
+      `,
+        variables: {
+          ident: "08842748500",
+        },
+      }),
+    })
+      .then((response) => {
+        console.log(response.json());
+        res.send(response.json());
+      })
+      .then((result) => console.log(result));
+  },
+};
+
+export default pdl;
