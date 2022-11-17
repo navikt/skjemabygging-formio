7ec1035e6ac99888205c10d791428931fcdf5cca
Lars Olav Torvik <lars.olav.torvik@nav.no>
2022-11-17 09:57:59 +0100

    Improve pdl fetch

--- packages/fyllut/server/src/routers/api/pdl.ts
+++ packages/fyllut/server/src/routers/api/pdl.ts
@@ -1,16 +1,18 @@
 import { NextFunction, Request, Response } from "express";
+import fetch from "node-fetch";
+import { logger } from "../../logger";
 
 const pdl = {
   get: async (req: Request, res: Response, next: NextFunction) => {
-    console.log("Fetch pdl");
-    console.log("Fetch pdl test");
-    fetch("https://pdl-api.dev.intern.nav.no/graphql", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query: `
+    logger.debug("Fetch PDL");
+    try {
+      const response = await fetch("https://pdl-api.dev.intern.nav.no/graphql", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          query: `
           query($ident: ID!) {
             hentPerson(ident: $ident) {
               navn(historikk: false) {
@@ -21,16 +23,23 @@ const pdl = {
             }
           }
       `,
-        variables: {
-          ident: "08842748500",
-        },
-      }),
-    })
-      .then((response) => {
-        console.log(response.json());
-        res.send(response.json());
-      })
-      .then((result) => console.log(result));
+          variables: {
+            ident: "08842748500",
+          },
+        }),
+      });
+
+      const body = await response.json();
+      logger.debug(body);
+      if (body.errors && body.errors.length > 0) {
+        throw new Error(body.errors[0].message);
+      }
+
+      res.send(body.data);
+    } catch (e) {
+      logger.error(e);
+      next(e);
+    }
   },
 };
 
