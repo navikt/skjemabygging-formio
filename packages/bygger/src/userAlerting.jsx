199ffee1d9f708a86a04741e9920808405b0e979
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2022-11-22 11:19:42 +0100

    Remove unused pusher channels

--- packages/bygger/src/userAlerting.jsx
+++ packages/bygger/src/userAlerting.jsx
@@ -39,35 +39,6 @@ const WarningAlert = ({ message, onClose }) => (
   </AlertStripeAdvarsel>
 );
 
-const SkjemautfyllingDeployedAlert = ({ message, onClose }) => {
-  return (
-    <AlertStripeSuksess>
-      <AlertContent>
-        <div>
-          <h3>Manuell deploy av skjemautfylling</h3>
-          <div>
-            <p>Versjonen ble endret av "{message.skjemautfyllerCommit.author.name}"</p>
-            <p>Endringsmeldingen var "{message.skjemautfyllerCommit.message}"</p>
-          </div>
-        </div>
-        <Xknapp type="flat" onClick={onClose} />
-      </AlertContent>
-    </AlertStripeSuksess>
-  );
-};
-
-const PublishSuccessAlert = ({ message, onClose }) => {
-  return (
-    <AlertStripeSuksess>
-      <AlertContent>
-        <h3>Publisering fullført</h3>
-        <div>{message.skjemapublisering.skjematittel || message.skjemapublisering.commitUrl} er nå publisert</div>
-        <Xknapp type="flat" onClick={onClose} />
-      </AlertContent>
-    </AlertStripeSuksess>
-  );
-};
-
 const FyllutDeploymentSuccessAlert = ({ data, onClose }) => {
   return (
     <AlertStripeSuksess>
@@ -82,18 +53,6 @@ const FyllutDeploymentSuccessAlert = ({ data, onClose }) => {
   );
 };
 
-const PublishAbortedAlert = ({ message, onClose }) => {
-  return (
-    <AlertStripeFeil>
-      <AlertContent>
-        <h3>Publisering feilet</h3>
-        <div>{message.skjemapublisering.skjematittel || message.skjemapublisering.commitUrl} ble ikke publisert</div>
-        <Xknapp type="flat" onClick={onClose} />
-      </AlertContent>
-    </AlertStripeFeil>
-  );
-};
-
 const FyllutDeploymentFailureAlert = ({ data, onClose }) => {
   return (
     <AlertStripeFeil>
@@ -101,59 +60,6 @@ const FyllutDeploymentFailureAlert = ({ data, onClose }) => {
         <div>
           <h3>{data.title}</h3>
           <div>{data.message}</div>
-        </div>
-        <Xknapp type="flat" onClick={onClose} />
-      </AlertContent>
-    </AlertStripeFeil>
-  );
-};
-
-export const BulkPublishSuccessAlert = ({ message, onClose }) => {
-  return (
-    <AlertStripeSuksess>
-      <AlertContent>
-        <h3>Bulk-publisering fullført</h3>
-        <div>
-          {message.skjemapublisering.antall || "Ukjent antall"} skjemaer ble bulk-publisert. Se&nbsp;
-          <a
-            target="_blank"
-            rel="noreferrer noopener"
-            href="https://github.com/navikt/skjemautfylling-formio/commits/master"
-          >
-            GitHub
-          </a>
-          &nbsp; for mer informasjon
-        </div>
-        <Xknapp type="flat" onClick={onClose} />
-      </AlertContent>
-    </AlertStripeSuksess>
-  );
-};
-
-export const BulkPublishAbortedAlert = ({ message, onClose }) => {
-  return (
-    <AlertStripeFeil>
-      <AlertContent>
-        <h3>Bulk-publisering feilet</h3>
-        <div>
-          Publisering av {message.skjemapublisering.antall || "ukjent antall"} skjemaer ble avbrutt på grunn av en feil
-        </div>
-        <Xknapp type="flat" onClick={onClose} />
-      </AlertContent>
-    </AlertStripeFeil>
-  );
-};
-
-const BuildAbortedAlert = ({ message, onClose }) => {
-  return (
-    <AlertStripeFeil>
-      <AlertContent>
-        <div>
-          <h3>Byggefeil</h3>
-          <p>
-            <a href={message.skjemautfyllerCommit.url}>git url</a>
-          </p>
-          <p>Commit melding: {message.skjemautfyllerCommit.message}</p>
         </div>
         <Xknapp type="flat" onClick={onClose} />
       </AlertContent>
@@ -232,32 +138,6 @@ export function useUserAlerting(pusher) {
     return () => window.removeEventListener("unhandledrejection", callback);
   }, [userAlerter]);
   useEffect(() => {
-    const deploymentChannel = pusher.subscribe("skjemautfyller-deployed");
-    deploymentChannel.bind("publication", (data) => {
-      let key;
-      key = userAlerter.addAlertComponent(() => (
-        <PublishSuccessAlert message={data} onClose={() => userAlerter.removeAlertComponent(key)} />
-      ));
-    });
-    deploymentChannel.bind("bulk-publication", (data) => {
-      let key;
-      key = userAlerter.addAlertComponent(() => (
-        <BulkPublishSuccessAlert message={data} onClose={() => userAlerter.removeAlertComponent(key)} />
-      ));
-    });
-    deploymentChannel.bind("other", (data) => {
-      let key;
-      key = userAlerter.addAlertComponent(() => (
-        <SkjemautfyllingDeployedAlert message={data} onClose={() => userAlerter.removeAlertComponent(key)} />
-      ));
-    });
-    return () => {
-      deploymentChannel.unbind("other");
-      deploymentChannel.unbind("bulk-publication");
-      deploymentChannel.unbind("publication");
-    };
-  }, [pusher, userAlerter]);
-  useEffect(() => {
     const fyllutDeploymentChannel = pusher.subscribe("fyllut-deployment");
     fyllutDeploymentChannel.bind("success", (data) => {
       let key;
@@ -275,54 +155,6 @@ export function useUserAlerting(pusher) {
       fyllutDeploymentChannel.unbind("success");
       fyllutDeploymentChannel.unbind("failure");
     };
-  }, [pusher, userAlerter]);
-  useEffect(() => {
-    const buildAbortedChannel = pusher.subscribe("build-aborted");
-    buildAbortedChannel.bind("publication", (data) => {
-      let key;
-      key = userAlerter.addAlertComponent(() => (
-        <PublishAbortedAlert message={data} onClose={() => userAlerter.removeAlertComponent(key)} />
-      ));
-    });
-    buildAbortedChannel.bind("bulk-publication", (data) => {
-      let key;
-      key = userAlerter.addAlertComponent(() => (
-        <BulkPublishAbortedAlert message={data} onClose={() => userAlerter.removeAlertComponent(key)} />
-      ));
-    });
-    buildAbortedChannel.bind("other", (data) => {
-      let key;
-      key = userAlerter.addAlertComponent(() => (
-        <BuildAbortedAlert message={data} onClose={() => userAlerter.removeAlertComponent(key)} />
-      ));
-    });
-    return () => {
-      buildAbortedChannel.unbind("publication");
-      buildAbortedChannel.unbind("bulk-publication");
-      buildAbortedChannel.unbind("other");
-    };
-  }, [pusher, userAlerter]);
-  useEffect(() => {
-    const buildAbortedChannel = pusher.subscribe("publish-aborted");
-    buildAbortedChannel.bind("failure", (data) => {
-      let key;
-      key = userAlerter.addAlertComponent(() => (
-        <PublishAbortedAlert message={data} onClose={() => userAlerter.removeAlertComponent(key)} />
-      ));
-    });
-    return () => {
-      buildAbortedChannel.unbind("failure");
-    };
-  }, [pusher, userAlerter]);
-  useEffect(() => {
-    const buildAbortedChannel = pusher.subscribe("publish-resource-aborted");
-    buildAbortedChannel.bind("failure", (data) => {
-      let key;
-      key = userAlerter.addAlertComponent(() => (
-        <PublishAbortedAlert message={data} onClose={() => userAlerter.removeAlertComponent(key)} />
-      ));
-    });
-    return () => buildAbortedChannel.unbind("failure");
   }, [pusher, userAlerter]);
   return userAlerter;
 }
