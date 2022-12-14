90d4f0d8589128eca2e2db25ff904fbeedff298a
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2022-12-14 14:22:46 +0100

    Cypress: Digital submission test coverage

---
+++ packages/fyllut/cypress/integration/submission-method.spec.ts
@@ -0,0 +1,109 @@
+describe("Submission method", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "/fyllut/api/forms/bug101010", { fixture: "submission-method.json" }).as("getForm");
+    cy.intercept("GET", "/fyllut/translations/bug101010", { fixture: "submission-method-translations.json" }).as(
+      "getFormTranslations"
+    );
+    cy.intercept("GET", "/fyllut/global-translations/en", { fixture: "global-translation.json" }).as(
+      "getGlobalTranslations"
+    );
+  });
+
+  describe("Subscription method 'digital'", () => {
+    beforeEach(() => {
+      cy.visit("/fyllut/bug101010/veiledning?sub=digital");
+      cy.wait("@getForm");
+      cy.wait("@getFormTranslations");
+      cy.wait("@getGlobalTranslations");
+    });
+
+    it("Renders stepper without 'Vedlegg'", () => {
+      cy.findByRole("navigation", { name: "Søknadssteg" })
+        .should("exist")
+        .within(() => {
+          cy.findAllByRole("link").should("have.length", 3);
+          cy.findByRole("link", { name: "Veiledning" }).should("exist");
+          cy.findByRole("link", { name: "Dine opplysninger" }).should("exist");
+          cy.findByRole("link", { name: "Vedlegg" }).should("not.exist");
+          cy.findByRole("link", { name: "Oppsummering" }).should("exist");
+        });
+    });
+
+    describe("Summary page", () => {
+      beforeEach(() => {
+        // fills out form and navigates to summary page
+        cy.clickNextStep();
+        cy.findByRole("textbox", { name: "Fornavn" }).should("exist").type("Test");
+        cy.findByRole("textbox", { name: "Etternavn" }).should("exist").type("Testesen");
+        cy.findByText("Bor du i Norge?")
+          .should("exist")
+          .parent()
+          .within(() => cy.findByLabelText("Ja").should("exist").check({ force: true }));
+        cy.findByRole("combobox", { name: "Hva søker du støtte til?" }).should("exist").type("Sykk{downArrow}{enter}");
+        cy.clickNextStep();
+        cy.findByRole("heading", { name: "Oppsummering" }).should("exist");
+      });
+
+      // Denne feiler pga. bug i react stepper hvor link til vedleggspanelet vises selv om søknaden skal sendes digitalt
+      it.skip("renders stepper without 'Vedlegg' on summary page", () => {
+        cy.findByRole("navigation", { name: "Søknadssteg" })
+          .should("exist")
+          .within(() => {
+            cy.findAllByRole("link").should("have.length", 3);
+            cy.findByRole("link", { name: "Veiledning" }).should("exist");
+            cy.findByRole("link", { name: "Dine opplysninger" }).should("exist");
+            cy.findByRole("link", { name: "Vedlegg" }).should("not.exist");
+            cy.findByRole("link", { name: "Oppsummering" }).should("exist");
+          });
+      });
+
+      it("includes one attachment", () => {
+        cy.intercept("POST", "/fyllut/api/send-inn", (req) => {
+          expect(req.body.attachments).to.have.length(1);
+          req.reply(200);
+        }).as("sendInn");
+
+        // submit application
+        cy.findByRole("button", { name: "Gå videre" }).click();
+        cy.wait("@sendInn");
+      });
+
+      it("includes two attachments", () => {
+        cy.intercept("POST", "/fyllut/api/send-inn", (req) => {
+          expect(req.body.attachments).to.have.length(2);
+          req.reply(200);
+        }).as("sendInn");
+
+        // edit data so that conditional attachment is triggered
+        cy.findByRole("link", { name: "Rediger dine opplysninger" }).should("exist").click();
+        cy.findByRole("combobox", { name: "Hva søker du støtte til?" }).should("exist").type("Brill{downArrow}{enter}");
+        cy.findByRole("link", { name: "Oppsummering" }).click();
+
+        // submit application
+        cy.findByRole("button", { name: "Gå videre" }).click();
+        cy.wait("@sendInn");
+      });
+    });
+  });
+
+  describe("Subscription method 'paper'", () => {
+    beforeEach(() => {
+      cy.visit("/fyllut/bug101010/veiledning?sub=paper");
+      cy.wait("@getForm");
+      cy.wait("@getFormTranslations");
+      cy.wait("@getGlobalTranslations");
+    });
+
+    it("Renders stepper with 'Vedlegg'", () => {
+      cy.findByRole("navigation", { name: "Søknadssteg" })
+        .should("exist")
+        .within(() => {
+          cy.findAllByRole("link").should("have.length", 4);
+          cy.findByRole("link", { name: "Veiledning" }).should("exist");
+          cy.findByRole("link", { name: "Dine opplysninger" }).should("exist");
+          cy.findByRole("link", { name: "Vedlegg" }).should("exist");
+          cy.findByRole("link", { name: "Oppsummering" }).should("exist");
+        });
+    });
+  });
+});
