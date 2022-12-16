397ce98f936175bf6927283a2da2af17a1fb32f1
Magnus Røed Hestvik <mrh@computas.com>
2022-12-16 14:53:42 +0100

    Update test with number of translation inputs

--- packages/bygger/src/translations/global/ApplicationTextTranslationEditPanel.test.jsx
+++ packages/bygger/src/translations/global/ApplicationTextTranslationEditPanel.test.jsx
@@ -43,7 +43,7 @@ describe("ApplicationTextTranslationEditPanel", () => {
     });
 
     it("renders all grensesnitt inputs", () => {
-      expect(screen.getAllByRole("textbox")).toHaveLength(35);
+      expect(screen.getAllByRole("textbox")).toHaveLength(38);
     });
 
     it("renders originalText with translatedText as value", () => {
