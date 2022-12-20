fa8344940565b4225b2018d58a0731cecadda64c
Magnus Røed Hestvik <mrh@computas.com>
2022-12-20 12:36:37 +0100

    Fix test

--- packages/bygger/src/translations/global/ApplicationTextTranslationEditPanel.test.jsx
+++ packages/bygger/src/translations/global/ApplicationTextTranslationEditPanel.test.jsx
@@ -43,7 +43,7 @@ describe("ApplicationTextTranslationEditPanel", () => {
     });
 
     it("renders all grensesnitt inputs", () => {
-      expect(screen.getAllByRole("textbox")).toHaveLength(52);
+      expect(screen.getAllByRole("textbox")).toHaveLength(48);
     });
 
     it("renders originalText with translatedText as value", () => {
