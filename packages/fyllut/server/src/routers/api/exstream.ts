599cf77ebdfca6357fe8849040d8227d661f7014
Rune Rosseland <rune.rosseland@nav.no>
2023-01-03 12:55:57 +0100

    Update html and json for generating pdf

--- packages/fyllut/server/src/routers/api/exstream.ts
+++ packages/fyllut/server/src/routers/api/exstream.ts
@@ -7,29 +7,170 @@ import { responseToError } from "../../utils/errorHandling.js";
 
 const { skjemabyggingProxyUrl } = config;
 
-const testHtml =
-  "<html lang='no'>" +
-  "<head>" +
-  "<title>TEST PDF GENERATION</title>" +
-  "<meta charset='UTF-8'>" +
-  "</head>" +
-  "<body style='background-color: red;'>" +
-  "<h1>This is a test</h1>" +
-  "<p>How is this converted?</p>" +
-  "<ul>" +
-  "<li>This</li>" +
-  "<li>is</li>" +
-  "<li>a</li>" +
-  "<li>list!</li>" +
-  "</ul>" +
-  "</body>" +
-  "</html>";
+const testHtml = `
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Søknad om stønad til anskaffelse av motorkjøretøy og / eller spesialutstyr og tilpassing til bil</title>
+    <style>
+    body {}
+    h1, h2, h3, h4, .spm {font-family: Arial;}
+    .svar {margin-bottom: 5px; font-family: "Courier New", sans-serif;}
+    .innrykk {margin: 0px 0px 10px 20px;}
+    .underskrift {margin-bottom: 30px;}
+</style>
+</head>
+<body>
+
+<h2>Dine opplysninger</h2>
+<div  class="spm">Fornavn</div>
+<div  class="svar">- Per</div>
+<div  class="spm">Etternavn</div>
+<div  class="svar">- Andersen</div>
+<div class="spm">Har du norsk fødselsnummer eller D-nummer?</div>
+<div class="svar">- Ja</div>
+<div class="spm">Fødselsnummer / D-nummer</div>
+<div class="svar">- 20048119949</div>
+<div class="spm">Er du under 18 år?</div>
+<div class="svar">- Nei</div>
+<div class="spm">Har du verge?</div>
+<div class="svar">- Nei</div>
+
+<h2>Veiledning</h2>
+<div class="spm">Hva skal du søke om?</div>
+<div class="svar">-  Stønad til spesialtilpasset kassebil</div>
+<div class="spm">Hva skal du bruke bilen til?</div>
+<div class="svar">-  Kjøring til og fra arbeid eller utdanningssted</div>
+<div class="spm">Jeg forstår at jeg må sende legeerklæring og dokumentajon fra ergo- eller fysioterapeut for at NAV skal kunne behandle søknaden.</div>
+<div class="svar">-  Ja</div>
+<div class="spm">Jeg forstår at NAV bruker personopplysningene ovenfor for å behandle søknaden.</div>
+<div class="svar">-  Ja</div>
+
+<h2>Inntekt</h2>
+<div class="spm">Jeg forstår at jeg må betale en egenandel på inntil 150 000 kroner dersom jeg og ektefellen, samboeren eller partneren min til sammen har en inntekt over 6 G.</div>
+<div class="svar">- Ja</div>
+
+<h2>Behov for bil</h2>
+<div class="spm">Kan du reise kollektivt alene?</div>
+<div class="svar">- Nei</div>
+<div class="spm">Kan du reise kollektivt hvis du har med deg følge?</div>
+<div class="svar">- Ja</div>
+<div class="spm">Har du så stort transportbehov til og fra arbeid eller utdanningssted at du trenger egen bil?</div>
+<div class="svar">- Ja</div>
+<div class="spm">Mottar du noen av de følgende støtteordningene?</div>
+<div class="svar">-  TT-kort eller andre tilsvarende kommunale transportordninger</div>
+<div class="svar">-  Grunnstønad til transport, arbeids- og utdanningsreiser eller lignende</div>
+<div class="spm">Hvor mange ganger i løpet av en måned reiser du til og fra arbeid eller utdanningsstedet?</div>
+<div class="svar">- 88</div>
+<div class="spm">Hvor mange kilometer er det mellom der du bor og arbeidsplassen din eller utdanningsstedet ditt?</div>
+<div class="svar">- 56</div>
+<div class="spm">Hvordan dekker du transportbehovet ditt til og fra arbeid eller utdanningssted i dag?</div>
+<div class="svar">- Saken blir nå henlagt for intet straffbart forhold. – Det er ikke grunnlag for å hevde at noen bevisst har holdt disse opplysningene tilbake eller bevisst bidro til feilopplysning til statsadvokaten, og da senere til gjenopptakelseskommisjonen. Så dette er altså henlagt som intet straffbart forhold, sier Terje Nybøe, sjef for Spesialenheten til NRK.</div>
+<div class="spm">Er du avhengig av hjelpemidler for å forflytte deg?</div>
+<div class="svar">- Ja</div>
+<div class="spm">Hvilke hjelpemidler er du avhengig av for å forflytte deg?</div>
+<div class="svar">- Verken tidligere etterforskningsleder Arne Pedersen eller Agder politidistrikt har gjort tjenestefeil i Baneheia-saken. Det er Spesialenhetens konklusjon etter flere måneder med etterforskning.</div>
+
+<h2>Førerkort</h2>
+<div class="spm">Har du førerkort som er gyldig i Norge?</div>
+<div class="svar">- Ja</div>
+
+<h2>Eier du bil?</h2>
+<div class="spm">Eier du bil?</div>
+<div class="svar">- Nei</div>
+<div class="spm">Har du eid en bil i løpet av de siste 6 månedene?</div>
+<div class="svar"Nei></div>
+<div class="spm">Er det andre i husstanden din som eier bil i dag?</div>
+<div class="svar">- Ja</div>
+<div class="spm">Hvem i husstanden eier bil i dag?</div>
+<div class="svar">- Samboer</div>
+
+<div class="innrykk">
+<h3>Bilen(e)s registreringsnummer</h3>
+<div class="spm">Registreringsnummer</div>
+<div class="svar">- DJ56378</div>
+<div class="spm">Registreringsnummer</div>
+<div class="svar">- KU48729</div>
+</div>
+
+<div class="spm">Kan du bruke noen av bilen(e)?</div>
+<div class="svar">- Nei</div>
+<div class="spm">Oppgi hvorfor du ikke kan bruke bilen(e)</div>
+<div class="svar">- Spesialenhetens etterforskning har vært rettet mot tidligere krimsjef Arne Pedersen og Agder Politidistrikt.</div>
+
+<h2>Fastlege</h2>
+<div class="spm">Har du fastlege?</div>
+<div class="svar">- Ja</div>
+<div class="spm">Fornavn</div>
+<div class="svar">- Dr.</div>
+<div class="spm">Etternavn</div>
+<div class="svar">- Dyrego</div>
+<div class="spm">Navn på fastlegekontor</div>
+<div class="svar">- DD & Dyrene</div>
+<div class="spm">Postadresse</div>
+<div class="svar">- Navergata 123</div>
+<div class="spm">Postnummer</div>
+<div class="svar">- 0235</div>
+<div class="spm">Poststed</div>
+<div class="svar">- Oslo</div>
+<div class="spm">Telefonnummer</div>
+<div class="svar">- 86759323</div>
+
+<h2>Tilleggsopplysninger</h2>
+<div class="spm">Tilleggsopplysninger</div>
+<div class="svar">- Spesialenhetens etterforskning har vært rettet mot tidligere krimsjef Arne Pedersen og Agder Politidistrikt.</div>
+
+<h2>Vedlegg</h2>
+<div class="spm">Legeerklæring for motorkjøretøy, NAV 10-07.42</div>
+<div class="svar">- Jeg legger det ved denne søknaden (anbefalt)</div>
+<div class="spm">Erklæring fra ergo- eller fysioterapeut i forbindelse med søknad om motorkjøretøy og spesialutstyr / tilpasning, NAV 10-07.43</div>
+<div class="svar">- Jeg legger det ved denne søknaden (anbefalt)</div>
+<div class="spm">Dokumentasjon av veiforhold</div>
+<div class="svar">- Jeg legger det ved denne søknaden (anbefalt)</div>
+<div class="spm">Kopi av førerkortet ditt</div>
+<div class="svar">- Jeg ettersender dokumentasjonen senere (jeg er klar over at NAV ikke kan behandle søknaden før jeg har levert dokumentasjonen)</div>
+
+<h2>Bekreftelse av opplysninger</h2>
+<div class="spm">Jeg bekrefter at opplysningene er riktige, og at feil opplysninger kan få konsekvenser for stønaden.</div>
+<div class="svar">- Ja</div>
+
+<h2>Underskrift</h2>
+<div>Signér på de stedene som er aktuelle for din stønad.</div>
+<div class="underskrift">NB! Har du to foreldre må begge signere søknaden.</div>
+
+<h3>Søker / verge</h3>
+<div>_____________________________________________________________</div>
+<div class="underskrift">Sted og dato</div>
+<div>_____________________________________________________________</div>
+<div class="underskrift">Underskrift</div>
+
+<h3>Forelder 1</h3>
+<div>_____________________________________________________________</div>
+<div class="underskrift">Sted og dato</div>
+<div>_____________________________________________________________</div>
+<div class="underskrift">Underskrift</div>
+
+<h3>Forelder 2</h3>
+<div>_____________________________________________________________</div>
+<div class="underskrift">Sted og dato</div>
+<div>_____________________________________________________________</div>
+<div class="underskrift">Underskrift</div>
+
+<div class="spm"></div>
+<div class="svar">- </div>
+</body>
+</html>`;
 
 const exstream = {
   // TODO: Change this to POST after testing done
   get: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const pdf = await createPdf(req.headers.AzureAccessToken as string, "Test", testHtml);
+      const pdf = await createPdf(
+        req.headers.AzureAccessToken as string,
+        "Søknad om stønad til anskaffelse av motorkjøretøy og / eller spesialutstyr og tilpassing til bil",
+        testHtml
+      );
       res.contentType(pdf.contentType);
       res.send(base64Decode(pdf.data));
     } catch (e) {
@@ -58,6 +199,10 @@ const createPdf = async (azureAccessToken: string, title: string, html: string) 
             dokumenttypeId: "fs_001",
             dokumentTittel: title,
             arkivSystem: "INGEN",
+            spraakkode: "NB",
+            blankettnr: "NAV 10-07.40",
+            brukersFnr: "20048119949",
+            skjemaversjon: "8b4d3fbc9b5be00d1db0285ec013f9dbc6750631",
             html: base64Encode(html),
           })
         ),
