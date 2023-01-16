c952a87ac8fdddd735f2df51a2243d903bc83cf8
Lars Olav Torvik <lars.olav.torvik@nav.no>
2023-01-16 09:46:17 +0100

    More tests on OBO

--- packages/fyllut/server/src/routers/api/pdl.ts
+++ packages/fyllut/server/src/routers/api/pdl.ts
@@ -1,18 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import fetch from "node-fetch";
-import qs from "qs";
-import { config } from "../../config/config";
 import { logger } from "../../logger";
 import { getTokenxAccessToken } from "../../security/tokenxHelper";
 import { Person } from "../../types/person";
 
-const { clientId, clientSecret, azureOpenidTokenEndpoint } = config;
-
 const pdl = {
   person: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      logger.debug(req.headers.AzureAccessToken);
-      const data = await getPerson(req.getIdportenJwt(), "AAP", req.params.id);
+      const data = await getPerson(req.headers.AzureAccessToken as string, "AAP", req.params.id);
       res.send(data);
     } catch (e) {
       next(e);
@@ -109,50 +104,15 @@ const getChildren = async (accessToken: string, theme: string, personId: string)
   return children;
 };
 
-const getPdlAccessToken = async (token: string) => {
-  return fetch(azureOpenidTokenEndpoint!, {
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    method: "POST",
-    body: qs.stringify({
-      grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
-      client_id: clientId,
-      client_secret: clientSecret,
-      assertion: token,
-      scope: "api:/dev-fss.pdl.pdl-api/.default",
-      requested_token_use: "on_behalf_of",
-    }),
-  })
-    .then((response) => {
-      logger.debug(`Client id: ${clientId}`);
-      logger.debug(`Token: ${token}`);
-      logger.debug(`Response: ${qs.stringify(response)}`);
-      if (response.ok) {
-        const data = response.json();
-        logger.debug("Ok");
-        logger.debug(qs.stringify(data));
-        // @ts-ignore
-        return data.access_token;
-      } else {
-        logger.debug("Error");
-      }
-    })
-    .catch((error) => {
-      logger.debug("PDL access token failed");
-      logger.debug(qs.stringify(error));
-    });
-};
-
 const pdlRequest = async (accessToken: string, theme: string, query: string) => {
   //const url = "https://pdl-api.prod-fss-pub.nais.io/graphql";
   const url = "https://pdl-api.dev-fss-pub.nais.io/graphql";
-  const pdlAccessToken = await getPdlAccessToken(accessToken);
-  logger.debug("pdlAccessToken");
-  logger.debug(pdlAccessToken);
+
   const response = await fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${pdlAccessToken}`,
+      Authorization: `Bearer ${accessToken}`,
       tema: theme,
     },
     body: query,
---
+++ packages/fyllut/server/src/security/azureOBOAccessTokenHandler.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from "express";
+import fetch from "node-fetch";
+import qs from "qs";
+import { config } from "../config/config";
+import { logger } from "../logger";
+import { toJsonOrThrowError } from "../utils/errorHandling.js";
+
+const { clientId, clientSecret, azureOpenidTokenEndpoint } = config;
+
+const azureOBOAccessTokenHandler = (req: Request, res: Response, next: NextFunction) => {
+  return fetch(azureOpenidTokenEndpoint!, {
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    method: "POST",
+    body: qs.stringify({
+      grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
+      client_id: clientId,
+      client_secret: clientSecret,
+      assertion: req.getIdportenJwt(),
+      scope: "api:/dev-fss.pdl.pdl-api/.default",
+      requested_token_use: "on_behalf_of",
+    }),
+  })
+    .then(toJsonOrThrowError("Feil ved OBO autentisering"))
+    .then((response) => {
+      logger.debug(`Client id: ${clientId}`);
+      logger.debug(`Response: ${qs.stringify(response)}`);
+
+      // @ts-ignore
+      req.headers.AzureAccessToken = response.access_token;
+
+      logger.debug(`OBO: ${req.headers.AzureAccessToken}`);
+      next();
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
+
+export default azureOBOAccessTokenHandler;
