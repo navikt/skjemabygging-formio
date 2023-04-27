d4684e6b1fe2a3388fb8ff433f235aa6021f9fd3
Lars Olav Torvik <lars.olav.torvik@nav.no>
2023-04-27 12:04:54 +0200

    Change type on Select

--- packages/bygger/src/components/FormMetaDataEditor/SubmissionTypeSelect.tsx
+++ packages/bygger/src/components/FormMetaDataEditor/SubmissionTypeSelect.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 interface Props {
   name: string;
-  label: React.ReactElement;
+  label: React.ReactNode;
   value: string | undefined;
   allowEmpty?: boolean;
   error?: React.ReactNode;
