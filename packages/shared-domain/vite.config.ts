884a667d9472b8b8e9ec7843bd3e3df4d03b6a31
Lars Olav Torvik <lars.olav.torvik@nav.no>
2023-08-28 11:11:34 +0200

    Temp hack for random uuid, will be solved with node 20.

--- packages/shared-domain/vite.config.ts
+++ packages/shared-domain/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    minify: true,
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "shared-domain",
