2dbc3da0846211ece2a57b5ac9df8762e84922db
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2023-08-28 10:35:41 +0200

    Revert "Workaround: getRandomValues not supported"
    This reverts commit 25f8c98768c22be1559e2f02487fce0f14856fac.

--- packages/bygger/server/src/server.js
+++ packages/bygger/server/src/server.js
@@ -1,3 +1,4 @@
+import * as crypto from "crypto";
 import express from "express";
 import correlator from "express-correlation-id";
 import config from "./config";
@@ -8,6 +9,8 @@ import apiRouter from "./routers/api";
 import internalRouter from "./routers/internal";
 import notificationsRouter from "./routers/notifications";
 import "./util/errorToJson";
+
+global.crypto = crypto;
 
 const app = express();
 
