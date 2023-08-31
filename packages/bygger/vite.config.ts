39e5b45738026c49f746fcfec8261010b5df0559
Lars Olav Torvik <lars.olav.torvik@nav.no>
2023-08-31 15:41:17 +0200

    Revert "Fix eslint errors in config"
    This reverts commit ab7ea864dec9e0f75108c69d067c0e666d2f3c23.

---
+++ packages/bygger/vite.config.ts
@@ -0,0 +1,76 @@
+/// <reference types="vitest" />
+import react from "@vitejs/plugin-react";
+import { readFileSync } from "fs";
+import lodashTemplate from "lodash/template";
+import { defineConfig, loadEnv } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, "env");
+  const plugins = [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: { ...env },
+      },
+    }),
+    {
+      name: "formio-template-handler",
+      enforce: "pre",
+      config() {},
+      load(id: string) {
+        if (!id.endsWith(".ejs")) {
+          return null;
+        }
+
+        const code = readFileSync(id).toString("utf-8");
+        // Use same values in Formio.Utils.Evaluator.templateSettings
+        const template = lodashTemplate(code, {
+          variable: "ctx",
+          evaluate: /\{%([\s\S]+?)%}/g,
+          interpolate: /\{\{([\s\S]+?)}}/g,
+          escape: /\{\{\{([\s\S]+?)}}}/g,
+        });
+
+        return `export default ${template};`;
+      },
+    },
+  ];
+
+  if (mode !== "production") {
+    plugins.push(tsconfigPaths());
+  }
+
+  return {
+    base: "/",
+    server: {
+      open: false,
+      host: "127.0.0.1",
+      port: 3000,
+      strictPort: true,
+      proxy: {
+        "/api": {
+          target: "http://127.0.0.1:8080",
+          changeOrigin: true,
+        },
+      },
+    },
+    preview: {
+      host: "127.0.0.1",
+      port: 3000,
+      strictPort: true,
+    },
+    resolve: {
+      dedupe: ["react-router-dom", "@navikt/ds-react", "@navikt/ds-icons"],
+    },
+    plugins,
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
+      include: ["src/(**/)?*.test.[jt]s(x)?"],
+    },
+  };
+});
