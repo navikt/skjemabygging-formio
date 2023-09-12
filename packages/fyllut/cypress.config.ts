cef3543a74b5f0bd305cf5f12a80826d20838018
Thomas Tykesson <thomas@tykesson.com>
2023-09-12 09:16:43 +0200

    Endrer tilbake til localhost, fra 127.0.0.1

--- packages/fyllut/cypress.config.ts
+++ packages/fyllut/cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   video: false,
   e2e: {
-    baseUrl: "http://127.0.0.1:3001",
+    baseUrl: "http://localhost:3001",
     viewportWidth: 1280,
     viewportHeight: 1000,
     testIsolation: false,
