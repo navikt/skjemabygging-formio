cf7ea1c51c8c8403574b7558e4367b0641bb8f5c
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2023-09-26 12:03:02 +0200

    nock cleanup

--- packages/fyllut-backend/src/utils/fetchWithRetry.test.ts
+++ packages/fyllut-backend/src/utils/fetchWithRetry.test.ts
@@ -5,12 +5,16 @@ import { FetchError } from "node-fetch";
 const TEST_HOST = "https://www.test.no";
 const TEST_PATH = "/projects/123";
 const TEST_URL = `${TEST_HOST}${TEST_PATH}`;
+const FETCH_ERROR = new FetchError(
+  "Client network socket disconnected before secure TLS connection was established",
+  "system",
+);
 
 describe("fetchWithRetry", () => {
-  const FETCH_ERROR = new FetchError(
-    "Client network socket disconnected before secure TLS connection was established",
-    "system",
-  );
+  afterEach(() => {
+    nock.abortPendingRequests();
+    nock.cleanAll();
+  });
 
   it("Retries fetch up to three times and succeeds", async () => {
     nock(TEST_HOST).get(TEST_PATH).twice().replyWithError(FETCH_ERROR).get(TEST_PATH).once().reply(200);
@@ -28,6 +32,7 @@ describe("fetchWithRetry", () => {
     expect(errorCaught).toBeUndefined();
     expect(response).toBeDefined();
     expect(response.ok).toBe(true);
+    expect(nock.isDone()).toBe(true);
   });
 
   it("Retries fetch up to three times and fails", async () => {
@@ -46,10 +51,11 @@ describe("fetchWithRetry", () => {
     expect(response).toBeUndefined();
     expect(errorCaught).toBeDefined();
     expect(errorCaught?.message).toContain(FETCH_ERROR.message);
+    expect(nock.isDone()).toBe(true);
   });
 
   it("Returns 500 response without retrying", async () => {
-    nock(TEST_HOST).get(TEST_PATH).thrice().reply(500);
+    nock(TEST_HOST).get(TEST_PATH).once().reply(500);
     let response;
     let errorCaught: any = undefined;
     try {
@@ -64,10 +70,11 @@ describe("fetchWithRetry", () => {
     expect(response).toBeDefined();
     expect(response.ok).toBe(false);
     expect(errorCaught).toBeUndefined();
+    expect(nock.isDone()).toBe(true);
   });
 
   it("Returns 200 response without retrying", async () => {
-    nock(TEST_HOST).get(TEST_PATH).thrice().reply(200);
+    nock(TEST_HOST).get(TEST_PATH).once().reply(200);
     let response;
     let errorCaught: any = undefined;
     try {
@@ -82,5 +89,6 @@ describe("fetchWithRetry", () => {
     expect(response).toBeDefined();
     expect(response.ok).toBe(true);
     expect(errorCaught).toBeUndefined();
+    expect(nock.isDone()).toBe(true);
   });
 });
