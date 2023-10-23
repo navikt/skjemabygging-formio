3462d53b79af7d4b275ef939fe71e8b923507b4f
Morten Stulen <morten.elias.stulen@nav.no>
2023-10-23 11:02:13 +0200

    Move status endpoint

--- packages/fyllut-backend/src/routers/internal/index.ts
+++ packages/fyllut-backend/src/routers/internal/index.ts
@@ -1,11 +1,9 @@
 import express from 'express';
 import metrics from './metrics';
-import status from './status';
 
 const internalRouter = express.Router();
 
 internalRouter.get('/isAlive|isReady', (req, res) => res.sendStatus(200));
 internalRouter.get('/metrics', metrics.get);
-internalRouter.get('/health/status', status.get);
 
 export default internalRouter;
