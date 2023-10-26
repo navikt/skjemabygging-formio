31befd036c2b5d9f7f5bea747c1f9dcb09f6427d
Morten Stulen <morten.elias.stulen@nav.no>
2023-10-26 12:14:27 +0200

    Typescript fix

--- packages/bygger-backend/src/config/types.d.ts
+++ packages/bygger-backend/src/config/types.d.ts
@@ -61,5 +61,5 @@ export type ConfigType = {
   isProduction: boolean;
   isDevelopment: boolean;
   featureToggles: { [key: string]: boolean };
-  naisClusterName: 'dev-gcp' | 'prod-gcp';
+  naisClusterName?: 'dev-gcp' | 'prod-gcp';
 };
