44e2c18e6fdae71b2d12f13b1ef4a66a3f735bd9
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2023-10-31 19:28:57 +0100

    Initialize prometheus metrics to zero. No labels will automatically lead to counter initializing with zero.

--- packages/fyllut-backend/src/services/AppMetrics.ts
+++ packages/fyllut-backend/src/services/AppMetrics.ts
@@ -2,10 +2,40 @@ import { Counter, Histogram, Registry, collectDefaultMetrics } from 'prom-client
 import { logger } from '../logger';
 
 class AppMetrics {
-  _register: Registry = new Registry();
+  private readonly _register: Registry;
+  private readonly _exstreamPdfRequestsCounter: Counter;
+  private readonly _exstreamPdfFailuresCounter: Counter;
+  private readonly _outgoingRequestDuration: Histogram;
 
   constructor() {
     logger.info('Initializing metrics client');
+
+    this._register = new Registry();
+
+    this._exstreamPdfRequestsCounter = new Counter({
+      name: 'fyllut_exstream_pdf_requests_total',
+      help: 'Number of exstream pdf requests',
+      labelNames: [],
+      registers: [this._register],
+    });
+
+    this._exstreamPdfFailuresCounter = new Counter({
+      name: 'fyllut_exstream_pdf_failures_total',
+      help: 'Number of exstream pdf requests which failed',
+      labelNames: [],
+      registers: [this._register],
+    });
+
+    this._outgoingRequestDuration = new Histogram({
+      name: 'fyllut_outgoing_request_duration_seconds',
+      help: 'Request duration for outgoing requests made by FyllUt',
+      labelNames: ['service', 'method', 'error'],
+      buckets: [0.7, 0.8, 0.9, 1.0, 1.2, 1.5, 2.0, 5.0, 15.0],
+      registers: [this._register],
+    });
+    this._outgoingRequestDuration.zero({ service: 'exstream', method: 'createPdf', error: 'false' });
+    this._outgoingRequestDuration.zero({ service: 'exstream', method: 'createPdf', error: 'true' });
+
     collectDefaultMetrics({ register: this._register });
   }
 
@@ -13,27 +43,17 @@ class AppMetrics {
     return this._register;
   }
 
-  public exstreamPdfRequestsCounter = new Counter({
-    name: 'fyllut_exstream_pdf_requests_total',
-    help: 'Number of exstream pdf requests',
-    labelNames: ['formPath', 'submissionMethod'],
-    registers: [this._register],
-  });
+  public get exstreamPdfRequestsCounter() {
+    return this._exstreamPdfRequestsCounter;
+  }
 
-  public exstreamPdfFailuresCounter = new Counter({
-    name: 'fyllut_exstream_pdf_failures_total',
-    help: 'Number of exstream pdf requests which failed',
-    labelNames: ['formPath', 'submissionMethod'],
-    registers: [this._register],
-  });
+  public get exstreamPdfFailuresCounter() {
+    return this._exstreamPdfFailuresCounter;
+  }
 
-  public outgoingRequestDuration = new Histogram({
-    name: 'fyllut_outgoing_request_duration_seconds',
-    help: 'Request duration for outgoing requests made by FyllUt',
-    labelNames: ['service', 'method', 'error'],
-    buckets: [0.7, 0.8, 0.9, 1.0, 1.2, 1.5, 2.0, 5.0, 15.0],
-    registers: [this._register],
-  });
+  public get outgoingRequestDuration() {
+    return this._outgoingRequestDuration;
+  }
 }
 
 export default AppMetrics;
