000328bd9b43ee586df60870bff92098759264ef
Lars Olav Torvik <lars.olav.torvik@nav.no>
2023-12-11 13:00:45 +0100

    On Aksel components we can only set the width style on the wrapper, so this is a workaround for that and keep the old styling. Should look for a better way to do this when we no longer use the old components.

--- packages/shared-components/src/formio/components/base/BaseComponent.tsx
+++ packages/shared-components/src/formio/components/base/BaseComponent.tsx
@@ -90,7 +90,7 @@ class BaseComponent extends FormioReactComponent {
    * Get class name for custom component renderReact()
    */
   getClassName() {
-    return this.component?.fieldSize;
+    return this.component?.fieldSize ? `nav-${this.component?.fieldSize}` : '';
   }
 
   /**
