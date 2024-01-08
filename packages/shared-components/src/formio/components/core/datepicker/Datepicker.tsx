52fa3ff91bc0c42a10031385b19cd975d3c92677
Magnus Røed Hestvik <mrh@computas.com>
2024-01-08 15:15:44 +0100

    Move validation functions out of component

--- packages/shared-components/src/formio/components/core/datepicker/Datepicker.tsx
+++ packages/shared-components/src/formio/components/core/datepicker/Datepicker.tsx
@@ -1,6 +1,5 @@
 import { DatePicker, DatePickerProps, useDatepicker } from '@navikt/ds-react';
 import { UseDatepickerOptions } from '@navikt/ds-react/esm/date/hooks/useDatepicker';
-import { TEXTS } from '@navikt/skjemadigitalisering-shared-domain';
 
 import moment from 'moment';
 import { useEffect } from 'react';
@@ -8,6 +7,7 @@ import FormioReactComponent from '../../FormioReactComponent';
 import BaseComponent from '../../base/BaseComponent';
 import datepickerBuilder from './Datepicker.builder';
 import datepickerForm from './Datepicker.form';
+import { validate, validateBackwardsCompatible } from './utils/validation';
 
 const SUBMISSION_DATE_FORMAT = 'YYYY-MM-DD';
 
@@ -47,10 +47,6 @@ const DatovelgerWrapper = ({ component, onChange, value, locale, readOnly, error
   );
 };
 
-function isCorrectOrder(beforeDate, afterDate, mayBeEqual = false) {
-  return mayBeEqual ? beforeDate.isSameOrBefore(afterDate, 'd') : beforeDate.isBefore(afterDate, 'd');
-}
-
 export default class NavDatepicker extends BaseComponent {
   isValid = this.errors.length === 0;
 
@@ -70,117 +66,29 @@ export default class NavDatepicker extends BaseComponent {
     return datepickerBuilder();
   }
 
-  showNorwegianOrTranslation(key, params?) {
-    if (params) {
-      return this.t(key) === key ? this.t(TEXTS.validering[key], params) : this.t(key, params);
-    }
-    return this.t(key) === key ? TEXTS.validering[key] : this.t(key);
-  }
-
-  validateToAndFromDate(beforeDate, inputDate, mayBeEqual) {
-    if (isCorrectOrder(beforeDate, inputDate, mayBeEqual)) {
-      return true;
-    }
-    const beforeDateAsString = beforeDate.format('DD.MM.YYYY');
-    return mayBeEqual
-      ? this.showNorwegianOrTranslation('dateNotBeforeFromDate', { fromDate: beforeDateAsString })
-      : this.showNorwegianOrTranslation('dateAfterFromDate', { fromDate: beforeDateAsString });
-  }
-
-  validateEarliestAndLatestDate(earliestFromToday = '', latestFromToday = '', inputDate) {
-    const earliestAllowedDate = !!String(earliestFromToday) ? moment().add(String(earliestFromToday), 'd') : undefined;
-    const latestAllowedDate = !!String(latestFromToday) ? moment().add(String(latestFromToday), 'd') : undefined;
-    return this.validateEarliestAndLatest(earliestAllowedDate, latestAllowedDate, inputDate);
-  }
-
-  validateEarliestAndLatest(earliestAllowedDate, latestAllowedDate, inputDate) {
-    const earliestAllowedDateAsString = earliestAllowedDate ? earliestAllowedDate.format('DD.MM.YYYY') : '';
-    const latestAllowedDateAsString = latestAllowedDate ? latestAllowedDate.format('DD.MM.YYYY') : '';
-    if (earliestAllowedDate && latestAllowedDate) {
-      if (!isCorrectOrder(earliestAllowedDate, latestAllowedDate, true)) {
-        return true;
-      }
-      return inputDate.isBefore(earliestAllowedDate, 'd') || inputDate.isAfter(latestAllowedDate, 'd')
-        ? `${this.showNorwegianOrTranslation('dateInBetween', {
-            minDate: earliestAllowedDateAsString,
-            maxDate: latestAllowedDateAsString,
-          })}`
-        : true;
-    }
-
-    if (earliestAllowedDate && inputDate.isBefore(earliestAllowedDate, 'd')) {
-      return `${this.showNorwegianOrTranslation('dateNotBeforeAllowedDate')} ${earliestAllowedDateAsString}`;
-    }
-
-    if (latestAllowedDate && inputDate.isAfter(latestAllowedDate, 'd')) {
-      return `${this.showNorwegianOrTranslation('dateNotLaterThanAllowedDate')} ${latestAllowedDateAsString}`;
-    }
-
-    return true;
-  }
-
   validateDatePicker(
     input,
     submissionData,
     beforeDateInputKey,
     mayBeEqual,
-    relativeEarliestAllowedDate = '',
-    relativeLatestAllowedDate = '',
+    relativeEarliestAllowedDate,
+    relativeLatestAllowedDate,
     row,
   ) {
-    if (!input) {
-      return true;
-    }
-
-    let toAndFromDateValidation = true;
-    if (beforeDateInputKey) {
-      const beforeDateValue =
-        submissionData[beforeDateInputKey] ||
-        (beforeDateInputKey.includes('.') && row && row[beforeDateInputKey.replace(/.*\./i, '')]);
-      if (beforeDateValue) {
-        toAndFromDateValidation = this.validateToAndFromDate(moment(beforeDateValue), moment(input), mayBeEqual);
-      }
-    }
-
-    const earliestFromToday = String(relativeEarliestAllowedDate);
-    const latestFromToday = String(relativeLatestAllowedDate);
-
-    const earliestAndLatestDateValidation =
-      !!earliestFromToday || !!latestFromToday
-        ? this.validateEarliestAndLatestDate(earliestFromToday, latestFromToday, moment(input))
-        : true;
-
-    if (typeof toAndFromDateValidation === 'string') {
-      return toAndFromDateValidation;
-    }
-    if (typeof earliestAndLatestDateValidation === 'string') {
-      return earliestAndLatestDateValidation;
-    }
-    return true;
+    return validateBackwardsCompatible(
+      input,
+      submissionData,
+      beforeDateInputKey,
+      mayBeEqual,
+      relativeEarliestAllowedDate,
+      relativeLatestAllowedDate,
+      row,
+      this.t,
+    );
   }
 
   validateDatePickerV2(input, submissionData, component, row) {
-    if (!input) {
-      return true;
-    }
-
-    const result = this.validateDatePicker(
-      input,
-      submissionData,
-      component.beforeDateInputKey,
-      component.mayBeEqual,
-      component.earliestAllowedDate,
-      component.latestAllowedDate,
-      row,
-    );
-    if (result === true) {
-      const { specificEarliestAllowedDate, specificLatestAllowedDate } = component;
-
-      const earliest = specificEarliestAllowedDate ? moment(specificEarliestAllowedDate) : undefined;
-      const latest = specificLatestAllowedDate ? moment(specificLatestAllowedDate) : undefined;
-      return this.validateEarliestAndLatest(earliest, latest, moment(input));
-    }
-    return result;
+    return validate(input, submissionData, component, row, this.t);
   }
 
   setValueOnReactInstance(_value) {}
