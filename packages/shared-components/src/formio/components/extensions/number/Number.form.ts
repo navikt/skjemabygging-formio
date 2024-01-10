e0b454f7855a84d8724b7654fbf087ade044ef08
Lars Olav Torvik <lars.olav.torvik@nav.no>
2024-01-10 09:43:02 +0100

    Update checkboxForm, radioForm, numberForm

--- packages/shared-components/src/formio/components/extensions/number/Number.form.ts
+++ packages/shared-components/src/formio/components/extensions/number/Number.form.ts
@@ -1,158 +1,39 @@
-import NumberEditForm from 'formiojs/components/number/Number.form';
-import NumberDisplayEditForm from 'formiojs/components/number/editForm/Number.edit.display';
-import { advancedDescription } from '../../fields/advancedDescription.js';
-import { fieldSizeField } from '../../fields/fieldSize.js';
+import editFormApi from '../../base/editForm/api';
+import editFormConditional from '../../base/editForm/conditional';
+import editFormData from '../../base/editForm/data';
+import editFormDisplay from '../../base/editForm/display';
+import editFormTabs from '../../base/editForm/editFormTabs';
+import editFormValidation from '../../base/editForm/validation';
 
 const numberForm = () => {
-  return NumberEditForm([
-    {
-      label: 'Display',
-      key: 'display',
-      components: [
-        ...NumberDisplayEditForm,
-        fieldSizeField,
-        ...advancedDescription,
-        {
-          key: 'placeholder',
-          ignore: true,
-        },
-        {
-          key: 'labelPosition',
-          ignore: true,
-        },
-        {
-          key: 'tabindex',
-          ignore: true,
-        },
-        {
-          key: 'tooltip',
-          ignore: true,
-        },
-        {
-          key: 'customClass',
-          ignore: true,
-        },
-        {
-          key: 'hidden',
-          ignore: true,
-        },
-        {
-          key: 'hideLabel',
-          ignore: true,
-        },
-        {
-          key: 'autofocus',
-          ignore: true,
-        },
-        {
-          key: 'tableView',
-          ignore: true,
-        },
-        {
-          key: 'modalEdit',
-          ignore: true,
-        },
-        {
-          key: 'widget.type',
-          ignore: true,
-        },
-        {
-          key: 'inputMask',
-          ignore: true,
-        },
-        {
-          key: 'allowMultipleMasks',
-          ignore: true,
-        },
-        {
-          key: 'mask',
-          ignore: true,
-        },
-        {
-          key: 'spellcheck',
-          ignore: true,
-        },
-        { key: 'displayMask', ignore: true },
-        { key: 'autocomplete', ignore: true },
-        { key: 'disabled', ignore: true },
-      ],
-    },
-    {
-      key: 'data',
-      components: [
-        {
-          key: 'multiple',
-          ignore: true,
-        },
-        {
-          key: 'persistent',
-          ignore: true,
-        },
-        {
-          key: 'inputFormat',
-          ignore: true,
-        },
-        {
-          key: 'protected',
-          ignore: true,
-        },
-        {
-          key: 'dbIndex',
-          ignore: true,
-        },
-        {
-          key: 'case',
-          ignore: true,
-        },
-        {
-          key: 'encrypted',
-          ignore: true,
-        },
-        {
-          key: 'redrawOn',
-          ignore: true,
-        },
-        {
-          key: 'calculateServer',
-          ignore: true,
-        },
-        {
-          key: 'allowCalculateOverride',
-          ignore: true,
-        },
-        { key: 'truncateMultipleSpaces', ignore: true },
-        { key: 'decimalLimit', ignore: true },
-        { key: 'requireDecimal', ignore: true },
-      ],
-    },
-    {
-      key: 'validation',
-      components: [
-        {
-          key: 'unique',
-          ignore: true,
-        },
-        { key: 'validateOn', ignore: true },
-      ],
-    },
-    {
-      key: 'api',
-      components: [
-        { key: 'tags', ignore: true },
-        { key: 'properties', ignore: true },
-      ],
-    },
-    {
-      key: 'logic',
-      ignore: true,
-      components: false,
-    },
-    {
-      key: 'layout',
-      ignore: true,
-      components: false,
-    },
-  ]);
+  const { api, conditional, createTabs, data, display, validation } = editFormTabs;
+
+  // prettier-ignore
+  return createTabs(
+    display([
+      editFormDisplay.label(),
+      editFormDisplay.fieldSize(),
+      editFormDisplay.description(),
+      editFormDisplay.additionalDescription(),
+    ]),
+    data([
+      editFormData.clearOnHide(),
+    ]),
+    validation([
+      editFormValidation.required(),
+      editFormValidation.minLength(),
+      editFormValidation.maxLength(),
+      editFormValidation.customValidation(),
+      editFormValidation.customError(),
+    ]),
+    api([
+      editFormApi.key(),
+    ]),
+    conditional([
+      editFormConditional.simpleConditional(),
+      editFormConditional.advancedConditional(),
+    ]),
+  );
 };
 
 export default numberForm;
