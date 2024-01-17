6279deff60050fd6d21c77d9faa272bc04a49dd2
Morten Stulen <morten.elias.stulen@nav.no>
2024-01-17 13:03:24 +0100

    Add tests

--- packages/fyllut/cypress/e2e/custom-react-components.cy.ts
+++ packages/fyllut/cypress/e2e/custom-react-components.cy.ts
@@ -448,12 +448,39 @@ describe('Custom react components', () => {
       cy.wait('@getAlertForm');
     });
 
+    // New alerts (using react)
     it('should show default info with content', () => {
-      cy.get('.navds-alert').contains('Testing info alert');
+      cy.get('.navds-alert').contains('New alert 1');
     });
 
-    it('should show success variant with conteent', () => {
-      cy.get('.navds-alert--success').contains('Testing success alert');
+    it('should show success variant with content', () => {
+      cy.get('.navds-alert--success').contains('New alert 2');
+    });
+
+    it('should have marginBottom of var(--a-spacing-10) which is 2.5rem = 40px', () => {
+      cy.contains('.formio-component-alertstripe', 'New alert 1').should('have.css', 'marginBottom', '40px');
+    });
+
+    it('should have marginBottom of 0px as the last element in the group', () => {
+      cy.contains('.formio-component-alertstripe', 'New alert 2').should('have.css', 'marginBottom', '0px');
+    });
+
+    it('should not show any labels', () => {
+      cy.get('.formio-form').should('not.have.descendants', 'label');
+    });
+
+    it('should display html content - h1 and a link to nav.no', () => {
+      cy.get('.formio-component-alertstripehtml').find('h1').should('have.text', 'Tittel');
+      cy.get('.formio-component-alertstripehtml').find('a').should('have.attr', 'href', 'https://www.nav.no/');
+    });
+
+    // Old alerts (components have input=true on component and label defined)
+    it('should have marginBottom from input=true of var(--a-spacing-10) which is 2.5rem = 40px', () => {
+      cy.contains('.formio-component-alertstripe', 'Old alert 1').should('have.css', 'marginBottom', '40px');
+    });
+
+    it('should have marginBottom from input=true of var(--a-spacing-10) which is 2.5rem = 40px for last element in group', () => {
+      cy.contains('.formio-component-alertstripe', 'Old alert 2').should('have.css', 'marginBottom', '40px');
     });
   });
 });
