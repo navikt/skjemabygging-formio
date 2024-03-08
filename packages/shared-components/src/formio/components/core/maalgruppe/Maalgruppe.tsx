76897dff0795f58e14b12eec96c988ad46feabb5
Thomas Tykesson <thomas@tykesson.com>
2024-03-08 11:35:20 +0100

    Move findSelectedMaalgruppe to new .utils -file

--- packages/shared-components/src/formio/components/core/maalgruppe/Maalgruppe.tsx
+++ packages/shared-components/src/formio/components/core/maalgruppe/Maalgruppe.tsx
@@ -1,48 +1,12 @@
 import BaseComponent from '../../base/BaseComponent';
 import maalgruppeBuilder from './Maalgruppe.builder';
 import maalgruppeForm from './Maalgruppe.form';
+import { findSelectedMaalgruppe } from './Maalgruppe.utils';
 
 export interface MaalgruppeValueType {
   calculated?: string;
   prefilled?: string;
 }
-
-type MaalgruppeMapValue = { priority: number; kodeverkVerdi: string };
-
-const maalgruppeMap: Record<string, MaalgruppeMapValue> = {
-  aapUforeNedsattArbEvne: { priority: 1, kodeverkVerdi: 'NEDSARBEVN' },
-  ensligUtdanning: { priority: 2, kodeverkVerdi: 'ENSFORUTD' },
-  ensligArbSoker: { priority: 3, kodeverkVerdi: 'ENSFORARBS' },
-  tidligereFamiliepleier: { priority: 4, kodeverkVerdi: 'TIDLFAMPL' },
-  gjenlevendeUtdanning: { priority: 5, kodeverkVerdi: 'GJENEKUTD' },
-  gjenlevendeArbSoker: { priority: 6, kodeverkVerdi: 'GJENEKARBS' },
-  tiltakspenger: { priority: 7, kodeverkVerdi: 'MOTTILTPEN' },
-  dagpenger: { priority: 8, kodeverkVerdi: 'MOTDAGPEN' },
-  regArbSoker: { priority: 9, kodeverkVerdi: 'ARBSOKERE' },
-  annet: { priority: 10, kodeverkVerdi: 'ANNET' },
-};
-
-const flattenedData = (data: { [key: string]: unknown }) =>
-  Object.entries(data).reduce<{ [key: string]: unknown }>(
-    (partialData, [key, value]) => ({
-      ...partialData,
-      ...(value && typeof value === 'object' ? value : { [key]: value }),
-    }),
-    {},
-  );
-
-const findSelectedMaalgruppe = (data: { [key: string]: unknown }) => {
-  return Object.entries(flattenedData(data)).reduce<MaalgruppeMapValue | null>(
-    (prevResult, [key, value]) =>
-      (value === true || value === 'ja') &&
-      maalgruppeMap[key] &&
-      maalgruppeMap[key].priority < (prevResult?.priority || Infinity)
-        ? maalgruppeMap[key]
-        : prevResult,
-    null,
-  )?.kodeverkVerdi;
-};
-
 class Maalgruppe extends BaseComponent {
   static schema() {
     return BaseComponent.schema({
