306a8faa02e54270c73b84be51074c7ae0521836
Morten Stulen <morten.elias.stulen@nav.no>
2024-03-13 09:49:58 +0100

    Fix driving list tests

--- packages/fyllut/cypress/e2e/components/driving-list.cy.ts
+++ packages/fyllut/cypress/e2e/components/driving-list.cy.ts
@@ -146,7 +146,7 @@ describe('DrivingList', () => {
       cy.findByRole('group', { name: ACTIVITIES_LABEL })
         .should('exist')
         .within(() => {
-          cy.findByRole('radio', { name: 'Arbeidstrening: 01.12.2023 - 06.04.2024' }).should('exist').check();
+          cy.findByRole('radio', { name: 'Arbeidstrening: 01.01.2024 - 31.08.2024' }).should('exist').check();
         });
 
       cy.get('.navds-alert').within(() => {
@@ -154,24 +154,24 @@ describe('DrivingList', () => {
         cy.findByText('Arbeidstrening').should('exist');
 
         cy.findByRole('heading', { name: 'Periode for aktiviteten' }).should('exist');
-        cy.findByText('01.12.2023 - 06.04.2024').should('exist');
+        cy.findByText('01.01.2024 - 31.08.2024').should('exist');
 
         cy.findByRole('heading', { name: 'Din dagsats uten parkeringsutgift' }).should('exist');
-        cy.findByText('63kr').should('exist');
+        cy.findByText('67kr').should('exist');
       });
 
       cy.findByRole('heading', { name: 'Perioder du tidligere har fått refundert reiseutgifter for' }).should('exist');
-      cy.findByText('14.12.2023 - 20.12.2023 (315 kr)').should('exist');
+      cy.findByText('01.01.2024 - 07.01.2024 (335 kr)').should('exist');
 
-      cy.findByRole('button', { name: '01.12.2023 - 07.12.2023' }).click();
-      cy.findByRole('button', { name: '08.12.2023 - 14.12.2023' }).click();
+      cy.findByRole('button', { name: '08.01.2024 - 14.01.2024' }).click();
+      cy.findByRole('button', { name: '15.01.2024 - 21.01.2024' }).click();
 
       cy.findAllByRole('group', { name: DAY_PICKER_LABEL_WITH_PARKING })
         .should('exist')
         .first()
         .within(() => {
           cy.findAllByRole('checkbox').should('have.length', 7);
-          cy.findByRole('checkbox', { name: 'lørdag 02.12.23' }).should('exist').check();
+          cy.findByRole('checkbox', { name: 'lørdag 13.01.24' }).should('exist').check();
           cy.findByRole('textbox', { name: PARKING_EXPENSES_LABEL }).should('exist').type('100');
         });
 
@@ -180,7 +180,7 @@ describe('DrivingList', () => {
         .last()
         .within(() => {
           cy.findAllByRole('checkbox').should('have.length', 7);
-          cy.findByRole('checkbox', { name: 'fredag 08.12.23' }).should('exist').check();
+          cy.findByRole('checkbox', { name: 'fredag 19.01.24' }).should('exist').check();
         });
 
       cy.clickSaveAndContinue();
@@ -192,8 +192,8 @@ describe('DrivingList', () => {
           cy.get('dt').eq(0).should('contain.text', 'Legg til kjøreliste for en eller flere perioder');
 
           cy.findAllByRole('listitem').should('have.length', 2);
-          cy.findByText('02.12.2023 - 100kr').should('exist');
-          cy.findByText('08.12.2023').should('exist');
+          cy.findByText('13.01.2024 - 100kr').should('exist');
+          cy.findByText('19.01.2024').should('exist');
         });
     });
 
@@ -203,17 +203,17 @@ describe('DrivingList', () => {
       cy.defaultWaits();
       cy.wait('@getActivities');
 
-      cy.findByRole('radio', { name: 'Arbeidstrening: 01.12.2023 - 06.04.2024' }).should('be.checked');
+      cy.findByRole('radio', { name: 'Arbeidstrening: 01.01.2024 - 31.08.2024' }).should('be.checked');
 
-      cy.findByRole('button', { name: '01.12.2023 - 07.12.2023' }).click();
-      cy.findByRole('checkbox', { name: 'fredag 01.12.23' }).should('be.checked');
-      cy.findByRole('checkbox', { name: 'mandag 04.12.23' }).should('be.checked');
+      cy.findByRole('button', { name: '08.01.2024 - 14.01.2024' }).click();
+      cy.findByRole('checkbox', { name: 'mandag 08.01.24' }).should('be.checked');
+      cy.findByRole('checkbox', { name: 'fredag 12.01.24' }).should('be.checked');
       cy.findAllByRole('textbox', { name: PARKING_EXPENSES_LABEL }).eq(0).should('have.value', '');
       cy.findAllByRole('textbox', { name: PARKING_EXPENSES_LABEL }).eq(1).should('have.value', '50');
 
-      cy.findByRole('button', { name: '08.12.2023 - 14.12.2023' }).click();
+      cy.findByRole('button', { name: '15.01.2024 - 21.01.2024' }).click();
       cy.findAllByRole('textbox', { name: PARKING_EXPENSES_LABEL }).eq(2).should('have.value', '100');
-      cy.findByRole('checkbox', { name: 'søndag 10.12.23' }).should('be.checked');
+      cy.findByRole('checkbox', { name: 'mandag 15.01.24' }).should('be.checked');
     });
 
     it('should show errors', () => {
@@ -226,15 +226,17 @@ describe('DrivingList', () => {
       cy.clickSaveAndContinue();
       cy.findByRole('link', { name: `Du må fylle ut: ${ACTIVITIES_LABEL}` }).should('exist');
 
-      cy.findByRole('radio', { name: 'Arbeidstrening: 01.12.2023 - 06.04.2024' }).should('exist').check();
+      cy.findByRole('radio', { name: 'Arbeidstrening: 01.01.2024 - 31.08.2024' }).should('exist').check();
 
       // Expenses are higher than the refund limit
-      cy.findByRole('button', { name: '01.12.2023 - 07.12.2023' }).click();
-      cy.findByRole('checkbox', { name: 'fredag 01.12.23' }).should('exist').check();
-      cy.findByRole('checkbox', { name: 'lørdag 02.12.23' }).should('exist').check();
+      cy.findByRole('button', { name: '08.01.2024 - 14.01.2024' }).click();
+      cy.findByRole('checkbox', { name: 'fredag 12.01.24' }).should('exist').check();
+      cy.findByRole('checkbox', { name: 'lørdag 13.01.24' }).should('exist').check();
+      cy.findByRole('checkbox', { name: 'søndag 14.01.24' }).should('exist').check();
 
       cy.findAllByRole('textbox', { name: PARKING_EXPENSES_LABEL }).eq(0).should('exist').type('100');
       cy.findAllByRole('textbox', { name: PARKING_EXPENSES_LABEL }).eq(1).should('exist').type('100');
+      cy.findAllByRole('textbox', { name: PARKING_EXPENSES_LABEL }).eq(2).should('exist').type('100');
 
       cy.get('.navds-alert')
         .eq(1)
@@ -243,7 +245,7 @@ describe('DrivingList', () => {
         });
     });
 
-    it.only('should render alert when there are no activities and error when trying to continue', () => {
+    it('should render alert when there are no activities and error when trying to continue', () => {
       cy.visit(`/fyllut/testdrivinglist?sub=digital`);
       cy.defaultWaits();
       cy.mocksUseRouteVariant('get-activities:success-empty');
