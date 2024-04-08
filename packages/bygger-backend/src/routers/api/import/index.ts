62b0960f60cefa942d3e71ccd17d58c3ae214cf4
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2024-04-08 15:55:19 +0200

    check naisClusterName

--- packages/bygger-backend/src/routers/api/import/index.ts
+++ packages/bygger-backend/src/routers/api/import/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import config from '../../../config';
 import { copyService } from '../../../services';
 
 const importRouter = express.Router();
 
 importRouter.get('/source-forms', async (req, res, next) => {
   try {
-    if (!copyService) {
+    if (config.naisClusterName === 'prod-gcp' || !copyService) {
       return res.sendStatus(405);
     }
     res.json(await copyService.getSourceForms());
