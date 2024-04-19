6e9dddc4de9ec0acfe2e7cb26d3a1a8ce12c300c
Morten Stulen <morten.elias.stulen@nav.no>
2024-04-19 14:57:37 +0200

    Update text

--- packages/shared-components/src/formio/components/base/editForm/data/editFormIsAttachmentPanel.ts
+++ packages/shared-components/src/formio/components/base/editForm/data/editFormIsAttachmentPanel.ts
@@ -8,7 +8,7 @@ const editFormIsAttachmentPanel = (): Component => {
     defaultValue: false,
     additionalDescriptionLabel: 'Hva innebærer dette?',
     additionalDescriptionText:
-      'Vedleggspanelet inneholder alle de relevante vedleggene for søknaden. Panelet vil være synlig ved papirinnsending og være skjult ved digital innsending. Med digital innsending vil man kunne laste opp vedleggene i send-inn løsningen.',
+      'Vedleggspanelet inneholder alle de relevante vedleggene for søknaden. Panelet vil være synlig ved papirinnsending og være skjult ved digital innsending. Med digital innsending vil man kunne laste opp vedleggene på opplastningssiden etter utfylt søknad.',
   };
 };
 
