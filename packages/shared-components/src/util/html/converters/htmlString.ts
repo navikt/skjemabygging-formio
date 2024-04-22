bc69196995af495f7461a2ec210ffb65d7e16885
Magnus Røed Hestvik <mrh@computas.com>
2024-04-22 15:11:44 +0200

    Exclude br-tags from html check

--- packages/shared-components/src/util/html/converters/htmlString.ts
+++ packages/shared-components/src/util/html/converters/htmlString.ts
@@ -1,7 +1,11 @@
 import { HtmlAsJsonElement, HtmlAsJsonTextElement } from './htmlAsJson';
 import { fromElement, toNode } from './htmlNode';
 
-const isHtmlString = (text: string) => /<[^>]*>/.test(text);
+/**
+ * Regex matches that there is an html-tag in the string,
+ * excluding the <br>-tag (with possible whitespace and self-closing "/").
+ */
+const isHtmlString = (text: string) => /<(?!br\s*\/?)[^>]+>/gm.test(text);
 
 const htmlString2Json = (htmlString: string): HtmlAsJsonElement => {
   const div = document.createElement('div');
