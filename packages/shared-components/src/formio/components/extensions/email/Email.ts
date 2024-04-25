d8d451eb63159af4be6f5eb8db15ca8ebd057dd0
Morten Stulen <morten.elias.stulen@nav.no>
2024-04-25 15:12:43 +0200

    Render email component with react

--- packages/shared-components/src/formio/components/extensions/email/Email.ts
+++ packages/shared-components/src/formio/components/extensions/email/Email.ts
@@ -1,9 +1,27 @@
-import FormioEmail from 'formiojs/components/email/Email';
+import BaseComponent from '../../base/BaseComponent';
+import TextField from '../../core/textfield/TextField';
+import emailBuilder from './Email.builder';
 import emailForm from './Email.form';
 
-class Email extends FormioEmail {
+class Email extends TextField {
+  static schema() {
+    return BaseComponent.schema({
+      label: 'E-post',
+      type: 'email',
+      key: 'epost',
+    });
+  }
+
   static editForm() {
     return emailForm();
+  }
+  static get builderInfo() {
+    return emailBuilder();
+  }
+
+  init() {
+    super.init();
+    this.validators.push('email');
   }
 }
 
