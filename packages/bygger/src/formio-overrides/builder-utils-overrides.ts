1696a01dfa024b93e8eee03291f93baca1973f62
Morten Stulen <morten.elias.stulen@nav.no>
2024-05-14 13:45:13 +0200

    Remove logging

--- packages/bygger/src/formio-overrides/builder-utils-overrides.ts
+++ packages/bygger/src/formio-overrides/builder-utils-overrides.ts
@@ -4,9 +4,7 @@ import { Component } from '@navikt/skjemadigitalisering-shared-domain';
 const originalUniqify = NavFormioJs.BuilderUtils.uniquify;
 const { eachComponent, getRandomComponentId } = NavFormioJs.Utils;
 
-console.log('Builder utils imported');
-NavFormioJs.BuilderUtils.uniquify = function (container, component: Component) {
-  console.log('Uniquifying component', container, component);
+NavFormioJs.BuilderUtils.uniquify = function (container: Component[], component: Component) {
   eachComponent(
     [component],
     (component: Component) => {
