3f41209cb46e39561f851fb9435d62546a56b660
Lars Olav Torvik <lars.olav.torvik@nav.no>
2024-05-23 14:04:59 +0200

    Test that lefferVedNaa gets included.

--- packages/shared-components/src/util/forsteside/forsteside.test.ts
+++ packages/shared-components/src/util/forsteside/forsteside.test.ts
@@ -106,6 +106,48 @@ describe('getVedleggsFelterSomSkalSendes', () => {
     );
     expect(actual.map((component) => component.key)).toEqual(['vedlegg1', 'vedlegg2', 'vedlegg3']);
   });
+
+  describe('handles new attachment type', () => {
+    it('onlye leggerVedNaa should be included', () => {
+      const actual = getVedleggsFelterSomSkalSendes(
+        {
+          vedlegg1: {
+            key: 'ettersender',
+          },
+          vedlegg2: {
+            key: 'nei',
+          },
+          vedlegg3: {
+            key: 'leggerVedNaa',
+          },
+          vedlegg4: {
+            key: 'levertTidligere',
+          },
+          vedlegg5: {
+            key: 'harIkke',
+          },
+          vedlegg6: {
+            key: 'andre',
+          },
+          vedlegg7: {
+            key: 'nav',
+          },
+        },
+        {
+          components: [
+            genererVedleggComponent('vedlegg1', 'Label 1', 'O9', 'Vedleggstittel 1'),
+            genererVedleggComponent('vedlegg2', 'Label 2', 'O9', 'Vedleggstittel 2'),
+            genererVedleggComponent('vedlegg3', 'Label 3', 'Q7', 'Vedleggstittel 3'),
+            genererVedleggComponent('vedlegg4', 'Label 4', 'Q7', 'Vedleggstittel 4'),
+            genererVedleggComponent('vedlegg5', 'Label 5', 'Q7', 'Vedleggstittel 5'),
+            genererVedleggComponent('vedlegg6', 'Label 6', 'Q7', 'Vedleggstittel 6'),
+            genererVedleggComponent('vedlegg7', 'Label 7', 'Q7', 'Vedleggstittel 7'),
+          ],
+        } as unknown as NavFormType,
+      );
+      expect(actual.map((component) => component.key)).toEqual(['vedlegg3']);
+    });
+  });
 });
 
 const formMedVedlegg = {
