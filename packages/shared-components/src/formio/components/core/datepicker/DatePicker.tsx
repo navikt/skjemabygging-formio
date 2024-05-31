8e39ff4b201aed9db905c1d2832cc4aff4ae4d1a
Magnus Røed Hestvik <mrh@computas.com>
2024-05-31 12:38:05 +0200

    Remove getDefaultValue

--- packages/shared-components/src/formio/components/core/datepicker/DatePicker.tsx
+++ packages/shared-components/src/formio/components/core/datepicker/DatePicker.tsx
@@ -113,7 +113,7 @@ export default class DatePicker extends BaseComponent {
         <ReactDatePicker
           id={this.getId()}
           required={this.isRequired()}
-          value={this.getDefaultValue()}
+          value={this.getValue()}
           onChange={this.onUpdate.bind(this)}
           readOnly={this.getReadOnly()}
           error={this.getError()}
