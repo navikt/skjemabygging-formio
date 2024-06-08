d713993f35cf363a7676af12f9a7fd18e633992f
Magnus Røed Hestvik <mrh@computas.com>
2024-06-08 13:57:08 +0200

    change to react parent class

--- packages/shared-components/src/formio/components/extensions/phone-number/PhoneNumber.ts
+++ packages/shared-components/src/formio/components/extensions/phone-number/PhoneNumber.ts
@@ -1,9 +1,30 @@
-import FormioPhoneNumber from 'formiojs/components/phonenumber/PhoneNumber';
+import { InputMode } from '@navikt/skjemadigitalisering-shared-domain';
+import BaseComponent from '../../base/BaseComponent';
+import TextField from '../../core/textfield/TextField';
+import phoneNumberBuilder from './PhoneNumber.builder';
 import phoneNumberForm from './PhoneNumber.form';
 
-class PhoneNumber extends FormioPhoneNumber {
+class PhoneNumber extends TextField {
+  static schema() {
+    return BaseComponent.schema({
+      label: 'Telefonnummer',
+      type: 'phoneNumber',
+      key: 'telefonNummer',
+      inputMode: 'decimal', // FIXME: trenger vi denne?
+      inputType: 'tel',
+    });
+  }
+
   static editForm() {
     return phoneNumberForm();
+  }
+
+  static get builderInfo() {
+    return phoneNumberBuilder();
+  }
+
+  getInputMode(): InputMode {
+    return this.component?.inputType || 'tel';
   }
 }
 
