9c400ec8030ceb8e205951740d75f33ab3520e1b
Magnus Røed Hestvik <mrh@computas.com>
2024-06-17 12:30:28 +0200

    Add global attachment option text

--- packages/bygger/src/translations/utils.ts
+++ packages/bygger/src/translations/utils.ts
@@ -17,6 +17,7 @@ import {
   NavFormType,
   navFormUtils,
   signatureUtils,
+  TEXTS,
 } from '@navikt/skjemadigitalisering-shared-domain';
 
 type TextObjectType = { text: string; type?: InputType };
@@ -183,10 +184,12 @@ const getAttachmentTexts = (attachmentValues?: AttachmentSettingValues): undefin
     return undefined;
   }
 
-  return Object.values(attachmentValues).flatMap((value: AttachmentSettingValue) => {
-    return value.additionalDocumentation
+  return Object.entries(attachmentValues).flatMap(([key, value]: [string, AttachmentSettingValue]) => {
+    const option = value.enabled ? [TEXTS.statiske.attachment[key]] : [];
+    const additionalDocumentation = value.additionalDocumentation
       ? [value?.additionalDocumentation?.label, value?.additionalDocumentation?.description]
       : [];
+    return [...option, ...additionalDocumentation];
   });
 };
 
