2521076491bd5b1800a51cd0ae9533ab2a660c8c
Lars Olav Torvik <lars.olav.torvik@nav.no>
2024-06-26 16:57:47 +0200

    Split up exists checks to make test more stable for locale run

--- packages/fyllut/cypress/e2e/components/currency-and-account.cy.ts
+++ packages/fyllut/cypress/e2e/components/currency-and-account.cy.ts
@@ -13,7 +13,8 @@ describe('Components', () => {
     });
 
     it('triggers errors', () => {
-      cy.findByRole('textbox', { name: 'Kontonummer' }).should('exist').type('12345678980');
+      cy.findByRole('textbox', { name: 'Kontonummer' }).should('exist');
+      cy.findByRole('textbox', { name: 'Kontonummer' }).type('12345678980');
       cy.findByRole('textbox', { name: 'IBAN' }).should('exist').type('AB04RABO8424598490');
       cy.clickNextStep();
 
@@ -31,10 +32,14 @@ describe('Components', () => {
     });
 
     it('allows kontonummer that starts with a 0', () => {
-      cy.findByRole('textbox', { name: 'Kontonummer' }).should('exist').type('01234567892');
-      cy.findByRole('textbox', { name: 'IBAN' }).should('exist').type('NL04RABO8424598490');
-      cy.findByRole('textbox', { name: 'Beløp' }).should('exist').type('450');
+      cy.findByRole('textbox', { name: 'Kontonummer' }).should('exist');
+      cy.findByRole('textbox', { name: 'Kontonummer' }).type('01234567892');
+      cy.findByRole('textbox', { name: 'IBAN' }).should('exist');
+      cy.findByRole('textbox', { name: 'IBAN' }).type('NL04RABO8424598490');
+      cy.findByRole('textbox', { name: 'Beløp' }).should('exist');
+      cy.findByRole('textbox', { name: 'Beløp' }).type('450');
       cy.findByRole('combobox', { name: 'Velg valuta' }).should('exist').click();
+      cy.findByRole('combobox', { name: 'Velg valuta' }).click();
       cy.findByRole('combobox', { name: 'Velg valuta' }).type('Nor{enter}');
 
       cy.clickNextStep();
@@ -47,7 +52,7 @@ describe('Components', () => {
           cy.get('dt').eq(1).should('contain.text', 'IBAN');
           cy.get('dd').eq(1).should('contain.text', 'NL04RABO8424598490');
           cy.get('dt').eq(2).should('contain.text', 'Angi valuta og beløp');
-          cy.get('dd').eq(2).should('contain.text', 'NOK 450'.replace(' ', '\u00A0'));
+          cy.get('dd').eq(2).should('contain.text', '450,00\u00A0NOK');
         });
     });
   });
