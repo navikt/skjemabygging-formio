6b84bd25966503f3238b66c98ada3e99df6c7ab9
Magnus Røed Hestvik <mrh@computas.com>
2024-09-11 13:24:59 +0200

    Add maximum 2 decimals as default in toLocaleString

--- packages/shared-domain/src/utils/numberUtils.ts
+++ packages/shared-domain/src/utils/numberUtils.ts
@@ -36,7 +36,7 @@ const isSmallerOrEqualMax = (value?: string | number, max?: string | number) => 
   return valueFloat <= maxFloat;
 };
 
-const toLocaleString = (value?: string | number, options: Intl.NumberFormatOptions = {}) => {
+const toLocaleString = (value?: string | number, options: Intl.NumberFormatOptions = { maximumFractionDigits: 2 }) => {
   if (value === undefined || value === null || value === '') {
     return '';
   }
