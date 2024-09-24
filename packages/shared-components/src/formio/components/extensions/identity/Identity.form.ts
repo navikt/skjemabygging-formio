be71e1b40e72a40119f1098d2784f58ade002529
Lars Olav Torvik <lars.olav.torvik@nav.no>
2024-09-24 16:04:22 +0200

    Remove label from identity

--- packages/shared-components/src/formio/components/extensions/identity/Identity.form.ts
+++ packages/shared-components/src/formio/components/extensions/identity/Identity.form.ts
@@ -1,18 +1,13 @@
 import editFormApi from '../../base/editForm/api';
 import editFormConditional from '../../base/editForm/conditional';
 import editFormData from '../../base/editForm/data';
-import editFormDisplay from '../../base/editForm/display';
 import editFormTabs from '../../base/editForm/editFormTabs';
 
 const identityForm = () => {
-  const { api, data, conditional, createTabs, display } = editFormTabs;
+  const { api, data, conditional, createTabs } = editFormTabs;
 
   // prettier-ignore
   return createTabs(
-    display([
-      editFormDisplay.label(),
-      editFormDisplay.description(),
-    ]),
     data([
       editFormData.prefillKey({prefillKey: 'sokerIdentifikasjonsnummer'}),
     ]),
