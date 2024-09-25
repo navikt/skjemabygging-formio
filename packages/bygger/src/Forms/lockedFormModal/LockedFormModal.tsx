89d4ced2aca7d494222b2e65b9e84381d65e3571
Magnus Røed Hestvik <mrh@computas.com>
2024-09-25 13:46:15 +0200

    Use predefined margin classname

--- packages/bygger/src/Forms/lockedFormModal/LockedFormModal.tsx
+++ packages/bygger/src/Forms/lockedFormModal/LockedFormModal.tsx
@@ -1,5 +1,5 @@
 import { BodyShort } from '@navikt/ds-react';
-import { ConfirmationModal, makeStyles } from '@navikt/skjemadigitalisering-shared-components';
+import { ConfirmationModal } from '@navikt/skjemadigitalisering-shared-components';
 import { NavFormType } from '@navikt/skjemadigitalisering-shared-domain';
 
 interface Props {
@@ -8,15 +8,7 @@ interface Props {
   onClose: () => void;
 }
 
-const useStyles = makeStyles({
-  content: {
-    marginBottom: 'var(--a-spacing-4)',
-  },
-});
-
 const LockedFormModal = ({ form, open, onClose }: Props) => {
-  const styles = useStyles();
-
   return (
     <ConfirmationModal
       open={open}
@@ -28,7 +20,7 @@ const LockedFormModal = ({ form, open, onClose }: Props) => {
       }}
       children={
         <>
-          <BodyShort className={styles.content}>{form.properties.lockedFormReason}</BodyShort>
+          <BodyShort className="mb-4">{form.properties.lockedFormReason}</BodyShort>
           <BodyShort>{'Gå til instillinger for å låse opp skjemaet.'}</BodyShort>
         </>
       }
