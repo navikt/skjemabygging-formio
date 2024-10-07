503275e5b17682df758a03e3a79a6d4711d48eee
Lars Olav Torvik <lars.olav.torvik@nav.no>
2024-10-07 14:13:27 +0200

    Update to our checkbox

--- packages/shared-components/src/formio/components/base/editForm/display/editFormBackgroundColor.ts
+++ packages/shared-components/src/formio/components/base/editForm/display/editFormBackgroundColor.ts
@@ -2,7 +2,7 @@ import { Component } from '@navikt/skjemadigitalisering-shared-domain';
 
 const editFormBackgroundColor = (): Component => {
   return {
-    type: 'checkbox',
+    type: 'navCheckbox',
     label: 'Bakgrunnsfarge',
     key: 'backgroundColor',
     defaultValue: true,
