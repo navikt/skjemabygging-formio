e6be0cd5846c572fb4b66473522b25dce3e30eab
Magnus Røed Hestvik <mrh@computas.com>
2024-10-18 13:56:50 +0200

    Fix dependencies

--- packages/bygger-backend/src/routers/api/helpers/azureOnBehalfOfTokenHandler.ts
+++ packages/bygger-backend/src/routers/api/helpers/azureOnBehalfOfTokenHandler.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
-import fetch from 'node-fetch';
 import qs from 'qs';
-import { config } from '../../../../../fyllut-backend/src/config/config';
-import { logger } from '../../../../../fyllut-backend/src/logger';
-import { toJsonOrThrowError } from '../../../../../fyllut-backend/src/utils/errorHandling';
+import config from '../../../config';
+import { fetchWithErrorHandling } from '../../../fetchUtils';
+import { logger } from '../../../logging/logger';
+import { UnauthorizedError } from './errors';
 
-const { clientId, clientSecret, azureOpenidTokenEndpoint, isDevelopment } = config;
+const { azure, isDevelopment } = config;
 
 const azureOnBehalfOfTokenHandler = (scope: string) => async (req: Request, res: Response, next: NextFunction) => {
   const accessToken = req.get('Authorization')?.replace('Bearer ', '');
@@ -16,26 +16,25 @@ const azureOnBehalfOfTokenHandler = (scope: string) => async (req: Request, res:
   }
 
   try {
-    const response = await fetch(azureOpenidTokenEndpoint, {
+    const response = await fetchWithErrorHandling(azure.openidTokenEndpoint, {
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       method: 'POST',
       body: qs.stringify({
         assertion: accessToken,
-        client_id: clientId,
-        client_secret: clientSecret,
+        client_id: azure.clientId,
+        client_secret: azure.clientSecret,
         grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
         requested_token_use: 'on_behalf_of',
         scope: `api://${scope}/.default`,
       }),
     });
-    const json: { access_token: string } = await toJsonOrThrowError('Feil ved autentisering')(response);
-    req.headers.AzureAccessToken = json.access_token;
+    req.headers.AzureAccessToken = (response.data as any).access_token;
     next();
   } catch (error: any) {
     if (error.http_response_body) {
-      logger.error(`Access token failed with: ${JSON.stringify(error.http_response_body)}`);
+      next(new UnauthorizedError(`Access token failed with: ${JSON.stringify(error.http_response_body)}`));
     } else {
-      logger.error(`Access token failed with: ${JSON.stringify(error)}`);
+      next(new UnauthorizedError(`Access token failed with: ${JSON.stringify(error)}`));
     }
 
     next(error);
