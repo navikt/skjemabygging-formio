866ab8580b990a67a90ea9613367b0f0ea771868
Lars Olav Torvik <lars.olav.torvik@nav.no>
2024-10-20 19:18:04 +0200

    Restore test

--- packages/shared-domain/src/utils/localization.test.js
+++ packages/shared-domain/src/utils/localization.test.js
@@ -14,4 +14,30 @@ describe('localization', () => {
       expect(localizationUtils.getLanguageCodeAsIso639_1('en')).toBe('en');
     });
   });
+
+  describe('zipCountryNames', () => {
+    const countryNames1 = [
+      { label: 'Country1', value: 'X1' },
+      { label: 'Country2', value: 'X2' },
+      { label: 'Country3', value: 'X3' },
+      { label: 'Country4', value: 'X4' },
+    ];
+    const countryNames2 = [
+      { label: 'CountryC', value: 'X3' },
+      { label: 'CountryB', value: 'X2' },
+      { label: 'CountryD', value: 'X4' },
+      { label: 'CountryA', value: 'X1' },
+    ];
+
+    it('returns an object combined of the arrays where value is used as identifier', () => {
+      expect(
+        localizationUtils.zipCountryNames(countryNames1, countryNames2, (countryName) => countryName.label),
+      ).toStrictEqual({
+        Country1: 'CountryA',
+        Country2: 'CountryB',
+        Country3: 'CountryC',
+        Country4: 'CountryD',
+      });
+    });
+  });
 });
