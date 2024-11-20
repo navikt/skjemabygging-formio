66274a0b236d316fccab3f5df39c0d304986a3a0
Magnus Røed Hestvik <mrh@computas.com>
2024-11-20 12:41:04 +0100

    Fix keys - nb value for all tags except validering

--- packages/bygger/src/translations/utils/editGlobalTranslationsUtils.ts
+++ packages/bygger/src/translations/utils/editGlobalTranslationsUtils.ts
@@ -5,15 +5,20 @@ const generateAndPopulateTag = (
   textObject,
   storedTranslationsMap: Record<string, FormsApiGlobalTranslation>,
 ) => {
-  return objectUtils.flattenToArray(textObject, ([entryKey, value], parentKey) => {
-    const key = objectUtils.concatKeys(entryKey, parentKey);
-    const stored = storedTranslationsMap?.[key];
-    if (stored && stored.tag === tagName) {
-      return { nb: value, ...stored };
-    }
-    return { key, nb: value, tag: tagName };
-  });
+  return objectUtils
+    .flattenToArray(textObject, ([entryKey, value], parentKey) => {
+      const key = tagName === 'validering' ? objectUtils.concatKeys(entryKey, parentKey) : value;
+      const stored = storedTranslationsMap?.[key];
+      if (stored && stored.tag === tagName) {
+        return { nb: value, ...stored };
+      }
+      return { key, nb: value, tag: tagName };
+    })
+    .filter(removeDuplicatesAfterFirstMatch);
 };
+
+const removeDuplicatesAfterFirstMatch = ({ key }, index, all) =>
+  !key || index === all.findIndex((translation) => translation.key === key);
 
 const getInputHeightInRows = (value?: string, rowSizeInCharacters: number = 35): number => {
   return Math.floor((value ?? '').length / rowSizeInCharacters) + 1;
