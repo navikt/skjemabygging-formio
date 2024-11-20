b174b3bd4d172399dc6d06815faca8c0f196e2e3
Lars Olav Torvik <lars.olav.torvik@nav.no>
2024-11-20 12:05:08 +0100

    Override Aksel default onFucus, since it format the date when clicking the input field.

--- packages/shared-components/src/components/datepicker/DatePicker.tsx
+++ packages/shared-components/src/components/datepicker/DatePicker.tsx
@@ -89,6 +89,7 @@ const DatePicker = ({
         label={label}
         description={description}
         className={className}
+        onFocus={() => {}}
         onBlur={(e) => {
           // Since we have problem listening on empty value on inputProps.value
           // we need to trigger onChange on blur when there is an empty value.
