d79b16e6aecd250e13afeaea6fa3a9df4e789bb0
Lars Olav Torvik <lars.olav@torvik.no>
2024-12-02 08:19:08 +0100

    Update FormioReactComponent.tsx
    Change log level

--- packages/shared-components/src/formio/components/base/FormioReactComponent.tsx
+++ packages/shared-components/src/formio/components/base/FormioReactComponent.tsx
@@ -153,7 +153,7 @@ class FormioReactComponent extends (ReactComponent as unknown as IReactComponent
    * Add message render the error messages Form.io template.
    */
   addMessages(messages) {
-    this.logger.info('Trying to addMessages with Formio.io old template. Should use new error messages instead', {
+    this.logger.warn('Trying to addMessages with Formio.io old template. Should use new error messages instead', {
       messages,
     });
   }
