8a9e4cbaa3fe671a5e3049a8a109878427991c14
Lars Olav Torvik <lars.olav.torvik@nav.no>
2025-01-30 12:32:54 +0100

    Remove more components not in use

--- packages/shared-domain/src/form/index.ts
+++ packages/shared-domain/src/form/index.ts
@@ -213,7 +213,7 @@ export interface Component {
   prefillValue?: string | object;
   protectedApiKey?: boolean;
   yourInformation?: boolean;
-  widthPercent?: string;
+  widthPercent?: number;
 }
 
 export type AddressType = 'NORWEGIAN_ADDRESS' | 'POST_OFFICE_BOX' | 'FOREIGN_ADDRESS';
