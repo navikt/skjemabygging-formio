d2b4cddcebb624626c469666078594d8cc8c4fe7
Magnus Røed Hestvik <mrh@computas.com>
2025-02-12 15:49:36 +0100

    Fiks skjemavisning i editForm

--- packages/bygger/src/components/SkjemaVisningSelect.tsx
+++ packages/bygger/src/components/SkjemaVisningSelect.tsx
@@ -1,8 +1,9 @@
 import { ToggleGroup } from '@navikt/ds-react';
 import { makeStyles } from '@navikt/skjemadigitalisering-shared-components';
+import { DisplayType } from '@navikt/skjemadigitalisering-shared-domain';
 
 interface Props {
-  onChange: (value: string) => void;
+  onChange: (value: DisplayType) => void;
 }
 
 const useStyles = makeStyles({
@@ -15,7 +16,12 @@ const SkjemaVisningSelect = ({ onChange }: Props) => {
   const styles = useStyles();
 
   return (
-    <ToggleGroup className={styles.toggleGroup} defaultValue={'wizard'} onChange={onChange} size="small">
+    <ToggleGroup
+      className={styles.toggleGroup}
+      defaultValue={'wizard'}
+      onChange={(value) => onChange(value as DisplayType)}
+      size="small"
+    >
       <ToggleGroup.Item value="wizard">Veiviser</ToggleGroup.Item>
       <ToggleGroup.Item value="form">Skjema</ToggleGroup.Item>
     </ToggleGroup>
