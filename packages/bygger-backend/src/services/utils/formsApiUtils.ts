83daa180ab2a5df0e578ef89253b47bcb68db093
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2025-02-18 15:33:40 +0100

    set header 'x-correlation-id' on requests to forms-api

--- packages/bygger-backend/src/services/utils/formsApiUtils.ts
+++ packages/bygger-backend/src/services/utils/formsApiUtils.ts
@@ -1,6 +1,10 @@
+import correlator from 'express-correlation-id';
+import { v4 as uuid } from 'uuid';
+
 const createHeaders = (accessToken?: string, revisionId?: number) => {
   return {
     'Content-Type': 'application/json',
+    'x-correlation-id': correlator.getId() ?? uuid(),
     ...(accessToken && { Authorization: `Bearer ${accessToken}` }),
     ...(revisionId && { 'Formsapi-Entity-Revision': `${revisionId}` }),
   };
