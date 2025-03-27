bbfd369a25fdfa42083313055f3a7515aef8a07e
nurrebass1 <16683497+nurrebass1@users.noreply.github.com>
2025-03-27 12:41:36 +0100

    Endret datamodell og sender inn token til API

--- packages/fyllut-backend/src/routers/api/register-data/activities.ts
+++ packages/fyllut-backend/src/routers/api/register-data/activities.ts
@@ -1,14 +1,20 @@
+import { Activity } from '@navikt/skjemadigitalisering-shared-domain';
 import { Request, Response } from 'express';
+import { getTokenxAccessToken } from '../../../security/tokenHelper';
 
-const dummyData = [
-  { label: 'Activity 1', value: 'activity1' },
-  { label: 'Activity 2', value: 'activity2' },
-  { label: 'Activity 3', value: 'activity3' },
-];
-
+const dummyData = {
+  alternativ: [
+    { tekst: 'Activity 1', id: 'activity1' },
+    { tekst: 'Activity 2', id: 'activity2' },
+    { tekst: 'Activity 3', id: 'activity3' },
+  ],
+};
+// GET <xx>/aktiviteter?lang=NB/NN/EN -> selv om vi kun har 1 språk nu
 const activities = {
   get: async (_req: Request, res: Response) => {
-    return res.json(dummyData);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const tokenXAccessToken = getTokenxAccessToken(_req);
+    return res.json(dummyData.alternativ as Activity[]);
   },
 };
 
--- packages/shared-components/src/api/register-data/activities.ts
+++ packages/shared-components/src/api/register-data/activities.ts
@@ -1,9 +1,10 @@
+import { Activity } from '@navikt/skjemadigitalisering-shared-domain';
 import { AppConfigContextType } from '../../context/config/configContext';
 
 const getActivities = async (appConfig: AppConfigContextType) => {
   const { http, baseUrl } = appConfig;
 
-  return http?.get<any>(`${baseUrl}/api/register-data/activities`);
+  return http?.get<Activity[]>(`${baseUrl}/api/register-data/activities`);
 };
 
 export { getActivities };
