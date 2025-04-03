c7d600ae10a7ac8bbc3384cd7fb5018c2cc07bde
nurrebass1 <16683497+nurrebass1@users.noreply.github.com>
2025-04-03 14:01:28 +0200

    Fjerner aktivitet fra responsen

--- packages/fyllut-backend/src/routers/api/register-data/utils.ts
+++ packages/fyllut-backend/src/routers/api/register-data/utils.ts
@@ -1,8 +1,8 @@
 import { Activity } from '@navikt/skjemadigitalisering-shared-domain';
 
-export type ActivityResponse = { alternativ: Array<{ id: string; tekst: string }> };
+export type ActivityResponse = { id: string; tekst: string }[];
 
-const mapActivityResponse = ({ alternativ }: ActivityResponse): Activity[] =>
-  alternativ.map(({ id, tekst }) => ({ value: id, label: tekst }));
+const mapActivityResponse = (response: ActivityResponse): Activity[] =>
+  response.map(({ id, tekst }) => ({ value: id, label: tekst }));
 
 export { mapActivityResponse };
