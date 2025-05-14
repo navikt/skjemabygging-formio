8566532868b45cf32f43cda4da90cec11bbe4b0d
nurrebass1 <16683497+nurrebass1@users.noreply.github.com>
2025-05-14 07:39:49 +0200

    Validerer formPath

--- packages/fyllut-backend/src/middleware/tryCatch.ts
+++ packages/fyllut-backend/src/middleware/tryCatch.ts
@@ -1,9 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 import { htmlResponseError } from '../utils/errorHandling';
+import { isValidPath } from '../utils/url';
 
 const tryCatch =
   (fn: (req: Request, res: Response, next: NextFunction) => void) =>
   async (req: Request, res: Response, next: NextFunction) => {
+    const formPath = req.params?.form;
+    if (formPath && !isValidPath(formPath)) {
+      throw new Error(`Invalid formPath: ${formPath}`);
+    }
     try {
       await fn(req, res, next);
     } catch (e: any) {
--- packages/fyllut-backend/src/utils/url.ts
+++ packages/fyllut-backend/src/utils/url.ts
@@ -1,6 +1,10 @@
 import { Request } from 'express';
 import { config } from '../config/config';
 
+export function isValidPath(path: string) {
+  return /^[a-z0-9]+$/.test(path);
+}
+
 export const getFyllutUrl = (req: Request) => {
   return config.isDevelopment
     ? `http://localhost:${config.port}${config.fyllutPath}`
