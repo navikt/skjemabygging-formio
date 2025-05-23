cf7264e7d637ccef71b53c067cb62fe7990726a3
nilsarne <nils-arne.ringstad@nav.no>
2025-05-23 16:56:04 +0200

    Lagt til kode for å bruke familie-pdf til å generere PDF av søknad, og kode for å benytte innsending-api tjeneste for å slå sammen førsteside med søknad. Tilpasset tester

--- packages/fyllut/cypress.config.ts
+++ packages/fyllut/cypress.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
       SEND_INN_FRONTEND: 'http://localhost:3300/send-inn-frontend',
       TOKEN_X_WELL_KNOWN_URL: 'http://127.0.0.1:3300/tokenx/.well-known',
       BASE_URL: 'http://localhost:3001',
-      FAMILIE_PDF_GENERATOR_URL: 'http://localhost:3001',
+      FAMILIE_PDF_GENERATOR_URL: 'http:///127.0.0.1:3300',
     },
   },
 });
