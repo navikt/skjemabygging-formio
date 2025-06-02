e0d8d411c64cf40158447a3f46906524281695bf
Lars Olav Torvik <lotorvik@torvik.no>
2025-06-02 10:23:08 +0200

    Add hover effect for intro link panel

--- packages/fyllut/src/components/intro/IntroLinkPanel.tsx
+++ packages/fyllut/src/components/intro/IntroLinkPanel.tsx
@@ -24,6 +24,11 @@ const useStyles = makeStyles({
 
     '&:hover': {
       backgroundColor: 'var(--a-surface-action-subtle)',
+      '& a': {
+        textDecoration: 'underline',
+        textDecorationThickness: '2px',
+        textUnderlineOffset: '.15625rem',
+      },
     },
   },
   content: {
