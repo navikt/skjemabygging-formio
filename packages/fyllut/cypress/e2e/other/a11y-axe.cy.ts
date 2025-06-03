9b2b4250413d3512b3b898f403debdb07750616d
Lars Olav Torvik <lotorvik@torvik.no>
2025-06-03 08:46:01 +0200

    Refactor the a11y test since we now do a redirect on start, so we need to injectAxe after the redirect. Also check that the heading is loaded so we are sure we do axe test on correct page.

--- packages/fyllut/cypress/e2e/other/a11y-axe.cy.ts
+++ packages/fyllut/cypress/e2e/other/a11y-axe.cy.ts
@@ -3,52 +3,65 @@
  */
 
 describe('Axe: Accessibility testing', () => {
+  describe('Test on the intro page', () => {
+    it('Static intro page', () => {
+      cy.defaultIntercepts();
+      cy.visit('/fyllut/cypressaxe');
+      cy.defaultWaits();
+      // Sometimes checkA11y fails if you do it to early, so this is instead of doing cy.wait(1000) or similar.
+      cy.findByRole('main').should('exist');
+      cy.injectAxe();
+      cy.checkA11y();
+    });
+  });
+
   describe('Simple test for all tabs in one run', () => {
     before(() => {
       cy.defaultIntercepts();
-      cy.injectAxe();
       cy.visit('/fyllut/cypressaxe');
       cy.defaultWaits();
-    });
-
-    it('Start page', () => {
-      // Sometimes checkA11y fails if you do it to early, so this is instead of doing cy.wait(1000) or similar.
-      cy.findByRole('main').should('exist');
-      cy.checkA11y();
+      cy.clickStart();
+      cy.injectAxe();
     });
 
     it('Person', () => {
-      cy.contains('Start').click();
+      cy.findByRole('heading', { name: 'Person' }).should('exist');
       cy.checkA11y();
     });
 
     it('Penger og konto', () => {
       cy.contains('Penger og konto').click();
+      cy.findByRole('heading', { name: 'Penger og konto' }).should('exist');
       cy.checkA11y();
     });
 
     it('Bedrift / organisasjon', () => {
       cy.contains('Bedrift / organisasjon').click();
+      cy.findByRole('heading', { name: 'Bedrift / organisasjon' }).should('exist');
       cy.checkA11y();
     });
 
     it('Dato og tid', () => {
       cy.contains('Dato og tid').click();
+      cy.findByRole('heading', { name: 'Dato og tid' }).should('exist');
       cy.checkA11y();
     });
 
     it('Standard felter', () => {
       cy.contains('Standard felter').click();
+      cy.findByRole('heading', { name: 'Standard felter' }).should('exist');
       cy.checkA11y();
     });
 
     it('Layout', () => {
       cy.contains('Layout').click();
+      cy.findByRole('heading', { name: 'Layout' }).should('exist');
       cy.checkA11y();
     });
 
     it('Data', () => {
       cy.contains('Data').click();
+      cy.findByRole('heading', { name: 'Data' }).should('exist');
       cy.checkA11y();
     });
   });
