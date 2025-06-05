6ebd8fdb7fde5adf2b97267936af119701d3608a
Lars Olav Torvik <lotorvik@torvik.no>
2025-06-05 08:58:32 +0200

    Remove mock data for intropage

--- packages/fyllut/src/components/intro/IntroPage.tsx
+++ packages/fyllut/src/components/intro/IntroPage.tsx
@@ -4,12 +4,7 @@ import {
   LanguageSelector,
   useAppConfig,
 } from '@navikt/skjemadigitalisering-shared-components';
-import {
-  Form,
-  formioFormsApiUtils,
-  FormPropertiesType,
-  navFormUtils,
-} from '@navikt/skjemadigitalisering-shared-domain';
+import { Form, formioFormsApiUtils, navFormUtils } from '@navikt/skjemadigitalisering-shared-domain';
 import { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useFormsApiForms from '../../api/useFormsApiForms';
@@ -28,63 +23,6 @@ const IntroPage = () => {
   const { submissionMethod } = useAppConfig();
   const [loading, setLoading] = useState<boolean>(true);
 
-  // TODO: Delete
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const testData = {
-    skjemanummer: 'nav-1234',
-    title: 'test',
-    introPage: {
-      enabled: true,
-      introduction: 'Introduction text',
-      sections: {
-        scope: {
-          title: 'Scope',
-          description: 'Scope description',
-          bulletPoints: ['Bullet point 1', 'Bullet point 2'],
-        },
-        outOfScope: {
-          title: 'Out of scope',
-          description: 'Out of scope description',
-          bulletPoints: ['Bullet point 1', 'Bullet point 2'],
-        },
-        prerequisites: {
-          title: 'Prerequisites',
-          description: 'Prerequisites description',
-          bulletPoints: ['Bullet point 1', 'Bullet point 2'],
-        },
-        dataDisclosure: {
-          title: 'Data disclosure',
-          description: 'Data disclosure description',
-          bulletPoints: ['Bullet point 1', 'Bullet point 2'],
-        },
-        dataTreatment: {
-          title: 'Data treatment',
-          description: 'Data treatment description',
-          bulletPoints: ['Bullet point 1', 'Bullet point 2'],
-        },
-        dataStorage: {
-          title: 'Data storage',
-          description: 'Data storage description',
-          bulletPoints: ['Bullet point 1', 'Bullet point 2'],
-        },
-        automaticProcessing: {
-          title: 'Automatic processing',
-          description: 'Automatic processing description',
-          bulletPoints: ['Bullet point 1', 'Bullet point 2'],
-        },
-        optional: {
-          title: 'Optional',
-          description: 'Optional description',
-          bulletPoints: ['Bullet point 1', 'Bullet point 2'],
-        },
-      },
-      selfDeclaration: 'Self declaration text',
-    },
-    properties: {
-      submissionTypes: ['DIGITAL', 'PAPER'],
-    } as FormPropertiesType,
-  } as Form;
-
   const loadForm = useCallback(async () => {
     if (!formPath) {
       return;
@@ -93,10 +31,7 @@ const IntroPage = () => {
     try {
       const formData = await get(formPath);
       if (formData) {
-        // TODO: Fetch only properties and intro page data
         setForm(await get(formPath));
-        // TODO: Delete
-        //setForm(testData);
       }
     } catch (_e) {
       setForm(undefined);
