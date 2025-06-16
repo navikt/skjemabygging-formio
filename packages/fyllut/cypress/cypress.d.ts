74f21b706c36ed535e594b73c0435a8ea4c0bc4b
Lars Olav Torvik <lars.olav@torvik.no>
2025-06-16 09:46:42 +0200

    Update cypress.d.ts

--- packages/fyllut/cypress/cypress.d.ts
+++ packages/fyllut/cypress/cypress.d.ts
@@ -43,7 +43,10 @@ declare global {
        * Configures the Mocks Server administration API client
        */
       configMocksServer(): Chainable<JQuery<HTMLElement>>;
-
+      
+      /**
+      * Run test only on build code. This is allways true on GitHub, but if you want to run locally you can chanage INCLUDE_DIST_TESTS to true.
+      */
       skipIfNoIncludeDistTests(): Chainable<void>;
     }
   }
