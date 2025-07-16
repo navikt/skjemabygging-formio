957e0d85c297e65d8b1df8233204ba1eda098522
Magnus Røed Hestvik <mrh@computas.com>
2025-07-16 14:25:56 +0200

    Fix after merge

--- packages/shared-components/src/pages/intro/IntroPage.tsx
+++ packages/shared-components/src/pages/intro/IntroPage.tsx
@@ -1,17 +1,14 @@
 import { formioFormsApiUtils } from '@navikt/skjemadigitalisering-shared-domain';
 import { useForm } from '../../context/form/FormContext';
 import { IntroPageProvider } from './IntroPageContext';
-import IntroPageDynamic from './IntroPageDynamic';
-import IntroPageStatic from './IntroPageStatic';
-import SelectSubmissionType from './SelectSubmissionType';
+import IntroPageDisplay from './IntroPageDisplay';
 
 const IntroPage = () => {
   const { form } = useForm();
 
   return (
     <IntroPageProvider form={formioFormsApiUtils.mapNavFormToForm(form)}>
-      <SelectSubmissionType />
-      {form.introPage?.enabled ? <IntroPageDynamic /> : <IntroPageStatic />}
+      <IntroPageDisplay />
     </IntroPageProvider>
   );
 };
