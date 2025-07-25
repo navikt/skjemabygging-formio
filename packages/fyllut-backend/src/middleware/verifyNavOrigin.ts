be3783e9c1f4dca33de52d6ca5c94288813e9fd0
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2025-07-25 12:18:03 +0200

    Log warning on unauthorized request from unknown origin

--- packages/fyllut-backend/src/middleware/verifyNavOrigin.ts
+++ packages/fyllut-backend/src/middleware/verifyNavOrigin.ts
@@ -1,11 +1,13 @@
 import { RequestHandler } from 'express';
 import { config } from '../config/config';
+import { logger } from '../logger';
 
 const { isProduction } = config;
 
 const verifyNavOrigin: RequestHandler = (req, res, next) => {
   const { origin } = req.headers;
   if (isProduction && (!origin || !origin?.endsWith('nav.no'))) {
+    logger.warn(`Unauthorized request from origin: ${origin}`);
     return res.sendStatus(403); // Forbidden
   }
   next();
