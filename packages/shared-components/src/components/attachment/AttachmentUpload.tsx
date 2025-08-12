0e64260dcd414247ce5c4f816c49e811523bd4ca
nurrebass1 <16683497+nurrebass1@users.noreply.github.com>
2025-08-12 10:21:32 +0200

    Håndterer annet vedlegg

--- packages/shared-components/src/components/attachment/AttachmentUpload.tsx
+++ packages/shared-components/src/components/attachment/AttachmentUpload.tsx
@@ -11,10 +11,14 @@ import {
   Radio,
   RadioGroup,
   ReadMore,
+  Textarea,
+  TextField,
   VStack,
 } from '@navikt/ds-react';
 import { AttachmentOption, TEXTS } from '@navikt/skjemadigitalisering-shared-domain';
+import clsx from 'clsx';
 import { useState } from 'react';
+import { useForm } from '../../context/form/FormContext';
 import { useLanguages } from '../../context/languages';
 import { useSendInn } from '../../context/sendInn/sendInnContext';
 import makeStyles from '../../util/styles/jss/jss';
@@ -27,6 +31,8 @@ interface Props {
   multiple?: boolean;
   description?: string;
   isIdUpload?: boolean;
+  className?: string;
+  otherAttachment?: boolean;
 }
 
 const useStyles = makeStyles({
@@ -36,13 +42,23 @@ const useStyles = makeStyles({
   },
 });
 
-const AttachmentUpload = ({ label, options, vedleggId, multiple = false, description, isIdUpload }: Props) => {
+const AttachmentUpload = ({
+  label,
+  options,
+  className,
+  vedleggId,
+  multiple = false,
+  description,
+  isIdUpload,
+  otherAttachment,
+}: Props) => {
   const [selectedOption, setSelectedOption] = useState<string>();
   const [loading, setLoading] = useState<boolean>(false);
   const styles = useStyles();
   const { translate } = useLanguages();
   const { innsendingsId } = useSendInn();
   const { handleUploadFiles, handleDeleteFile, uploadedFiles, errors } = useAttachmentUpload();
+  const { form } = useForm();
 
   const uploadedAttachmentFiles = uploadedFiles.filter((file) => file.vedleggId === vedleggId);
   const error = errors[vedleggId];
@@ -63,19 +79,30 @@ const AttachmentUpload = ({ label, options, vedleggId, multiple = false, descrip
   //TODO: store this in context
   const uploadSelected = !!options.find((option) => option.value === selectedOption)?.upload;
 
+  const uploadButtonText = isIdUpload ? TEXTS.statiske.uploadId.selectFile : TEXTS.statiske.uploadId.uploadFiles;
+  const deadline = form.properties?.ettersendelsesfrist;
+
+  function handleDeleteAll() {
+    uploadedAttachmentFiles.map(({ filId }) => handleDelete(filId));
+  }
+
   return (
-    <VStack gap="8" className="mb">
+    <VStack gap="8" className={clsx('mb', className)}>
       <RadioGroup legend={label} onChange={(value) => setSelectedOption(value)} description={description}>
-        {options.map((option) => (
-          <Radio key={option.value} value={option.value}>
-            {option.label}
-          </Radio>
-        ))}
+        {!uploadedAttachmentFiles.length &&
+          options.map((option) => (
+            <Radio key={option.value} value={option.value}>
+              {option.label}
+            </Radio>
+          ))}
       </RadioGroup>
       {uploadSelected && (
         <VStack gap="4">
           {isIdUpload && <Label>Last opp bilde eller skannet kopi av ID-en din</Label>}
 
+          {otherAttachment && selectedOption === 'leggerVedNaa' && (
+            <TextField label={TEXTS.statiske.attachment.descriptionLabel} size="small" required maxLength={50} />
+          )}
           {uploadedAttachmentFiles.length === 0 && (
             <FileUpload.Trigger multiple={!!innsendingsId && multiple} onSelect={handleUpload}>
               {
@@ -84,10 +111,15 @@ const AttachmentUpload = ({ label, options, vedleggId, multiple = false, descrip
                   loading={loading}
                   icon={<UploadIcon title="a11y-title" fontSize="1.5rem" />}
                 >
-                  {translate(isIdUpload ? TEXTS.statiske.uploadId.selectFile : TEXTS.statiske.uploadId.uploadFiles)}
+                  {translate(uploadButtonText)}
                 </Button>
               }
             </FileUpload.Trigger>
+          )}
+          {uploadedAttachmentFiles.length > 1 && (
+            <Button variant="tertiary" onClick={handleDeleteAll}>
+              {translate(TEXTS.statiske.attachment.deleteAllFiles)}
+            </Button>
           )}
           {uploadedAttachmentFiles.map(({ filId, filnavn, storrelse }) => (
             <FileUpload.Item
@@ -99,21 +131,50 @@ const AttachmentUpload = ({ label, options, vedleggId, multiple = false, descrip
               }}
             ></FileUpload.Item>
           ))}
-          <ReadMore header="Gyldige filformater og størrelser" defaultOpen>
+          {uploadedAttachmentFiles.length > 0 && (
+            <FileUpload.Trigger multiple={!!innsendingsId && multiple} onSelect={handleUpload}>
+              {
+                <Button
+                  variant="secondary"
+                  className={styles.button}
+                  loading={loading}
+                  icon={<UploadIcon title="a11y-title" fontSize="1.5rem" />}
+                >
+                  {translate(TEXTS.statiske.attachment.uploadMoreFiles)}
+                </Button>
+              }
+            </FileUpload.Trigger>
+          )}
+          <ReadMore header={translate(TEXTS.statiske.attachment.sizeAndFormatHeader)}>
             <HStack gap="2" align="start">
               <BodyShort weight="semibold">{translate(TEXTS.statiske.attachment.validFormatsLabel)}</BodyShort>
               <BodyLong>{translate(TEXTS.statiske.attachment.validFormatsDescrption)}</BodyLong>
-
               <BodyShort weight="semibold">{translate(TEXTS.statiske.attachment.maxFileSizeLabel)}</BodyShort>
               <BodyLong>{translate(TEXTS.statiske.attachment.maxFileSizeDescription)}</BodyLong>
             </HStack>
           </ReadMore>
         </VStack>
       )}
+
       {error && (
         <Alert variant="error">
           <BodyLong>{error}</BodyLong>
         </Alert>
+      )}
+
+      {selectedOption === 'ettersender' && deadline && (
+        <Alert variant="warning" inline>
+          {translate(TEXTS.statiske.attachment.deadline, { deadline })}
+        </Alert>
+      )}
+
+      {selectedOption === 'levertTidligere' && (
+        <Textarea
+          label={TEXTS.statiske.attachment.alreadySentLabel}
+          description={TEXTS.statiske.attachment.alreadySentDescription}
+          maxLength={200}
+          required
+        />
       )}
     </VStack>
   );
--- packages/shared-components/src/pages/upload-attachments/AttachmentsUploadPage.tsx
+++ packages/shared-components/src/pages/upload-attachments/AttachmentsUploadPage.tsx
@@ -1,15 +1,26 @@
 import { TEXTS } from '@navikt/skjemadigitalisering-shared-domain';
+import clsx from 'clsx';
 import AttachmentUpload from '../../components/attachment/AttachmentUpload';
 import AttachmentUploadProvider from '../../components/attachment/AttachmentUploadContext';
 import { FormContainer } from '../../components/form/container/FormContainer';
 import { useForm } from '../../context/form/FormContext';
 import { useLanguages } from '../../context/languages';
 import { Attachment, getAttachmentsFromSchemaDefinition } from '../../util/attachment/attachmentsUtil';
+import htmlUtils from '../../util/html/htmlUtils';
+import makeStyles from '../../util/styles/jss/jss';
 import UploadPersonalIdButtonRow from '../upload-personal-id/UploadPersonalIdButtonRow';
+
+const useStyles = makeStyles({
+  attachmentUpload: {
+    borderBottom: '1px solid #C0D6E4',
+    paddingBottom: '2rem',
+  },
+});
 
 export function AttachmentsUploadPage() {
   const { translate } = useLanguages();
   const { form, submission } = useForm();
+  const styles = useStyles();
 
   function shouldEnableUpload(value: string) {
     return !(value === 'ettersender' || value === 'levertTidligere' || value === 'nei');
@@ -32,12 +43,14 @@ export function AttachmentsUploadPage() {
       <AttachmentUploadProvider>
         {attachmentPanels.map(({ label, description, attachmentValues, navId }, index) => (
           <AttachmentUpload
+            className={clsx(index !== attachmentPanels.length - 1 && styles.attachmentUpload)}
             key={index}
             label={label}
-            description={description}
+            description={htmlUtils.extractTextContent(description as string)}
             options={mapKeysToOptions(attachmentValues)}
             vedleggId={navId || ''}
             multiple
+            otherAttachment
           />
         ))}
         <UploadPersonalIdButtonRow />
