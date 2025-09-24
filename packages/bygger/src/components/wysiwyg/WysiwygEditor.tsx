4b7a4a821c8d4598ec7d2961229ddd7db73806e5
nurrebass1 <16683497+nurrebass1@users.noreply.github.com>
2025-09-24 16:44:36 +0200

    WIP

--- packages/bygger/src/components/wysiwyg/WysiwygEditor.tsx
+++ packages/bygger/src/components/wysiwyg/WysiwygEditor.tsx
@@ -12,9 +12,11 @@ import {
 } from '@portabletext/editor';
 import { EventListenerPlugin } from '@portabletext/editor/plugins';
 import { toHTML } from '@portabletext/to-html';
-import { forwardRef, useState } from 'react';
-import { ToolBar } from './ToolBar';
+import clsx from 'clsx';
+import { forwardRef, useRef, useState } from 'react';
 import './editor.css';
+import { LinkModal } from './LinkModal';
+import { LinkData, ToolBar } from './ToolBar';
 
 const useStyles = makeStyles({
   editor: {
@@ -34,137 +36,155 @@ const useStyles = makeStyles({
 });
 
 interface Props {
-  defaultValue?: PortableTextBlock[];
+  defaultValue?: string;
   onChange: (value: string | PortableTextBlock[]) => void;
   error?: string | boolean;
   autoFocus?: boolean;
   className?: string;
 }
 
-const WysiwygEditor = forwardRef<HTMLDivElement, Props>(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  ({ className, defaultValue, onChange, error, autoFocus }, ref) => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const styles = useStyles();
-    const [value, setValue] = useState<PortableTextBlock[] | undefined>(defaultValue);
+const WysiwygEditor = forwardRef<HTMLDivElement, Props>(({ className, defaultValue, onChange, error }, ref) => {
+  const [value, setValue] = useState<string | undefined>(defaultValue);
+  const styles = useStyles();
+  console.log(value);
 
-    const schemaDefinition = defineSchema({
-      decorators: [{ name: 'strong', title: 'B' }],
-      annotations: [{ name: 'link', title: 'Lenke', fields: [{ name: 'href', type: 'string' }] }],
-      styles: [
-        { name: 'p', title: 'Avsnitt' },
-        { name: 'h3', title: 'Overskrift' },
-        { name: 'h4', title: 'Underoverskrift ' },
-      ],
-      lists: [
-        { name: 'bullet', title: 'Bulleted List' },
-        { name: 'number', title: 'Numbered List' },
-      ],
-      inlineObjects: [
-        {
-          name: 'link',
-          fields: [
-            { name: 'title', type: 'string' },
-            { name: 'url', type: 'string' },
-            { name: 'openInNewTab', type: 'boolean' },
-          ],
-        },
-      ],
-    });
+  const schemaDefinition = defineSchema({
+    decorators: [{ name: 'strong', title: 'B' }],
+    annotations: [{ name: 'link', title: 'Lenke', fields: [{ name: 'href', type: 'string' }] }],
+    styles: [
+      { name: 'p', title: 'Avsnitt' },
+      { name: 'h3', title: 'Overskrift' },
+      { name: 'h4', title: 'Underoverskrift ' },
+    ],
+    lists: [
+      { name: 'bullet', title: 'Bulleted List' },
+      { name: 'number', title: 'Numbered List' },
+    ],
+    inlineObjects: [
+      {
+        name: 'link',
+        fields: [
+          { name: 'title', type: 'string' },
+          { name: 'url', type: 'string' },
+          { name: 'openInNewTab', type: 'boolean' },
+        ],
+      },
+    ],
+  });
 
-    const renderDecorator: RenderDecoratorFunction = (props) => {
-      if (props.value === 'strong') {
-        return <strong>{props.children}</strong>;
-      }
-      if (props.value === 'underline') {
-        return <u>{props.children}</u>;
-      }
-      return <>{props.children}</>;
-    };
-
-    const renderStyle: RenderStyleFunction = (props) => {
-      if (props.schemaType.value === 'h3') {
-        return (
-          <Heading level="3" size="large">
-            {props.children}
-          </Heading>
-        );
-      }
-      if (props.schemaType.value === 'h4') {
-        return (
-          <Heading level="4" size="medium">
-            {props.children}
-          </Heading>
-        );
-      }
-      return <>{props.children}</>;
-    };
-
-    function isLink(
-      props: PortableTextChild,
-    ): props is PortableTextChild & { url: string; title: string; openInNewTab: boolean } {
-      return 'openInNewTab' in props;
+  const renderDecorator: RenderDecoratorFunction = (props) => {
+    if (props.value === 'strong') {
+      return <strong>{props.children}</strong>;
     }
+    if (props.value === 'underline') {
+      return <u>{props.children}</u>;
+    }
+    return <>{props.children}</>;
+  };
 
-    const renderChild: RenderChildFunction = (props) => {
-      console.log(JSON.stringify(props.value));
-      if (props.schemaType.name === 'link' && isLink(props.value)) {
-        return (
-          <a href={props.value.url} target={props.value.openInNewTab ? '_blank' : '_self'} rel="noreferrer">
+  const renderStyle: RenderStyleFunction = (props) => {
+    if (props.schemaType.value === 'h3') {
+      return (
+        <Heading level="3" size="large">
+          {props.children}
+        </Heading>
+      );
+    }
+    if (props.schemaType.value === 'h4') {
+      return (
+        <Heading level="4" size="medium">
+          {props.children}
+        </Heading>
+      );
+    }
+    return <>{props.children}</>;
+  };
+
+  function isLink(
+    props: PortableTextChild,
+  ): props is PortableTextChild & { url: string; title: string; openInNewTab: boolean } {
+    return 'openInNewTab' in props;
+  }
+  const [linkData, setLinkData] = useState<LinkData | undefined>();
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  const renderChild: RenderChildFunction = (props) => {
+    const onSubmit = (data: LinkData) => {
+      setLinkData(data);
+    };
+    console.log(JSON.stringify(props.value));
+    if (props.schemaType.name === 'link' && isLink(props.value)) {
+      return (
+        <div>
+          <a
+            href={props.value.url}
+            target={props.value.openInNewTab ? '_blank' : '_self'}
+            rel="noreferrer"
+            onClick={(e) => {
+              e.preventDefault();
+              setLinkData({
+                url: (props.value as any).url,
+                title: (props.value as any).title,
+                openInNewTab: (props.value as any).openInNewTab,
+              });
+              dialogRef.current?.showModal();
+            }}
+          >
             {`${props.value.title}${props.value.openInNewTab ? ' (åpnes i ny fane)' : ''}`}
           </a>
-        );
-      }
-      return <>{props.children}</>;
-    };
-
-    const components = {
-      types: {
-        link: ({ value }) => {
-          console.log('vallie', value);
-          const href = value?.url || '#';
-          const text = value?.title || href;
-          return `<a href="${href}" target="_blank" rel="noopener noreferrer">${text}</a>`;
-        },
-      },
-    };
-
-    const html = toHTML(value, { components });
-
-    return (
-      <EditorProvider
-        initialConfig={{
-          schemaDefinition,
-          initialValue: value,
-        }}
-      >
-        <EventListenerPlugin
-          on={(event) => {
-            if (event.type === 'mutation') {
-              setValue(event.value);
-              onChange(toHTML(event.value, { components }));
-            }
-          }}
-        />
-        <div style={{ marginRight: '2rem' }}>
-          <ToolBar schemaDefinition={schemaDefinition} />
-          <PortableTextEditable
-            ref={ref}
-            renderBlock={(props) => <div>{props.children}</div>}
-            renderDecorator={renderDecorator}
-            renderStyle={renderStyle}
-            renderChild={renderChild}
-            renderListItem={(props) => <>{props.children}</>}
-            style={{ border: '1px solid black', minHeight: '150px', padding: '0.5rem' }}
-            className={className}
-          />
-          <p>{JSON.stringify(value)}</p>
-          <div>{html}</div>
+          <LinkModal dialogRef={dialogRef} onSubmit={onSubmit} setLinkData={setLinkData} linkData={linkData!} />
         </div>
-        {error && typeof error === 'string' && <FieldsetErrorMessage errorMessage={error} ref={ref} />}
-      </EditorProvider>
-    );
-  },
-);
+      );
+    }
+    return <>{props.children}</>;
+  };
+
+  const components = {
+    types: {
+      link: ({ value }) => {
+        console.log('vallie', value);
+        const href = value?.url || '#';
+        const text = value?.title || href;
+        return `<a href="${href}" target="_blank" rel="noopener noreferrer">${text}</a>`;
+      },
+    },
+  };
+
+  const html = toHTML(value ?? [], { components });
+
+  return (
+    <EditorProvider
+      initialConfig={{
+        schemaDefinition,
+        initialValue: value,
+      }}
+    >
+      <EventListenerPlugin
+        on={(event) => {
+          if (event.type === 'mutation') {
+            setValue(event.value);
+            onChange(toHTML(event.value ?? [], { components }));
+          }
+        }}
+      />
+      <div style={{ marginRight: '2rem' }}>
+        <ToolBar schemaDefinition={schemaDefinition} />
+        <PortableTextEditable
+          ref={ref}
+          renderBlock={(props) => <div>{props.children}</div>}
+          renderDecorator={renderDecorator}
+          renderStyle={renderStyle}
+          renderChild={renderChild}
+          renderListItem={(props) => <>{props.children}</>}
+          style={{ border: '1px solid black', minHeight: '150px', padding: '0.5rem' }}
+          className={clsx(className, { [styles.error]: !!error })}
+        />
+        {/*<p>{JSON.stringify(value)}</p>*/}
+        <div>{html}</div>
+      </div>
+      {error && typeof error === 'string' && <FieldsetErrorMessage errorMessage={error} />}
+    </EditorProvider>
+  );
+});
 
 export default WysiwygEditor;
