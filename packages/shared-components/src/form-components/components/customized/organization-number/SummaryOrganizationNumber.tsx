bbb71d7954bef395d2c380312ac8c2d284a65afb
Lars Olav Torvik <lotorvik@torvik.no>
2025-10-06 16:51:07 +0200

    Add formatting for organization number

--- packages/shared-components/src/form-components/components/customized/organization-number/SummaryOrganizationNumber.tsx
+++ packages/shared-components/src/form-components/components/customized/organization-number/SummaryOrganizationNumber.tsx
@@ -1,8 +1,9 @@
 import { FormComponentProps } from '../../../types';
 import { DefaultAnswer } from '../../shared/form-summary';
+import { formatOrganizationNumber } from './organizationNumberUtils';
 
 const SummaryOrganizationNumber = ({ component, submissionPath }: FormComponentProps) => {
-  return <DefaultAnswer component={component} submissionPath={submissionPath} />;
+  return <DefaultAnswer component={component} submissionPath={submissionPath} valueFormat={formatOrganizationNumber} />;
 };
 
 export default SummaryOrganizationNumber;
