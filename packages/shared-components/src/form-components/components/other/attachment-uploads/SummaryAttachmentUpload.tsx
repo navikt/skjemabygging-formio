0ae67327e0618a9178ade2e52e7e8874d78ee15c
Magnus Røed Hestvik <mrh@computas.com>
2025-10-13 08:34:45 +0200

    Display uploaded files

--- packages/shared-components/src/form-components/components/other/attachment-uploads/SummaryAttachmentUpload.tsx
+++ packages/shared-components/src/form-components/components/other/attachment-uploads/SummaryAttachmentUpload.tsx
@@ -1,4 +1,4 @@
-import { Alert, FormSummary } from '@navikt/ds-react';
+import { Alert, FileUpload, FormSummary, VStack } from '@navikt/ds-react';
 import { TEXTS } from '@navikt/skjemadigitalisering-shared-domain';
 import { useForm } from '../../../../context/form/FormContext';
 import { useLanguages } from '../../../../context/languages';
@@ -11,8 +11,9 @@ const SummaryAttachmentUpload = ({ component }) => {
   const submissionAttachment = submission?.attachments?.find(
     (attachment) => attachment.attachmentId === formComponentUtils.getNavId(component),
   );
-  const shouldShowDeadline =
+  const showDeadline =
     submissionAttachment?.value && !!component.attachmentValues?.[submissionAttachment.value]?.showDeadline;
+  const uploadedFiles = (submissionAttachment?.files ?? []).length > 0;
 
   if (submissionAttachment === undefined) {
     return null;
@@ -24,19 +25,33 @@ const SummaryAttachmentUpload = ({ component }) => {
   return (
     <FormSummary.Answer>
       <FormSummary.Label>{translate(label)}</FormSummary.Label>
-      <FormSummary.Value>
-        {translate(TEXTS.statiske.attachment[submissionAttachment.value])}
-        {submissionAttachment.additionalDocumentation && (
-          <div>{translate(submissionAttachment.additionalDocumentation)}</div>
-        )}
-        {shouldShowDeadline && form.properties?.ettersendelsesfrist && (
-          <Alert variant="warning">
-            {translate(TEXTS.statiske.attachment.deadline, {
-              deadline: form.properties?.ettersendelsesfrist,
-            })}
-          </Alert>
-        )}
-      </FormSummary.Value>
+      {uploadedFiles ? (
+        <FormSummary.Value>
+          <VStack gap="2" as="ul">
+            {(submissionAttachment.files ?? []).map((file) => (
+              <FileUpload.Item
+                as="li"
+                key={file.fileId}
+                file={{ name: file.fileName, size: file.size }}
+              ></FileUpload.Item>
+            ))}
+          </VStack>
+        </FormSummary.Value>
+      ) : (
+        <FormSummary.Value>
+          {translate(TEXTS.statiske.attachment[submissionAttachment.value])}
+          {submissionAttachment.additionalDocumentation && (
+            <div>{translate(submissionAttachment.additionalDocumentation)}</div>
+          )}
+          {showDeadline && form.properties?.ettersendelsesfrist && (
+            <Alert variant="warning">
+              {translate(TEXTS.statiske.attachment.deadline, {
+                deadline: form.properties?.ettersendelsesfrist,
+              })}
+            </Alert>
+          )}
+        </FormSummary.Value>
+      )}
     </FormSummary.Answer>
   );
 };
