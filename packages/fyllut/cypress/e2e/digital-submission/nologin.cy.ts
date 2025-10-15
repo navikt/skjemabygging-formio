563a4ca6f4c0a9e97d5bcb2cf576d934d8685375
Magnus Røed Hestvik <mrh@computas.com>
2025-10-15 14:38:55 +0200

    Cypress tests

--- packages/fyllut/cypress/e2e/digital-submission/nologin.cy.ts
+++ packages/fyllut/cypress/e2e/digital-submission/nologin.cy.ts
@@ -105,7 +105,7 @@ describe('Digital submission without user login', () => {
     cy.findByRole('group', { name: 'Hvilken legitimasjon ønsker du å bruke?' }).should('exist');
   });
 
-  describe('Validation', () => {
+  describe('Attachments', () => {
     beforeEach(() => {
       cy.visit('/fyllut/nologinform/legitimasjon?sub=digitalnologin');
       cy.defaultWaits();
@@ -118,24 +118,182 @@ describe('Digital submission without user login', () => {
       cy.findByRole('link', { name: 'Vedlegg' }).click();
     });
 
-    it('validates that all required fields are filled', () => {
-      cy.clickNextStep();
-      cy.get('[data-cy=error-summary]').should('exist');
-      cy.get('[data-cy=error-summary]').within(() => {
-        cy.findByRole('link', { name: 'Du må fylle ut: Vedlegg med masse greier' }).should('exist');
-        cy.findByRole('link', { name: 'Du må fylle ut: Annen dokumentasjon' }).should('exist');
+    describe('Summary page', () => {
+      it('shows titles, value and deadline for attachments with no uploads', () => {
+        cy.findByRole('group', { name: 'Vedlegg med masse greier Beskrivelse til vedlegget' }).within(() =>
+          cy.findByLabelText(TEXTS.statiske.attachment.ettersender).check(),
+        );
+        cy.findByRole('group', {
+          name: 'Annen dokumentasjon Har du noen annen dokumentasjon du ønsker å legge ved?',
+        }).within(() => {
+          cy.findByLabelText(TEXTS.statiske.attachment.nei).check();
+        });
+        cy.clickNextStep();
+        cy.get('.navds-form-summary')
+          .eq(3)
+          .within(() => {
+            cy.findByRole('heading', { name: 'Vedlegg' }).should('exist');
+            cy.findByText('Vedlegg med masse greier').should('exist');
+            cy.findByText(TEXTS.statiske.attachment.ettersender).should('exist');
+            cy.findByText('Bekreftelse på utdanning').should('not.exist');
+            cy.findByText(
+              'Hvis vi ikke har mottatt dette vedlegget innen 14 dager blir saken behandlet med de opplysningene som foreligger.',
+            ).should('exist');
+            cy.findByText('Annen dokumentasjon').should('exist');
+            cy.findByText(TEXTS.statiske.attachment.nei).should('exist');
+          });
       });
-      cy.findByRole('group', { name: 'Vedlegg med masse greier Beskrivelse til vedlegget' }).within(() =>
-        cy.findByLabelText(TEXTS.statiske.attachment.ettersender).check(),
-      );
-      cy.findByRole('group', {
-        name: 'Annen dokumentasjon Har du noen annen dokumentasjon du ønsker å legge ved?',
-      }).within(() => {
-        cy.findByLabelText(TEXTS.statiske.attachment.nei).check();
+
+      it('shows file names and titles for attachments with uploads', () => {
+        cy.findByRole('group', { name: 'Vedlegg med masse greier Beskrivelse til vedlegget' }).within(() =>
+          cy.findByLabelText(TEXTS.statiske.attachment.leggerVedNaa).check(),
+        );
+        cy.get('input[type=file]').selectFile('cypress/fixtures/files/id-billy-bruker.jpg', { force: true });
+        cy.findByRole('group', {
+          name: 'Annen dokumentasjon Har du noen annen dokumentasjon du ønsker å legge ved?',
+        }).within(() => {
+          cy.findByLabelText(TEXTS.statiske.attachment.leggerVedNaa).check();
+        });
+
+        cy.findByRole('textbox', { name: 'Gi vedlegget et beskrivende navn' }).type('Vitnemål');
+        cy.findByRole('button', { name: 'Velg fil' }).click();
+        cy.get('[data-cy="upload-button-en5h1c"] > input[type=file]').selectFile(
+          'cypress/fixtures/files/id-billy-bruker.jpg',
+          {
+            force: true,
+          },
+        );
+        cy.findByRole('button', { name: 'Legg til nytt vedlegg' }).click();
+        cy.findByRole('textbox', { name: 'Gi vedlegget et beskrivende navn' }).type('Egenerklæring');
+        cy.get('[data-cy="upload-button-en5h1c-1"] > input[type=file]').selectFile(
+          'cypress/fixtures/files/small-file.txt',
+          {
+            force: true,
+          },
+        );
+        cy.clickNextStep();
+
+        cy.get('.navds-form-summary')
+          .eq(3)
+          .within(() => {
+            cy.findByRole('heading', { name: 'Vedlegg' }).should('exist');
+            cy.findByText('Vedlegg med masse greier').should('exist');
+            cy.findByText(TEXTS.statiske.attachment.leggerVedNaa).should('not.exist');
+            cy.findByText(
+              'Hvis vi ikke har mottatt dette vedlegget innen 14 dager blir saken behandlet med de opplysningene som foreligger.',
+            ).should('not.exist');
+            cy.findByText('Annen dokumentasjon').should('exist');
+            cy.findByText('Vitnemål').should('exist');
+            cy.findByText('Egenerklæring').should('exist');
+            cy.findAllByText('test.txt').should('have.length', 3);
+          });
       });
-      cy.clickNextStep();
-      cy.findByRole('heading', { name: 'VedleggOpplysninger mangler' }).should('not.exist');
-      cy.findByRole('heading', { name: 'Vedlegg' }).should('exist');
+    });
+
+    describe('Validation', () => {
+      it('validates that all required fields are filled', () => {
+        cy.clickNextStep();
+        cy.get('[data-cy=error-summary]').should('exist');
+        cy.get('[data-cy=error-summary]').within(() => {
+          cy.findAllByRole('link').should('have.length', 2);
+          cy.findByRole('link', { name: 'Du må fylle ut: Vedlegg med masse greier' }).should('exist');
+          cy.findByRole('link', { name: 'Du må fylle ut: Annen dokumentasjon' }).should('exist');
+        });
+
+        cy.findByRole('link', { name: 'Oppsummering' }).click();
+        cy.findByRole('heading', { name: 'VedleggOpplysninger mangler' }).should('exist');
+        cy.findByRole('link', { name: 'Vedlegg' }).click();
+
+        cy.findByRole('group', { name: 'Vedlegg med masse greier Beskrivelse til vedlegget' }).within(() =>
+          cy.findByLabelText(TEXTS.statiske.attachment.ettersender).check(),
+        );
+        cy.findByRole('group', {
+          name: 'Annen dokumentasjon Har du noen annen dokumentasjon du ønsker å legge ved?',
+        }).within(() => {
+          cy.findByLabelText(TEXTS.statiske.attachment.nei).check();
+        });
+
+        cy.clickNextStep();
+        cy.findByRole('heading', { name: 'VedleggOpplysninger mangler' }).should('not.exist');
+        cy.findByRole('heading', { name: 'Vedlegg' }).should('exist');
+      });
+
+      it('validates that files are uploaded for attachment', () => {
+        cy.findByRole('group', { name: 'Vedlegg med masse greier Beskrivelse til vedlegget' }).within(() =>
+          cy.findByLabelText(TEXTS.statiske.attachment.leggerVedNaa).check(),
+        );
+        cy.findByRole('group', {
+          name: 'Annen dokumentasjon Har du noen annen dokumentasjon du ønsker å legge ved?',
+        }).within(() => {
+          cy.findByLabelText(TEXTS.statiske.attachment.nei).check();
+        });
+
+        cy.clickNextStep();
+        cy.get('[data-cy=error-summary]').should('exist');
+        cy.get('[data-cy=error-summary]').within(() => {
+          cy.findAllByRole('link').should('have.length', 1);
+          cy.findByRole('link', { name: 'Du må laste opp fil: Vedlegg med masse greier' }).should('exist');
+        });
+        cy.findByRole('link', { name: 'Oppsummering' }).click();
+        cy.findByRole('heading', { name: 'VedleggOpplysninger mangler' }).should('exist');
+        cy.findByRole('link', { name: 'Vedlegg' }).click();
+
+        cy.get('[data-cy="upload-button-eyobqqf"] > input[type=file]').selectFile(
+          'cypress/fixtures/files/small-file.txt',
+          { force: true },
+        );
+        cy.clickNextStep();
+        cy.findByRole('heading', { name: 'VedleggOpplysninger mangler' }).should('not.exist');
+        cy.findByRole('heading', { name: 'Vedlegg' }).should('exist');
+      });
+
+      it('validates that files are uploaded for other attachment', () => {
+        cy.findByRole('group', { name: 'Vedlegg med masse greier Beskrivelse til vedlegget' }).within(() =>
+          cy.findByLabelText(TEXTS.statiske.attachment.ettersender).check(),
+        );
+        cy.findByRole('group', {
+          name: 'Annen dokumentasjon Har du noen annen dokumentasjon du ønsker å legge ved?',
+        }).within(() => {
+          cy.findByLabelText(TEXTS.statiske.attachment.leggerVedNaa).check();
+        });
+        cy.clickNextStep();
+        cy.get('[data-cy=error-summary]').should('exist');
+        cy.get('[data-cy=error-summary]').within(() => {
+          cy.findAllByRole('link').should('have.length', 1);
+          cy.findByRole('link', { name: 'Du må laste opp fil: Annen dokumentasjon' }).should('exist');
+        });
+        cy.findByRole('link', { name: 'Oppsummering' }).click();
+        cy.findByRole('heading', { name: 'VedleggOpplysninger mangler' }).should('exist');
+        cy.findByRole('link', { name: 'Vedlegg' }).click();
+
+        cy.clickNextStep();
+        cy.findByRole('button', { name: 'Velg fil' }).click();
+        cy.get('[data-cy=error-summary]').should('exist');
+        cy.get('[data-cy=error-summary]').within(() => {
+          cy.findAllByRole('link').should('have.length', 2);
+          cy.findByRole('link', { name: 'Du må fylle ut: Gi vedlegget et beskrivende navn' }).should('exist');
+        });
+        cy.findByRole('textbox', { name: 'Gi vedlegget et beskrivende navn' }).type('Vitnemål');
+        cy.get('[data-cy=error-summary]').should('not.exist');
+        cy.findByRole('button', { name: 'Velg fil' }).click();
+        cy.get('[data-cy="upload-button-en5h1c"] > input[type=file]').selectFile(
+          'cypress/fixtures/files/small-file.txt',
+          {
+            force: true,
+          },
+        );
+        cy.findByRole('button', { name: 'Legg til nytt vedlegg' }).click();
+        cy.findByRole('textbox', { name: 'Gi vedlegget et beskrivende navn' }).type('Egenerklæring');
+        cy.get('[data-cy="upload-button-en5h1c-1"] > input[type=file]').selectFile(
+          'cypress/fixtures/files/small-file.txt',
+          {
+            force: true,
+          },
+        );
+        cy.clickNextStep();
+        cy.findByRole('heading', { name: 'VedleggOpplysninger mangler' }).should('not.exist');
+        cy.findByRole('heading', { name: 'Vedlegg' }).should('exist');
+      });
     });
   });
 });
