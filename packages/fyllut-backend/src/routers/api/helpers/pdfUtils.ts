d8ad9bf031c8bfab8c2c426642ec6f1e0d095a8f
Lars Olav Torvik <lotorvik@torvik.no>
2025-10-15 10:34:48 +0200

    Remove debugs logs that should not be in prod.

--- packages/fyllut-backend/src/routers/api/helpers/pdfUtils.ts
+++ packages/fyllut-backend/src/routers/api/helpers/pdfUtils.ts
@@ -3,7 +3,6 @@ import { logger } from '../../../logger';
 const stringifyPdf = (pdfData) => {
   if (pdfData) {
     try {
-      logger.warn('Trying to stringify pdfData', pdfData);
       return JSON.stringify(pdfData).replaceAll('\\t', '  ');
     } catch (error) {
       logger.warn('Could not stringify pdfData', pdfData, error);
