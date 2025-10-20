f1f873d92a69fe9f7bee88d6994e918320173c41
Lars Olav Torvik <lotorvik@torvik.no>
2025-10-20 16:08:47 +0200

    Design tweeks for for group

--- packages/shared-components/src/styles/form/group.ts
+++ packages/shared-components/src/styles/form/group.ts
@@ -2,6 +2,10 @@ const group = {
   '& .form-group:not(.formio-hidden)': {
     clear: 'both',
     marginBottom: 'var(--a-spacing-10)',
+
+    '&:last-child': {
+      marginBottom: 0,
+    },
   },
   '& .group-margin-small .form-group:not(.formio-hidden)': {
     clear: 'both',
