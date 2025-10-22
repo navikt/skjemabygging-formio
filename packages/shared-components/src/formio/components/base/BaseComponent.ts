f01c413bd5d349f9517616696d2ce286568a772f
Magnus Røed Hestvik <mrh@computas.com>
2025-10-22 12:48:24 +0200

    add check on floatingLabels and inputsOnly

--- packages/shared-components/src/formio/components/base/BaseComponent.ts
+++ packages/shared-components/src/formio/components/base/BaseComponent.ts
@@ -212,7 +212,10 @@ class BaseComponent extends FormioReactComponent {
   }
 
   override labelIsHidden(): boolean {
-    return !this.component?.label || !!(this.component.hideLabel && !this.builderMode);
+    return (
+      !this.component?.label ||
+      !!((this.component.hideLabel || this.options.floatingLabels || this.options.inputsOnly) && !this.builderMode)
+    );
   }
 
   override setCustomValidity(messages: string | ComponentError[], _dirty?: boolean, _external?: boolean) {
