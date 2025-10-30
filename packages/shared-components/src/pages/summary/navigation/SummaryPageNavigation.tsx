4becb538b4ce700e2d38a62e60e3d3254b44ae83
nurrebass1 <16683497+nurrebass1@users.noreply.github.com>
2025-10-30 10:06:42 +0100

    Legger tilbake shouldRenderNextButton for conditional rendering for css styling i NavigationButtonRow

--- packages/shared-components/src/pages/summary/navigation/SummaryPageNavigation.tsx
+++ packages/shared-components/src/pages/summary/navigation/SummaryPageNavigation.tsx
@@ -1,5 +1,5 @@
 import { Alert } from '@navikt/ds-react';
-import { NavFormType, Submission } from '@navikt/skjemadigitalisering-shared-domain';
+import { NavFormType, Submission, submissionTypesUtils } from '@navikt/skjemadigitalisering-shared-domain';
 import { useState } from 'react';
 import EditAnswersButton from '../../../components/button/navigation/edit-answers/EditAnswersButton';
 import FormError from '../../../components/form/FormError';
@@ -8,6 +8,7 @@ import { CancelButton } from '../../../components/navigation/CancelButton';
 import { NavigationButtonRow } from '../../../components/navigation/NavigationButtonRow';
 import { SaveButton } from '../../../components/navigation/SaveButton';
 import { SummaryPageNextButton } from '../../../components/navigation/SummaryPageNextButton';
+import { useAppConfig } from '../../../context/config/configContext';
 import { useSendInn } from '../../../context/sendInn/sendInnContext';
 import { PanelValidation } from '../../../util/form/panel-validation/panelValidation';
 
@@ -21,6 +22,23 @@ export interface Props {
 const SummaryPageNavigation = ({ form, submission, panelValidationList, isValid }: Props) => {
   const { mellomlagringError, isMellomlagringActive } = useSendInn();
   const [error, setError] = useState<Error>();
+  const { submissionMethod, app } = useAppConfig();
+
+  const submissionTypes = form.properties.submissionTypes;
+  const canSubmit =
+    !error &&
+    !!panelValidationList &&
+    panelValidationList.every((panelValidation) => !panelValidation.hasValidationErrors);
+  const sendIPosten =
+    (submissionTypesUtils.isPaperSubmission(submissionTypes) && (submissionMethod === 'paper' || app === 'bygger')) ||
+    submissionTypesUtils.isPaperSubmissionOnly(submissionTypes);
+
+  const shouldRenderNextButton =
+    (canSubmit && sendIPosten) ||
+    (canSubmit && (submissionMethod === 'digital' || submissionTypesUtils.isDigitalSubmissionOnly(submissionTypes))) ||
+    (canSubmit && submissionMethod === 'digitalnologin') ||
+    submissionTypesUtils.isNoneSubmission(submissionTypes);
+
   return (
     <>
       <FormError error={mellomlagringError} />
@@ -38,13 +56,15 @@ const SummaryPageNavigation = ({ form, submission, panelValidationList, isValid 
         saveButton={isMellomlagringActive && <SaveButton submission={submission} />}
         previousButton={<EditAnswersButton form={form} panelValidationList={panelValidationList} />}
         nextButton={
-          <SummaryPageNextButton
-            form={form}
-            submission={submission}
-            panelValidationList={panelValidationList}
-            setError={setError}
-            isValid={isValid}
-          />
+          shouldRenderNextButton && (
+            <SummaryPageNextButton
+              form={form}
+              submission={submission}
+              panelValidationList={panelValidationList}
+              setError={setError}
+              isValid={isValid}
+            />
+          )
         }
       />
     </>
