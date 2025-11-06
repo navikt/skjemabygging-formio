bec3e557add0d48610042a39c3cb666d93dcdb37
Lars Olav Torvik <lotorvik@torvik.no>
2025-11-06 20:01:02 +0100

    Fix some wrong components mock values.

--- packages/fyllut/cypress/e2e/other/summary-page.cy.ts
+++ packages/fyllut/cypress/e2e/other/summary-page.cy.ts
@@ -8,7 +8,7 @@ describe('SummaryPage', () => {
     cy.mocksRestoreRouteVariants();
   });
 
-  it('All values', () => {
+  it('All values, paper', () => {
     cy.visit('/fyllut/components?sub=paper');
     cy.defaultWaits();
     cy.clickShowAllSteps();
@@ -299,7 +299,7 @@ describe('SummaryPage', () => {
       });
   });
 
-  it('All values (en)', () => {
+  it('All values, paper (en)', () => {
     cy.visit('/fyllut/components?sub=paper&lang=en');
     cy.defaultWaits();
     cy.clickShowAllSteps();
@@ -589,6 +589,40 @@ describe('SummaryPage', () => {
         cy.get('dd').eq(0).should('contain.text', 'I will forward the documentation later');
         cy.get('dt').eq(1).should('contain.text', 'Other documentation');
         cy.get('dd').eq(1).should('contain.text', 'No, I have no additional documentation to attach');
+      });
+  });
+
+  it('Digital no login, some components', () => {
+    cy.visit('/fyllut/components/legitimasjon?sub=digitalnologin');
+    cy.defaultWaits();
+    cy.findByRole('group', { name: 'Hvilken legitimasjon ønsker du å bruke?' }).within(() =>
+      cy.findByLabelText('Norsk pass').check(),
+    );
+    cy.get('input[type=file]').selectFile('cypress/fixtures/files/id-billy-bruker.jpg', { force: true });
+    cy.clickNextStep();
+
+    cy.clickShowAllSteps();
+
+    cy.findByRole('link', { name: 'Vedlegg' }).click();
+    cy.findByRole('heading', { name: 'Vedlegg' }).shouldBeVisible();
+    cy.findByRole('group', { name: /Vedlegg/ }).within(() => {
+      cy.findByRole('radio', { name: 'Jeg ettersender dokumentasjonen senere' }).check();
+    });
+    cy.findByRole('group', { name: /Annen dokumentasjon/ }).within(() => {
+      cy.findByRole('radio', { name: 'Nei, jeg har ingen ekstra dokumentasjon jeg vil legge ved' }).check();
+    });
+
+    cy.findByRole('link', { name: 'Oppsummering' }).click();
+    cy.findByRole('heading', { name: 'Oppsummering' }).shouldBeVisible();
+
+    cy.findByRole('heading', { level: 2, name: 'Vedlegg' })
+      .parent()
+      .parent()
+      .within(() => {
+        cy.get('dt').eq(0).should('contain.text', 'Vedlegg');
+        cy.get('dd').eq(0).should('contain.text', 'Jeg ettersender dokumentasjonen senere');
+        cy.get('dt').eq(1).should('contain.text', 'Annen dokumentasjon');
+        cy.get('dd').eq(1).should('contain.text', 'Nei, jeg har ingen ekstra dokumentasjon jeg vil legge ved');
       });
   });
 });
