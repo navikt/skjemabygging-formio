5015ab81eddf2e1cf9e7ebd2836dfca41936a4d2
Lars Olav Torvik <lotorvik@torvik.no>
2025-11-18 08:34:06 +0100

    Add dataDisclosure to components

--- mocks/mocks/form-builder/form/formIntroPage.ts
+++ mocks/mocks/form-builder/form/formIntroPage.ts
@@ -15,6 +15,9 @@ const staticDefaultValues = {
   introduction: '82757584-4f7c-48e2-8c84-8ad6b6ebacbd',
   selfDeclaration: 'introPage.selfDeclaration.description.alt1',
   sections: {
+    dataDisclosure: {
+      title: 'introPage.dataDisclosure.title.alt1',
+    },
     dataTreatment: {
       description: '01dc83d4-cc78-4b5f-8815-adbff6437378',
     },
