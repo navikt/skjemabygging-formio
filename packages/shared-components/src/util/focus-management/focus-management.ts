99d86a42745edfe3e54b4244cfdf7663cc60485d
Lars Olav Torvik <lotorvik@torvik.no>
2025-11-21 11:58:21 +0100

    Add debug info

--- packages/shared-components/src/util/focus-management/focus-management.ts
+++ packages/shared-components/src/util/focus-management/focus-management.ts
@@ -1,13 +1,21 @@
-export function scrollToAndSetFocus(selector, alignTarget = 'center') {
+export function scrollToAndSetFocus(selector, _alignTarget = 'center') {
   const targetTag = document.querySelector(selector);
+  console.log('targetTag', targetTag);
+  console.log('Before focus', document.activeElement);
   if (!targetTag) {
     return;
   }
 
+  targetTag.focus();
+
+  console.log('After focus', document.activeElement);
+
+  /*
   const behavior = window.matchMedia('(prefers-reduced-motion: reduce)').matches ? 'auto' : 'smooth';
   targetTag.scrollIntoView({
     behavior,
     block: alignTarget,
   });
   targetTag.focus({ preventScroll: true });
+  */
 }
