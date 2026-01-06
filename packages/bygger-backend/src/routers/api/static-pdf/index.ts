b674e38cde110c9e1e9c3634a4c4f38593101c1a
Lars Olav Torvik <lotorvik@torvik.no>
2026-01-06 15:03:18 +0100

    Add authentication on delete and post static pdf

--- packages/bygger-backend/src/routers/api/static-pdf/index.ts
+++ packages/bygger-backend/src/routers/api/static-pdf/index.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import multer from 'multer';
+import authHandlers from '../helpers/authHandlers';
 import staticPdf from './staticPdf';
 
 const upload = multer();
 const formsApiStaticPdfRouter = Router({ mergeParams: true });
+const { formsApiAuthHandler } = authHandlers;
 
 formsApiStaticPdfRouter.get('/', staticPdf.getAll);
-formsApiStaticPdfRouter.post('/:languageCode', upload.single('fileContent'), staticPdf.uploadPdf);
 formsApiStaticPdfRouter.get('/:languageCode', staticPdf.downloadPdf);
-formsApiStaticPdfRouter.delete('/:languageCode', staticPdf.deletePdf);
+formsApiStaticPdfRouter.post('/:languageCode', formsApiAuthHandler, upload.single('fileContent'), staticPdf.uploadPdf);
+formsApiStaticPdfRouter.delete('/:languageCode', formsApiAuthHandler, staticPdf.deletePdf);
 
 export default formsApiStaticPdfRouter;
