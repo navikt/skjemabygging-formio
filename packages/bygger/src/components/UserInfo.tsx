694f67f313f6537fbf95b83fcf2eebcde8cdae1e
SonyN <sony.nafstad@nav.no>
2026-01-16 14:03:17 +0100

    Change to correct replacement Icons in aksel-icons.

--- packages/bygger/src/components/UserInfo.tsx
+++ packages/bygger/src/components/UserInfo.tsx
@@ -1,10 +1,10 @@
-import { PersonGroupIcon } from '@navikt/aksel-icons';
+import { PersonIcon } from '@navikt/aksel-icons';
 import { useAuth } from '../context/auth-context';
 
 const UserInfo = () => {
   const { userData } = useAuth();
   if (userData) {
-    return <PersonGroupIcon title={`${userData.name} (${userData.NAVident || 'formio-internal'})`} />;
+    return <PersonIcon title={`${userData.name} (${userData.NAVident || 'formio-internal'})`} />;
   }
   return null;
 };
