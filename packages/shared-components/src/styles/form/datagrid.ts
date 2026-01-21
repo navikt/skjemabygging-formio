f10e8d55e90aaf619d247931b2e4957e8f73e7de
SonyN <sony.nafstad@nav.no>
2026-01-21 23:50:49 +0100

    Adjust spacings within datagrid fieldsets.

--- packages/shared-components/src/styles/form/datagrid.ts
+++ packages/shared-components/src/styles/form/datagrid.ts
@@ -2,6 +2,17 @@ const datagrid = {
   '& .aksel-fieldset__content': {
     marginBottom: 'var(--ax-space-24)',
   },
+  '& .formio-component-datagrid': {
+    '& .aksel-fieldset__content': {
+      marginBottom: 'var(--ax-space-16)',
+    },
+    '& .aksel-fieldset__content:last-of-type': {
+      marginBottom: 0,
+    },
+    '& .aksel-fieldset__content button[ref$="-removeRow"]': {
+      marginBottom: 'var(--ax-space-8)',
+    },
+  },
 };
 
 export default datagrid;
