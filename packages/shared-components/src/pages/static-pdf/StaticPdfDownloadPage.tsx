d06cd287fd797c72aad3f3e30d366dc5bd6cb0c2
Lars Olav Torvik <lotorvik@torvik.no>
2026-01-26 08:12:34 +0100

    Remove console log

--- packages/shared-components/src/pages/static-pdf/StaticPdfDownloadPage.tsx
+++ packages/shared-components/src/pages/static-pdf/StaticPdfDownloadPage.tsx
@@ -1,9 +1,4 @@
-import { useForm } from '../../context/form/FormContext';
-
 const StaticPdfDownloadPage = () => {
-  const { submission } = useForm();
-  console.log(submission);
-
   return <div className="mb"></div>;
 };
 
