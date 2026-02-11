78d9b6b4d6f1c0925b8b5cc0a3e4a36798d82d4e
Lars Olav Torvik <lotorvik@torvik.no>
2026-02-11 23:42:36 +0100

    Add ssr true in vite config

--- packages/shared-backend/vite.config.ts
+++ packages/shared-backend/vite.config.ts
@@ -3,15 +3,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   build: {
+    ssr: true,
     minify: true,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'shared-backend',
       formats: ['es'],
       fileName: 'index',
-    },
-    rollupOptions: {
-      external: ['fs', 'util', 'os', 'crypto', 'stream', 'path', 'http', 'https', 'zlib', 'async_hooks', 'events'],
     },
   },
   test: {
