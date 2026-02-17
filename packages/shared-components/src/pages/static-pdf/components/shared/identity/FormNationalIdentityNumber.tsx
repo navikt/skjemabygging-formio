c786d35a127d09bdc8c82219002235f07315e7ba
Lars Olav Torvik <lotorvik@torvik.no>
2026-02-17 17:01:23 +0100

    Add national identity number as required

--- packages/shared-components/src/pages/static-pdf/components/shared/identity/FormNationalIdentityNumber.tsx
+++ packages/shared-components/src/pages/static-pdf/components/shared/identity/FormNationalIdentityNumber.tsx
@@ -1,19 +1,22 @@
 import { TEXTS } from '@navikt/skjemadigitalisering-shared-domain';
+import { Validators } from '../../../../../context/validator/Validators';
 import FormTextField, { FormTextFieldProps } from '../form/FormTextField';
 
 interface Props extends Omit<FormTextFieldProps, 'label'> {
   label?: string;
+  validators?: Pick<Validators, 'required'>;
 }
 
 const FormNationalIdentityNumber = (props: Props) => {
-  const { label, ...rest } = props;
+  const { label, validators, ...rest } = props;
+  const { required } = validators || { required: true };
 
   return (
     <FormTextField
       label={label ?? TEXTS.statiske.identity.identityNumber}
       {...rest}
       autoComplete="off"
-      validators={{ nationalIdentityNumber: true }}
+      validators={{ required, nationalIdentityNumber: true }}
     />
   );
 };
