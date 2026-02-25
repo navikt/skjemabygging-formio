de36575805ff871f1f12104b68fe479c95fccc40
Lars Olav Torvik <lotorvik@torvik.no>
2026-02-25 18:34:04 +0100

    Add tests for htmlServerUtils

--- packages/shared-components/src/util/html/htmlUtils.test.ts
+++ packages/shared-components/src/util/html/htmlUtils.test.ts
@@ -2,6 +2,28 @@ import { expect } from 'vitest';
 import htmlUtils from './htmlUtils';
 
 describe('htmlUtils', () => {
+  describe('isHtmlString', () => {
+    it('returns true if string contains an opening tag', () => {
+      expect(htmlUtils.isHtmlString('bla bla <a>hello')).toBe(true);
+      expect(htmlUtils.isHtmlString('bla bla <a href="www.url.no">hello')).toBe(true);
+      expect(
+        htmlUtils.isHtmlString('bla bla <a href="www.url.no" target="_blank" rel="noopener noreferrer">hello'),
+      ).toBe(true);
+    });
+
+    it('returns false if the only tag is a <br>', () => {
+      expect(htmlUtils.isHtmlString('bla bla <br>hello')).toBe(false);
+    });
+
+    it('returns true if it contains an allowed tag in addition to a <br>', () => {
+      expect(htmlUtils.isHtmlString('bla <a> bla <br>hello')).toBe(true);
+    });
+
+    it('does not match when string contains a < with no closing >', () => {
+      expect(htmlUtils.isHtmlString('bla bla <a hello')).toBe(false);
+    });
+  });
+
   describe('getHtmlTag', () => {
     it('returns the tag name of the first html element in the string', () => {
       expect(htmlUtils.getHtmlTag('<p>hello</p>')).toBe('P');
@@ -69,10 +91,25 @@ describe('htmlUtils', () => {
   });
 
   describe('sanitizeHtmlString', () => {
+    it('removes script and styles tags', () => {
+      expect(htmlUtils.sanitizeHtmlString('<script></script><p>hello</p>')).toBe('<p>hello</p>');
+      expect(htmlUtils.sanitizeHtmlString('<style>h1 {color:red; }p {color:blue;}</style><p>hello</p>')).toBe(
+        '<p>hello</p>',
+      );
+    });
+
     it('allows target attribute on links and keeps the order of the attributes', () => {
       expect(
         htmlUtils.sanitizeHtmlString('<a href="www.url.no" target="_blank" rel="noopener noreferrer">hello</a>'),
       ).toBe('<a href="www.url.no" target="_blank" rel="noopener noreferrer">hello</a>');
+    });
+
+    it('removes forbidden attributes', () => {
+      const options = { FORBID_ATTR: ['style', 'class'] };
+      expect(htmlUtils.sanitizeHtmlString('<div style="color: deeppink">hello</div>', options)).toBe(
+        '<div>hello</div>',
+      );
+      expect(htmlUtils.sanitizeHtmlString('<div class="wrapper">hello</div>', options)).toBe('<div>hello</div>');
     });
   });
 
