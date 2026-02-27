e83f3d7fca4a841781e010decc7b1fb569512699
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2026-02-27 10:52:36 +0100

    Move common code to ApplicationClient, rename and move services, remove unnecessary config

--- packages/fyllut-backend/src/routers/api/nologin-file/nologin-file.ts
+++ packages/fyllut-backend/src/routers/api/nologin-file/nologin-file.ts
@@ -1,6 +1,6 @@
 import { TEXTS } from '@navikt/skjemadigitalisering-shared-domain';
 import { NextFunction, Request, Response } from 'express';
-import { noLoginFileService } from '../../../services';
+import { nologinService } from '../../../services';
 import { HttpError } from '../../../utils/errors/HttpError';
 
 const nologinFile = {
@@ -20,7 +20,7 @@ const nologinFile = {
         return res.status(500).json({ message: 'Error: innsendingsId mangler i konteksten' });
       }
 
-      const result = await noLoginFileService.postFile(file, accessToken, attachmentId, innsendingsId);
+      const result = await nologinService.postFile(file, accessToken, attachmentId, innsendingsId);
       res.status(201).json(result);
     } catch (error: any) {
       if (error instanceof HttpError && error.http_status === 403) {
@@ -49,11 +49,16 @@ const nologinFile = {
   delete: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const nologinContext = req.getNologinContext();
+      const innsendingsId = nologinContext?.innsendingsId;
       const attachmentId = req.query.attachmentId as string | undefined;
       const fileId = req.params.fileId as string | undefined;
       const accessToken = req.headers.AzureAccessToken as string;
 
-      await noLoginFileService.delete(accessToken, nologinContext?.innsendingsId, attachmentId, fileId);
+      if (!innsendingsId) {
+        return res.status(500).json({ message: 'Error: innsendingsId mangler i konteksten' });
+      }
+
+      await nologinService.delete(accessToken, innsendingsId, attachmentId, fileId);
       res.sendStatus(204);
     } catch (error) {
       next(error);
--- packages/fyllut-backend/src/services/index.ts
+++ packages/fyllut-backend/src/services/index.ts
@@ -1,18 +1,18 @@
 import { config } from '../config/config';
 import AppMetrics from './AppMetrics';
 import FormService from './FormService';
-import NoLoginFileService from './NoLoginFileService';
-import TranslationsService from './TranslationsService';
 import NologinService from './nologin/NologinService';
+import NologinTokenService from './nologin/NologinTokenService';
+import TranslationsService from './TranslationsService';
 
 const translationsService = new TranslationsService(config);
 
-const noLoginFileService = new NoLoginFileService(config);
+const nologinService = new NologinService(config);
 
 const formService = new FormService();
 
 const appMetrics: AppMetrics = new AppMetrics();
 
-const nologinService = NologinService(config);
+const nologinTokenService = NologinTokenService(config);
 
-export { appMetrics, formService, noLoginFileService, nologinService, translationsService };
+export { appMetrics, formService, nologinService, nologinTokenService, translationsService };
