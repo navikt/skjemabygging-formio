5fabc11f21e51e926ed3b814b803ed93f67a0082
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2026-05-19 11:52:01 +0200

    fix: form path after merge conflict

--- packages/fyllut/cypress/e2e/pdf/static-pdf.cy.ts
+++ packages/fyllut/cypress/e2e/pdf/static-pdf.cy.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 
 describe('Static PDF', () => {
   const visitStaticPdfPage = (search = '') => {
-    cy.visit(`/fyllut/staticpdf/pdf${search}`);
+    cy.visit(`/fyllut/pdfstatic/pdf${search}`);
     cy.defaultWaits();
     cy.wait('@getStaticPdf');
   };
