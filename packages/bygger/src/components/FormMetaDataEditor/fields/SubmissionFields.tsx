cffab55e76b59ecdcacbe2b71a8da4e222bf3c64
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2026-05-20 09:57:52 +0200

    fix: typo

--- packages/bygger/src/components/FormMetaDataEditor/fields/SubmissionFields.tsx
+++ packages/bygger/src/components/FormMetaDataEditor/fields/SubmissionFields.tsx
@@ -40,7 +40,7 @@ const SubmissionFields = ({ onChange, diff, form, errors }: SubmissionFieldsProp
       {paperNoCoverPageWarning && (
         <Alert variant="warning" size="small" className="mb" data-testid="paper-no-cover-page-warning">
           Når man kombinerer ingen innsending til Nav med andre standard innsendingstyper er det viktig at lenken for
-          førstnevnte inneholde ?sub=papernocoverpage.
+          førstnevnte inneholder ?sub=papernocoverpage.
         </Alert>
       )}
 
