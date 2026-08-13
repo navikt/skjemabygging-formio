4ba3ac42cc6bb59ffdb1e01843877ce28dd92aae
Lars Olav Torvik <lotorvik@torvik.no>
2026-08-13 13:24:43 +0200

    Revert pdf account

--- packages/shared-backend/src/form-components/components/account-number/PdfAccountNumber.ts
+++ packages/shared-backend/src/form-components/components/account-number/PdfAccountNumber.ts
@@ -1,54 +1,9 @@
-import { formatUtils, PdfData, submissionUtils } from '@navikt/skjemadigitalisering-shared-domain';
+import { formatUtils } from '@navikt/skjemadigitalisering-shared-domain';
+import DefaultAnswer from '../../shared/PdfDefaultAnswer';
 import { PdfComponentProps } from '../../types';
 
-const findNestedValueByKey = (value: unknown, key: string): unknown => {
-  if (!value || typeof value !== 'object') {
-    return undefined;
-  }
-
-  if (Array.isArray(value)) {
-    for (const item of value) {
-      const nested = findNestedValueByKey(item, key);
-      if (nested !== undefined) {
-        return nested;
-      }
-    }
-
-    return undefined;
-  }
-
-  if (key in value) {
-    const direct = value[key as keyof typeof value];
-    if (direct !== '' && direct !== undefined) {
-      return direct;
-    }
-  }
-
-  for (const nestedValue of Object.values(value)) {
-    const nested = findNestedValueByKey(nestedValue, key);
-    if (nested !== undefined) {
-      return nested;
-    }
-  }
-
-  return undefined;
-};
-
-const PdfAccountNumber = (props: PdfComponentProps): PdfData | null => {
-  const { component, submissionPath, submission, translate } = props;
-  const directValue = submissionUtils.getPdfSubmissionValue(submissionPath, submission);
-  const fallbackValue =
-    directValue ?? (component.key ? findNestedValueByKey(submission?.data, component.key) : undefined);
-  const value = typeof fallbackValue === 'string' || typeof fallbackValue === 'number' ? `${fallbackValue}` : undefined;
-
-  if (value === undefined) {
-    return null;
-  }
-
-  return {
-    label: translate(component.label),
-    verdi: formatUtils.formatAccountNumber(value),
-  };
+const PdfAccountNumber = (props: PdfComponentProps) => {
+  return DefaultAnswer(props, formatUtils.formatAccountNumber);
 };
 
 export default PdfAccountNumber;
