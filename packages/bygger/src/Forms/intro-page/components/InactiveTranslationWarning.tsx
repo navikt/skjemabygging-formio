8655465099a7ab56250464744a599a674db65c7d
Magnus Røed Hestvik <mrh@computas.com>
2026-08-21 14:00:48 +0200

    Removed link from warning

--- packages/bygger/src/Forms/intro-page/components/InactiveTranslationWarning.tsx
+++ packages/bygger/src/Forms/intro-page/components/InactiveTranslationWarning.tsx
@@ -1,25 +1,10 @@
-import { ExternalLinkIcon } from '@navikt/aksel-icons';
-import { Alert, Box, Link } from '@navikt/ds-react';
-import { Link as ReactRouterLink } from 'react-router';
-import { useForm } from '../../../context/old_form/FormContext';
+import { Alert, Box } from '@navikt/ds-react';
 
 const InactiveTranslationWarning = () => {
-  const { formState } = useForm();
-  const formPath = formState.form?.path;
-
   return (
     <Box marginBlock="space-8 space-0">
       <Alert variant="warning" size="small" data-testid="inactive-translation-warning">
-        Teksten har en oversettelse som vil bli inaktiv med denne endringen. Vurder å endre under{' '}
-        {formPath ? (
-          <Link as={ReactRouterLink} to={`/forms/${formPath}/oversettelser`} target="_blank" rel="noreferrer">
-            oversettelser <ExternalLinkIcon aria-hidden fontSize="1em" />
-            <span className="sr-only"> (åpnes i ny fane)</span>
-          </Link>
-        ) : (
-          'oversettelser'
-        )}
-        .
+        Teksten har en oversettelse som vil bli inaktiv med denne endringen. Vurder å endre under oversettelser.
       </Alert>
     </Box>
   );
